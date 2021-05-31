d2603bd248d5f1cc1de2260e35bbceb7f86a759c
mad-legion <mad@legionlabs.com>
2021-05-31 12:16:00 -0400

    add init-workspace command

--- modules/source_control/src/main.rs
+++ modules/source_control/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{App, Arg, SubCommand};
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::result::Result;
 
@@ -21,6 +22,30 @@ fn init_local_repository(directory: &str) -> Result<(), String> {
     if let Err(e) = fs::create_dir_all(format!("{}/branches", directory)) {
         return Err(format!("Error creating branches directory: {}", e));
     }
+    if let Err(e) = fs::create_dir_all(format!("{}/locks", directory)) {
+        return Err(format!("Error creating locks directory: {}", e));
+    }
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Workspace {
+    id: String,
+    repository: String,
+}
+
+fn init_workspace(workspace_directory: &str, repository_directory: &str) -> Result<(), String> {
+    if let Ok(_) = fs::metadata(workspace_directory) {
+        return Err(format!("{} already exists", workspace_directory));
+    }
+    if let Err(e) = fs::create_dir_all(format!("{}/.lsc", workspace_directory)) {
+        return Err(format!("Error creating .lsc directory: {}", e));
+    }
+    let spec = Workspace {
+        id: "allo".to_string(),
+        repository: repository_directory.to_string(),
+    };
+    println!("spec {:?}", spec);
     Ok(())
 }
 
@@ -28,21 +53,49 @@ fn main() {
     let matches = App::new("Legion Source Control")
         .version("0.1.0")
         .subcommand(
-            SubCommand::with_name("init-local")
-                .about("Initializes a repository stored on a local filesystem")
+            SubCommand::with_name("init-local-repository")
+                .about("Initializes a repository stored on a local filesystem.")
                 .arg(
-                    Arg::with_name("directory")
-                        .short("d")
-                        .value_name("directory")
+                    Arg::with_name("repository-directory")
+                        .short("r")
+                        .value_name("repository-directory")
                         .required(true)
                         .help("lsc database directory"),
                 ),
         )
+        .subcommand(
+            SubCommand::with_name("init-workspace")
+                .about("Initializes a workspace and populates it with the latest version of the main branch.")
+                .arg(
+                    Arg::with_name("workspace-directory")
+                        .short("w")
+                        .value_name("workspace-directory")
+                        .required(true)
+                        .help("lsc workspace directory"))
+                .arg(
+                    Arg::with_name("repository-directory")
+                        .short("r")
+                        .value_name("repository-directory")
+                        .required(true)
+                        .help("local repository directory"),
+                ),
+        )
         .get_matches();
 
-    if let Some(init_local_match) = matches.subcommand_matches("init-local") {
-        if let Err(e) = init_local_repository(init_local_match.value_of("directory").unwrap()) {
+    if let Some(command_match) = matches.subcommand_matches("init-local-repository") {
+        if let Err(e) = init_local_repository(command_match.value_of("repository-directory").unwrap()) {
             println!("init_local_repository failed: {}", e);
+            std::process::exit(1);
+        }
+    }
+
+    //todo: process in the order specified
+    if let Some(command_match) = matches.subcommand_matches("init-workspace") {
+        if let Err(e) = init_workspace(
+            command_match.value_of("workspace-directory").unwrap(),
+            command_match.value_of("repository-directory").unwrap(),
+        ) {
+            println!("init_workspace failed: {}", e);
             std::process::exit(1);
         }
     }
--- modules/source_control/test.bat
+++ modules/source_control/test.bat
@@ -1,3 +1,13 @@
 @echo off
+SET LSC_BIN_DIR=%~dp0target\debug
 rmdir /Q /S d:\temp\data
-cargo run -- init-local -d d:\temp\data
+rmdir /Q /S d:\temp\workspace
+
+cargo build
+IF %ERRORLEVEL% NEQ 0 exit /b 1
+
+%LSC_BIN_DIR%\lsc-cli.exe init-local-repository -r d:\temp\data
+IF %ERRORLEVEL% NEQ 0 exit /b 1
+
+%LSC_BIN_DIR%\lsc-cli.exe init-workspace -w d:\temp\workspace -r d:\temp\data
+IF %ERRORLEVEL% NEQ 0 exit /b 1
