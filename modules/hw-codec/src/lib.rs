9dd208d3a99c83f573e444895fdf9d12a14e3354
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-06-05 11:17:47 -0400

    Add hw-codec clippy lints

--- modules/hw-codec/src/lib.rs
+++ modules/hw-codec/src/lib.rs
@@ -1,7 +1,66 @@
+// BEGIN - Legion Labs lints v0.1
+// do not change or add/remove here, but one can add exceptions after this section
+#![deny(unsafe_code)]
+#![warn(
+    clippy::all,
+    clippy::await_holding_lock,
+    clippy::dbg_macro,
+    clippy::debug_assert_with_mut_call,
+    clippy::doc_markdown,
+    clippy::empty_enum,
+    clippy::enum_glob_use,
+    clippy::exit,
+    clippy::explicit_into_iter_loop,
+    clippy::filter_map_next,
+    clippy::fn_params_excessive_bools,
+    clippy::if_let_mutex,
+    clippy::imprecise_flops,
+    clippy::inefficient_to_string,
+    clippy::large_types_passed_by_value,
+    clippy::let_unit_value,
+    clippy::linkedlist,
+    clippy::lossy_float_literal,
+    clippy::macro_use_imports,
+    clippy::map_err_ignore,
+    clippy::map_flatten,
+    clippy::map_unwrap_or,
+    clippy::match_on_vec_items,
+    clippy::match_same_arms,
+    clippy::match_wildcard_for_single_variants,
+    clippy::mem_forget,
+    clippy::mismatched_target_os,
+    clippy::needless_borrow,
+    clippy::needless_continue,
+    clippy::needless_pass_by_value,
+    clippy::option_option,
+    clippy::pub_enum_variant_names,
+    clippy::ref_option_ref,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::string_add_assign,
+    clippy::string_to_string,
+    clippy::suboptimal_flops,
+    clippy::todo,
+    clippy::unimplemented,
+    clippy::unnested_or_patterns,
+    clippy::unused_self,
+    clippy::use_self,
+    clippy::verbose_file_reads,
+    future_incompatible,
+    nonstandard_style,
+    broken_intra_doc_links,
+    private_intra_doc_links,
+    missing_crate_level_docs,
+    rust_2018_idioms
+)]
+// END - Legion Labs standard lints v0.1
+// crate-specific exceptions:
+#![allow()]
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let four = 2 + 2;
+        assert_eq!(four, 4);
     }
 }
