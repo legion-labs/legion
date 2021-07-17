212f0b3ebd259a3222fc82c51b742829a1c5cfbb
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-07-17 03:32:03 -0400

    Add base api defintion

---
+++ modules/graphics-hal/src/backends/null.rs
@@ -0,0 +1,463 @@
+// Don't use standard formatting in this file
+#![allow(unused_attributes)]
+#![allow(unused_variables)]
+#![allow(clippy::unimplemented)]
+
+use crate::*;
+use raw_window_handle::HasRawWindowHandle;
+
+//
+// Root of the API
+//
+#[derive(Clone)]
+struct NullApi;
+
+impl Api for NullApi {
+    fn device_context(&self) -> &NullDeviceContext {
+        unimplemented!()
+    }
+
+    fn destroy(&mut self) -> GfxResult<()> {
+        unimplemented!()
+    }
+
+    type DeviceContext = NullDeviceContext;
+    type Buffer = NullBuffer;
+    type Texture = NullTexture;
+    type Sampler = NullSampler;
+    type ShaderModule = NullShaderModule;
+    type Shader = NullShader;
+    type RootSignature = NullRootSignature;
+    type Pipeline = NullPipeline;
+    type DescriptorSetHandle = NullDescriptorSetHandle;
+    type DescriptorSetArray = NullDescriptorSetArray;
+    type Queue = NullQueue;
+    type CommandPool = NullCommandPool;
+    type CommandBuffer = NullCommandBuffer;
+    type Fence = NullFence;
+    type Semaphore = NullSemaphore;
+    type Swapchain = NullSwapchain;
+}
+
+#[derive(Clone)]
+struct NullDeviceContext;
+impl DeviceContext<NullApi> for NullDeviceContext {
+    fn device_info(&self) -> &DeviceInfo {
+        unimplemented!()
+    }
+
+    fn create_queue(&self, queue_type: QueueType) -> GfxResult<NullQueue> {
+        unimplemented!();
+    }
+    fn create_fence(&self) -> GfxResult<NullFence> {
+        unimplemented!();
+    }
+    fn create_semaphore(&self) -> GfxResult<NullSemaphore> {
+        unimplemented!();
+    }
+    fn create_swapchain(
+        &self,
+        raw_window_handle: &dyn HasRawWindowHandle,
+        swapchain_def: &SwapchainDef,
+    ) -> GfxResult<NullSwapchain> {
+        unimplemented!();
+    }
+    fn create_sampler(&self, sampler_def: &SamplerDef) -> GfxResult<NullSampler> {
+        unimplemented!();
+    }
+    fn create_texture(&self, texture_def: &TextureDef) -> GfxResult<NullTexture> {
+        unimplemented!();
+    }
+    fn create_buffer(&self, buffer_def: &BufferDef) -> GfxResult<NullBuffer> {
+        unimplemented!();
+    }
+    fn create_shader(&self, stages: Vec<ShaderStageDef>) -> GfxResult<NullShader> {
+        unimplemented!();
+    }
+    fn create_root_signature(
+        &self,
+        root_signature_def: &RootSignatureDef,
+    ) -> GfxResult<NullRootSignature> {
+        unimplemented!();
+    }
+    fn create_descriptor_set_array(
+        &self,
+        descriptor_set_array_def: &DescriptorSetArrayDef,
+    ) -> GfxResult<NullDescriptorSetArray> {
+        unimplemented!();
+    }
+    fn create_graphics_pipeline(
+        &self,
+        graphics_pipeline_def: &GraphicsPipelineDef,
+    ) -> GfxResult<NullPipeline> {
+        unimplemented!();
+    }
+    fn create_compute_pipeline(
+        &self,
+        compute_pipeline_def: &ComputePipelineDef,
+    ) -> GfxResult<NullPipeline> {
+        unimplemented!();
+    }
+    fn create_shader_module(&self, data: ShaderModuleDef) -> GfxResult<NullShaderModule> {
+        unimplemented!();
+    }
+
+    fn wait_for_fences(&self, fences: &[&NullFence]) -> GfxResult<()> {
+        unimplemented!();
+    }
+
+    fn find_supported_format(
+        &self,
+        candidates: &[Format],
+        resource_type: ResourceType,
+    ) -> Option<Format> {
+        unimplemented!();
+    }
+    fn find_supported_sample_count(&self, candidates: &[SampleCount]) -> Option<SampleCount> {
+        unimplemented!();
+    }
+}
+
+//
+// Resources (Buffers, Textures, Samplers)
+//
+#[derive(Debug)]
+struct NullBuffer;
+impl Buffer<NullApi> for NullBuffer {
+    fn buffer_def(&self) -> &BufferDef {
+        unimplemented!()
+    }
+    fn map_buffer(&self) -> GfxResult<*mut u8> {
+        unimplemented!()
+    }
+    fn unmap_buffer(&self) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn mapped_memory(&self) -> Option<*mut u8> {
+        unimplemented!()
+    }
+    fn copy_to_host_visible_buffer<T: Copy>(&self, data: &[T]) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn copy_to_host_visible_buffer_with_offset<T: Copy>(
+        &self,
+        data: &[T],
+        buffer_byte_offset: u64,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+}
+
+#[derive(Clone, Debug)]
+struct NullTexture;
+impl Texture<NullApi> for NullTexture {
+    fn texture_def(&self) -> &TextureDef {
+        unimplemented!()
+    }
+}
+
+#[derive(Clone, Debug)]
+struct NullSampler;
+impl Sampler<NullApi> for NullSampler {}
+
+//
+// Shaders/Pipelines
+//
+#[derive(Clone, Debug)]
+struct NullShaderModule;
+impl ShaderModule<NullApi> for NullShaderModule {}
+
+#[derive(Clone, Debug)]
+struct NullShader;
+impl Shader<NullApi> for NullShader {
+    fn pipeline_reflection(&self) -> &PipelineReflection {
+        unimplemented!()
+    }
+}
+
+#[derive(Clone, Debug)]
+struct NullRootSignature;
+impl RootSignature<NullApi> for NullRootSignature {
+    fn pipeline_type(&self) -> PipelineType {
+        unimplemented!()
+    }
+}
+
+#[derive(Debug)]
+struct NullPipeline;
+impl Pipeline<NullApi> for NullPipeline {
+    fn pipeline_type(&self) -> PipelineType {
+        unimplemented!();
+    }
+    fn root_signature(&self) -> &NullRootSignature {
+        unimplemented!();
+    }
+}
+
+//
+// Descriptor Sets
+//
+#[derive(Clone, Debug)]
+struct NullDescriptorSetHandle;
+impl DescriptorSetHandle<NullApi> for NullDescriptorSetHandle {}
+
+#[derive(Debug)]
+struct NullDescriptorSetArray;
+impl DescriptorSetArray<NullApi> for NullDescriptorSetArray {
+    fn handle(&self, array_index: u32) -> Option<NullDescriptorSetHandle> {
+        unimplemented!();
+    }
+    fn root_signature(&self) -> &NullRootSignature {
+        unimplemented!();
+    }
+    fn update_descriptor_set(&mut self, params: &[DescriptorUpdate]) -> GfxResult<()> {
+        unimplemented!();
+    }
+    fn queue_descriptor_set_update(&mut self, update: &DescriptorUpdate) -> GfxResult<()> {
+        unimplemented!();
+    }
+    fn flush_descriptor_set_updates(&mut self) -> GfxResult<()> {
+        unimplemented!();
+    }
+}
+
+//
+// Queues, Command Buffers
+//
+#[derive(Clone, Debug)]
+struct NullQueue;
+impl Queue<NullApi> for NullQueue {
+    fn device_context(&self) -> &NullDeviceContext {
+        unimplemented!()
+    }
+    fn queue_id(&self) -> u32 {
+        unimplemented!();
+    }
+    fn queue_type(&self) -> QueueType {
+        unimplemented!();
+    }
+    fn create_command_pool(&self, command_pool_def: &CommandPoolDef) -> GfxResult<NullCommandPool> {
+        unimplemented!();
+    }
+    fn submit(
+        &self,
+        command_buffers: &[&NullCommandBuffer],
+        wait_semaphores: &[&NullSemaphore],
+        signal_semaphores: &[&NullSemaphore],
+        signal_fence: Option<&NullFence>,
+    ) -> GfxResult<()> {
+        unimplemented!();
+    }
+    fn present(
+        &self,
+        swapchain: &NullSwapchain,
+        wait_semaphores: &[&NullSemaphore],
+        image_index: u32,
+    ) -> GfxResult<PresentSuccessResult> {
+        unimplemented!()
+    }
+    fn wait_for_queue_idle(&self) -> GfxResult<()> {
+        unimplemented!()
+    }
+}
+
+struct NullCommandPool;
+impl CommandPool<NullApi> for NullCommandPool {
+    fn device_context(&self) -> &NullDeviceContext {
+        unimplemented!()
+    }
+    fn create_command_buffer(
+        &self,
+        command_buffer_def: &CommandBufferDef,
+    ) -> GfxResult<NullCommandBuffer> {
+        unimplemented!()
+    }
+    fn reset_command_pool(&self) -> GfxResult<()> {
+        unimplemented!()
+    }
+}
+
+#[derive(Debug)]
+struct NullCommandBuffer;
+impl CommandBuffer<NullApi> for NullCommandBuffer {
+    fn begin(&self) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn end(&self) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn return_to_pool(&self) -> GfxResult<()> {
+        unimplemented!()
+    }
+
+    fn cmd_begin_render_pass(
+        &self,
+        color_targets: &[ColorRenderTargetBinding],
+        depth_target: Option<DepthStencilRenderTargetBinding>,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_end_render_pass(&self) -> GfxResult<()> {
+        unimplemented!()
+    }
+
+    fn cmd_set_viewport(
+        &self,
+        x: f32,
+        y: f32,
+        width: f32,
+        height: f32,
+        depth_min: f32,
+        depth_max: f32,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_set_scissor(&self, x: u32, y: u32, width: u32, height: u32) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_set_stencil_reference_value(&self, value: u32) -> GfxResult<()> {
+        unimplemented!()
+    }
+
+    fn cmd_bind_pipeline(&self, pipeline: &NullPipeline) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_bind_vertex_buffers(
+        &self,
+        first_binding: u32,
+        bindings: &[VertexBufferBinding],
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_bind_index_buffer(&self, binding: &IndexBufferBinding) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_bind_descriptor_set(
+        &self,
+        descriptor_set_array: &NullDescriptorSetArray,
+        index: u32,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_bind_descriptor_set_handle(
+        &self,
+        root_signature: &NullRootSignature,
+        set_index: u32,
+        descriptor_set_handle: &NullDescriptorSetHandle,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+
+    fn cmd_draw(&self, vertex_count: u32, first_vertex: u32) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_draw_instanced(
+        &self,
+        vertex_count: u32,
+        first_vertex: u32,
+        instance_count: u32,
+        first_instance: u32,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_draw_indexed(
+        &self,
+        index_count: u32,
+        first_index: u32,
+        vertex_offset: i32,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_draw_indexed_instanced(
+        &self,
+        index_count: u32,
+        first_index: u32,
+        instance_count: u32,
+        first_instance: u32,
+        vertex_offset: i32,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+
+    fn cmd_dispatch(
+        &self,
+        group_count_x: u32,
+        group_count_y: u32,
+        group_count_z: u32,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+
+    fn cmd_resource_barrier(
+        &self,
+        buffer_barriers: &[BufferBarrier],
+        texture_barriers: &[TextureBarrier],
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_copy_buffer_to_buffer(
+        &self,
+        src_buffer: &NullBuffer,
+        dst_buffer: &NullBuffer,
+        src_offset: u64,
+        dst_offset: u64,
+        size: u64,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+    fn cmd_copy_buffer_to_texture(
+        &self,
+        src_buffer: &NullBuffer,
+        dst_texture: &NullTexture,
+        params: &CmdCopyBufferToTextureParams,
+    ) -> GfxResult<()> {
+        unimplemented!()
+    }
+}
+
+//
+// Fences and Semaphores
+//
+struct NullFence;
+impl Fence<NullApi> for NullFence {
+    fn wait(&self) -> GfxResult<()> {
+        unimplemented!();
+    }
+    fn wait_for_fences(device_context: &NullDeviceContext, fences: &[&Self]) -> GfxResult<()> {
+        unimplemented!();
+    }
+    fn get_fence_status(&self) -> GfxResult<FenceStatus> {
+        unimplemented!();
+    }
+}
+
+struct NullSemaphore;
+impl Semaphore<NullApi> for NullSemaphore {}
+
+//
+// Swapchain
+//
+struct NullSwapchain;
+impl Swapchain<NullApi> for NullSwapchain {
+    fn swapchain_def(&self) -> &SwapchainDef {
+        unimplemented!()
+    }
+    fn image_count(&self) -> usize {
+        unimplemented!()
+    }
+    fn format(&self) -> Format {
+        unimplemented!()
+    }
+    fn acquire_next_image_fence(&mut self, fence: &NullFence) -> GfxResult<SwapchainImage> {
+        unimplemented!()
+    }
+    fn acquire_next_image_semaphore(
+        &mut self,
+        semaphore: &NullSemaphore,
+    ) -> GfxResult<SwapchainImage> {
+        unimplemented!()
+    }
+    fn rebuild(&mut self, swapchain_def: &SwapchainDef) -> GfxResult<()> {
+        unimplemented!()
+    }
+}
--- modules/graphics-hal/src/lib.rs
+++ modules/graphics-hal/src/lib.rs
@@ -1,1 +1,395 @@
+//! Graphics Hal
+//!
 
+// BEGIN - Legion Labs lints v0.2
+// do not change or add/remove here, but one can add exceptions after this section
+#![deny(unsafe_code)]
+#![warn(
+    clippy::all,
+    clippy::await_holding_lock,
+    clippy::char_lit_as_u8,
+    clippy::checked_conversions,
+    clippy::dbg_macro,
+    clippy::debug_assert_with_mut_call,
+    clippy::doc_markdown,
+    clippy::empty_enum,
+    clippy::enum_glob_use,
+    clippy::exit,
+    clippy::expl_impl_clone_on_copy,
+    clippy::explicit_deref_methods,
+    clippy::explicit_into_iter_loop,
+    clippy::fallible_impl_from,
+    clippy::filter_map_next,
+    clippy::float_cmp_const,
+    clippy::fn_params_excessive_bools,
+    clippy::if_let_mutex,
+    clippy::implicit_clone,
+    clippy::imprecise_flops,
+    clippy::inefficient_to_string,
+    clippy::invalid_upcast_comparisons,
+    clippy::large_types_passed_by_value,
+    clippy::let_unit_value,
+    clippy::linkedlist,
+    clippy::lossy_float_literal,
+    clippy::macro_use_imports,
+    clippy::manual_ok_or,
+    clippy::map_err_ignore,
+    clippy::map_flatten,
+    clippy::map_unwrap_or,
+    clippy::match_on_vec_items,
+    clippy::match_same_arms,
+    clippy::match_wildcard_for_single_variants,
+    clippy::mem_forget,
+    clippy::mismatched_target_os,
+    clippy::mut_mut,
+    clippy::mutex_integer,
+    clippy::needless_borrow,
+    clippy::needless_continue,
+    clippy::needless_pass_by_value,
+    clippy::option_option,
+    clippy::path_buf_push_overwrite,
+    clippy::ptr_as_ptr,
+    clippy::ref_option_ref,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::same_functions_in_if_condition,
+    clippy::semicolon_if_nothing_returned,
+    clippy::string_add_assign,
+    clippy::string_lit_as_bytes,
+    clippy::string_to_string,
+    clippy::todo,
+    clippy::trait_duplication_in_bounds,
+    clippy::unimplemented,
+    clippy::unnested_or_patterns,
+    clippy::unused_self,
+    clippy::useless_transmute,
+    clippy::use_self,
+    clippy::verbose_file_reads,
+    clippy::zero_sized_map_values,
+    future_incompatible,
+    nonstandard_style,
+    broken_intra_doc_links,
+    private_intra_doc_links,
+    missing_crate_level_docs,
+    rust_2018_idioms
+)]
+// END - Legion Labs standard lints v0.2
+// crate-specific exceptions:
+#![allow()]
+//#![warn(missing_docs)]
+
+use std::{fmt, sync::Arc};
+
+use raw_window_handle::HasRawWindowHandle;
+
+pub type GfxResult<T> = Result<T, GfxError>;
+
+/// Generic error that contains all the different kinds of errors that may occur when using the API
+#[derive(Debug, Clone)]
+pub enum GfxError {
+    StringError(String),
+    IoError(Arc<std::io::Error>),
+}
+
+mod backends;
+//mod types;
+
+//
+// Root of the API
+//
+pub trait Api: Clone + Sized {
+    fn device_context(&self) -> &Self::DeviceContext;
+    fn destroy(&mut self) -> GfxResult<()>;
+
+    type DeviceContext: DeviceContext<Self>;
+    type Buffer: Buffer<Self>;
+    type Texture: Texture<Self>;
+    type Sampler: Sampler<Self>;
+    type ShaderModule: ShaderModule<Self>;
+    type Shader: Shader<Self>;
+    type RootSignature: RootSignature<Self>;
+    type Pipeline: Pipeline<Self>;
+    type DescriptorSetHandle: DescriptorSetHandle<Self>;
+    type DescriptorSetArray: DescriptorSetArray<Self>;
+    type Queue: Queue<Self>;
+    type CommandPool: CommandPool<Self>;
+    type CommandBuffer: CommandBuffer<Self>;
+    type Fence: Fence<Self>;
+    type Semaphore: Semaphore<Self>;
+    type Swapchain: Swapchain<Self>;
+}
+
+pub struct DeviceInfo();
+pub struct QueueType();
+pub struct SwapchainDef();
+pub struct FenceStatus();
+pub struct Format();
+pub struct SwapchainImage();
+pub struct CommandBufferDef();
+pub struct SamplerDef();
+pub struct TextureDef();
+pub struct BufferDef();
+pub struct ShaderStageDef();
+pub struct RootSignatureDef();
+pub struct DescriptorSetArrayDef();
+pub struct GraphicsPipelineDef();
+pub struct ComputePipelineDef();
+pub struct ResourceType();
+pub struct SampleCount();
+pub struct PipelineReflection();
+pub struct PipelineType();
+pub struct DescriptorUpdate();
+pub struct CommandPoolDef();
+pub struct PresentSuccessResult();
+pub struct ColorRenderTargetBinding();
+pub struct DepthStencilRenderTargetBinding();
+pub struct VertexBufferBinding();
+pub struct IndexBufferBinding();
+pub struct BufferBarrier();
+pub struct TextureBarrier();
+pub struct CmdCopyBufferToTextureParams();
+pub struct ShaderModuleDef();
+
+pub trait DeviceContext<A: Api>: Clone {
+    fn device_info(&self) -> &DeviceInfo;
+    fn create_queue(&self, queue_type: QueueType) -> GfxResult<A::Queue>;
+    fn create_fence(&self) -> GfxResult<A::Fence>;
+    fn create_semaphore(&self) -> GfxResult<A::Semaphore>;
+    fn create_swapchain(
+        &self,
+        raw_window_handle: &dyn HasRawWindowHandle,
+        swapchain_def: &SwapchainDef,
+    ) -> GfxResult<A::Swapchain>;
+    fn create_sampler(&self, sampler_def: &SamplerDef) -> GfxResult<A::Sampler>;
+    fn create_texture(&self, texture_def: &TextureDef) -> GfxResult<A::Texture>;
+    fn create_buffer(&self, buffer_def: &BufferDef) -> GfxResult<A::Buffer>;
+    fn create_shader(&self, stages: Vec<ShaderStageDef>) -> GfxResult<A::Shader>;
+    fn create_root_signature(
+        &self,
+        root_signature_def: &RootSignatureDef,
+    ) -> GfxResult<A::RootSignature>;
+    fn create_descriptor_set_array(
+        &self,
+        descriptor_set_array_def: &DescriptorSetArrayDef,
+    ) -> GfxResult<A::DescriptorSetArray>;
+    fn create_graphics_pipeline(
+        &self,
+        graphics_pipeline_def: &GraphicsPipelineDef,
+    ) -> GfxResult<A::Pipeline>;
+    fn create_compute_pipeline(
+        &self,
+        compute_pipeline_def: &ComputePipelineDef,
+    ) -> GfxResult<A::Pipeline>;
+    fn create_shader_module(&self, data: ShaderModuleDef) -> GfxResult<A::ShaderModule>;
+
+    fn wait_for_fences(&self, fences: &[&A::Fence]) -> GfxResult<()>;
+
+    fn find_supported_format(
+        &self,
+        candidates: &[Format],
+        resource_type: ResourceType,
+    ) -> Option<Format>;
+    fn find_supported_sample_count(&self, candidates: &[SampleCount]) -> Option<SampleCount>;
+}
+
+//
+// Resources (Buffers, Textures, Samplers)
+//
+pub trait Buffer<A: Api>: fmt::Debug {
+    fn buffer_def(&self) -> &BufferDef;
+    fn map_buffer(&self) -> GfxResult<*mut u8>;
+    fn unmap_buffer(&self) -> GfxResult<()>;
+    fn mapped_memory(&self) -> Option<*mut u8>;
+    fn copy_to_host_visible_buffer<T: Copy>(&self, data: &[T]) -> GfxResult<()>;
+    fn copy_to_host_visible_buffer_with_offset<T: Copy>(
+        &self,
+        data: &[T],
+        buffer_byte_offset: u64,
+    ) -> GfxResult<()>;
+}
+pub trait Texture<A: Api>: Clone + fmt::Debug {
+    fn texture_def(&self) -> &TextureDef;
+}
+pub trait Sampler<A: Api>: Clone + fmt::Debug {}
+
+//
+// Shaders/Pipelines
+//
+pub trait ShaderModule<A: Api>: Clone + fmt::Debug {}
+
+pub trait Shader<A: Api>: Clone + fmt::Debug {
+    fn pipeline_reflection(&self) -> &PipelineReflection;
+}
+
+pub trait RootSignature<A: Api>: Clone + fmt::Debug {
+    fn pipeline_type(&self) -> PipelineType;
+}
+
+pub trait Pipeline<A: Api>: fmt::Debug {
+    fn pipeline_type(&self) -> PipelineType;
+    fn root_signature(&self) -> &A::RootSignature;
+}
+
+//
+// Descriptor Sets
+//
+pub trait DescriptorSetHandle<A: Api>: fmt::Debug {}
+
+pub trait DescriptorSetArray<A: Api>: fmt::Debug {
+    fn handle(&self, array_index: u32) -> Option<A::DescriptorSetHandle>;
+    fn root_signature(&self) -> &A::RootSignature;
+    fn update_descriptor_set(&mut self, params: &[DescriptorUpdate]) -> GfxResult<()>;
+    fn queue_descriptor_set_update(&mut self, update: &DescriptorUpdate) -> GfxResult<()>;
+    fn flush_descriptor_set_updates(&mut self) -> GfxResult<()>;
+}
+
+//
+// Queues, Command Buffers
+//
+pub trait Queue<A: Api>: Clone + fmt::Debug {
+    fn device_context(&self) -> &A::DeviceContext;
+    fn queue_id(&self) -> u32;
+    fn queue_type(&self) -> QueueType;
+    fn create_command_pool(&self, command_pool_def: &CommandPoolDef) -> GfxResult<A::CommandPool>;
+    fn submit(
+        &self,
+        command_buffers: &[&A::CommandBuffer],
+        wait_semaphores: &[&A::Semaphore],
+        signal_semaphores: &[&A::Semaphore],
+        signal_fence: Option<&A::Fence>,
+    ) -> GfxResult<()>;
+    fn present(
+        &self,
+        swapchain: &A::Swapchain,
+        wait_semaphores: &[&A::Semaphore],
+        image_index: u32,
+    ) -> GfxResult<PresentSuccessResult>;
+    fn wait_for_queue_idle(&self) -> GfxResult<()>;
+}
+
+pub trait CommandPool<A: Api> {
+    fn device_context(&self) -> &A::DeviceContext;
+    fn create_command_buffer(
+        &self,
+        command_buffer_def: &CommandBufferDef,
+    ) -> GfxResult<A::CommandBuffer>;
+    fn reset_command_pool(&self) -> GfxResult<()>;
+}
+
+pub trait CommandBuffer<A: Api>: fmt::Debug {
+    fn begin(&self) -> GfxResult<()>;
+    fn end(&self) -> GfxResult<()>;
+    fn return_to_pool(&self) -> GfxResult<()>;
+
+    fn cmd_begin_render_pass(
+        &self,
+        color_targets: &[ColorRenderTargetBinding],
+        depth_target: Option<DepthStencilRenderTargetBinding>,
+    ) -> GfxResult<()>;
+    fn cmd_end_render_pass(&self) -> GfxResult<()>;
+
+    fn cmd_set_viewport(
+        &self,
+        x: f32,
+        y: f32,
+        width: f32,
+        height: f32,
+        depth_min: f32,
+        depth_max: f32,
+    ) -> GfxResult<()>;
+    fn cmd_set_scissor(&self, x: u32, y: u32, width: u32, height: u32) -> GfxResult<()>;
+    fn cmd_set_stencil_reference_value(&self, value: u32) -> GfxResult<()>;
+    fn cmd_bind_pipeline(&self, pipeline: &A::Pipeline) -> GfxResult<()>;
+    fn cmd_bind_vertex_buffers(
+        &self,
+        first_binding: u32,
+        bindings: &[VertexBufferBinding],
+    ) -> GfxResult<()>;
+    fn cmd_bind_index_buffer(&self, binding: &IndexBufferBinding) -> GfxResult<()>;
+    fn cmd_bind_descriptor_set(
+        &self,
+        descriptor_set_array: &A::DescriptorSetArray,
+        index: u32,
+    ) -> GfxResult<()>;
+    fn cmd_bind_descriptor_set_handle(
+        &self,
+        root_signature: &A::RootSignature,
+        set_index: u32,
+        descriptor_set_handle: &A::DescriptorSetHandle,
+    ) -> GfxResult<()>;
+
+    fn cmd_draw(&self, vertex_count: u32, first_vertex: u32) -> GfxResult<()>;
+    fn cmd_draw_instanced(
+        &self,
+        vertex_count: u32,
+        first_vertex: u32,
+        instance_count: u32,
+        first_instance: u32,
+    ) -> GfxResult<()>;
+    fn cmd_draw_indexed(
+        &self,
+        index_count: u32,
+        first_index: u32,
+        vertex_offset: i32,
+    ) -> GfxResult<()>;
+    fn cmd_draw_indexed_instanced(
+        &self,
+        index_count: u32,
+        first_index: u32,
+        instance_count: u32,
+        first_instance: u32,
+        vertex_offset: i32,
+    ) -> GfxResult<()>;
+
+    fn cmd_dispatch(
+        &self,
+        group_count_x: u32,
+        group_count_y: u32,
+        group_count_z: u32,
+    ) -> GfxResult<()>;
+
+    fn cmd_resource_barrier(
+        &self,
+        buffer_barriers: &[BufferBarrier],
+        texture_barriers: &[TextureBarrier],
+    ) -> GfxResult<()>;
+    fn cmd_copy_buffer_to_buffer(
+        &self,
+        src_buffer: &A::Buffer,
+        dst_buffer: &A::Buffer,
+        src_offset: u64,
+        dst_offset: u64,
+        size: u64,
+    ) -> GfxResult<()>;
+    fn cmd_copy_buffer_to_texture(
+        &self,
+        src_buffer: &A::Buffer,
+        dst_texture: &A::Texture,
+        params: &CmdCopyBufferToTextureParams,
+    ) -> GfxResult<()>;
+}
+
+//
+// Fences and Semaphores
+//
+pub trait Fence<A: Api> {
+    fn wait(&self) -> GfxResult<()>;
+    fn wait_for_fences(device_context: &A::DeviceContext, fences: &[&A::Fence]) -> GfxResult<()>;
+    fn get_fence_status(&self) -> GfxResult<FenceStatus>;
+}
+
+pub trait Semaphore<A: Api> {}
+
+//
+// Swapchain
+//
+pub trait Swapchain<A: Api> {
+    fn swapchain_def(&self) -> &SwapchainDef;
+    fn image_count(&self) -> usize;
+    fn format(&self) -> Format;
+    fn acquire_next_image_fence(&mut self, fence: &A::Fence) -> GfxResult<SwapchainImage>;
+    fn acquire_next_image_semaphore(
+        &mut self,
+        semaphore: &A::Semaphore,
+    ) -> GfxResult<SwapchainImage>;
+    fn rebuild(&mut self, swapchain_def: &SwapchainDef) -> GfxResult<()>;
+}
