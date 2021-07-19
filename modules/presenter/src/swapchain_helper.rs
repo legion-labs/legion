0d910a775e4802df2107b57cc989b54dce24e02e
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-07-19 00:36:18 -0400

    Rename gaphics-hal to graphics-api

--- modules/presenter/src/swapchain_helper.rs
+++ modules/presenter/src/swapchain_helper.rs
@@ -1,5 +1,5 @@
 use crossbeam_channel::{Receiver, Sender};
-use gfx_hal::{
+use gfx_api::{
     Api, DeviceContext, Fence, Format, GfxError, GfxResult, PresentSuccessResult, Queue, Swapchain,
     SwapchainDef, SwapchainImage,
 };
