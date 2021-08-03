029016d3a8b6a6bd665e8ebdc85788eb8a20128f
Kris Stasik <kris.stasik@legionlabs.com>
2021-08-03 07:07:50 -0400

    Add data-build CLI

---
+++ apps/data-build-cli/src/main.rs
@@ -0,0 +1,109 @@
+use std::{path::PathBuf, str::FromStr};
+
+use clap::{AppSettings, Arg, SubCommand};
+use legion_data_build::{Config, DataBuild, ResourcePath};
+use legion_data_compiler::{
+    compiled_asset_store::CompiledAssetStoreAddr, Locale, Platform, Target,
+};
+
+const ARG_NAME_RESOURCE: &str = "resource";
+const ARG_NAME_BUILDINDEX: &str = "buildindex";
+const ARG_NAME_CAS: &str = "cas";
+const ARG_NAME_MANIFEST: &str = "manifest";
+const ARG_NAME_TARGET: &str = "target";
+const ARG_NAME_PLATFORM: &str = "platform";
+const ARG_NAME_LOCALE: &str = "locale";
+
+fn main() -> Result<(), String> {
+    let matches = clap::App::new("Data Build")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Data Build CLI")
+        .subcommand(
+            SubCommand::with_name("compile")
+                .about("Compile input resource.")
+                .arg(
+                    Arg::with_name(ARG_NAME_RESOURCE)
+                        .required(true)
+                        .help("Source to compile"),
+                )
+                .arg(
+                    Arg::with_name(ARG_NAME_BUILDINDEX)
+                        .takes_value(true)
+                        .required(true)
+                        .long(ARG_NAME_BUILDINDEX)
+                        .help("Build index file."),
+                )
+                .arg(
+                    Arg::with_name(ARG_NAME_CAS)
+                        .takes_value(true)
+                        .long(ARG_NAME_CAS)
+                        .required(true)
+                        .multiple(true)
+                        .help("Compiled Asset Store addresses where assets will be output."),
+                )
+                .arg(
+                    Arg::with_name(ARG_NAME_MANIFEST)
+                        .takes_value(true)
+                        .default_value("output.manifest")
+                        .long(ARG_NAME_MANIFEST)
+                        .help("Manifest file path."),
+                )
+                .arg(
+                    Arg::with_name(ARG_NAME_TARGET)
+                        .required(true)
+                        .takes_value(true)
+                        .long(ARG_NAME_TARGET)
+                        .help("Build target (Game, Server, etc)."),
+                )
+                .arg(
+                    Arg::with_name(ARG_NAME_PLATFORM)
+                        .required(true)
+                        .takes_value(true)
+                        .long(ARG_NAME_PLATFORM)
+                        .help("Build platform (Windows, Unix, etc)"),
+                )
+                .arg(
+                    Arg::with_name(ARG_NAME_LOCALE)
+                        .required(true)
+                        .takes_value(true)
+                        .long(ARG_NAME_LOCALE)
+                        .help("Build localization (en, fr, etc)"),
+                ),
+        )
+        .get_matches();
+
+    if let ("compile", Some(cmd_args)) = matches.subcommand() {
+        let source = cmd_args.value_of(ARG_NAME_RESOURCE).unwrap();
+        let manifest = cmd_args.value_of(ARG_NAME_MANIFEST).unwrap();
+        let target = cmd_args.value_of(ARG_NAME_TARGET).unwrap();
+        let platform = cmd_args.value_of(ARG_NAME_PLATFORM).unwrap();
+        let locale = cmd_args.value_of(ARG_NAME_LOCALE).unwrap();
+        let source_name = ResourcePath::from_str(source).map_err(|_e| "Invalid ResourcePath")?;
+        let manifest_file = PathBuf::from_str(manifest).map_err(|_e| "Invalid Manifest name")?;
+        let target = Target::from_str(target).map_err(|_e| "Invalid Target")?;
+        let platform = Platform::from_str(platform).map_err(|_e| "Invalid Platform")?;
+        let locale = Locale::new(locale);
+        let asset_store_path =
+            CompiledAssetStoreAddr::from(cmd_args.value_of(ARG_NAME_CAS).unwrap());
+        let buildindex_path = PathBuf::from(cmd_args.value_of(ARG_NAME_BUILDINDEX).unwrap());
+
+        let mut config = Config::new(buildindex_path, asset_store_path);
+        if let Ok(cwd) = std::env::current_dir() {
+            config.compiler_dir(cwd);
+        }
+        if let Some(mut exe_dir) = std::env::args().next().map(|s| PathBuf::from(&s)) {
+            if exe_dir.pop() && exe_dir.is_dir() {
+                config.compiler_dir(exe_dir);
+            }
+        }
+
+        let mut build = DataBuild::open(config).map_err(|_e| "Failed to open build index")?;
+        let output = build
+            .compile(&source_name, &manifest_file, target, platform, &locale)
+            .map_err(|e| format!("Compilation Failed: '{}'", e))?;
+
+        println!("{:?}", output);
+    }
+    Ok(())
+}
---
+++ apps/data-build-cli/tests/tests.rs
@@ -0,0 +1,90 @@
+use std::fs;
+
+use legion_data_build::{Config, DataBuild, ResourcePath};
+use legion_data_compiler::compiled_asset_store::CompiledAssetStoreAddr;
+use legion_resources::{test_resource, Project, ResourceRegistry};
+
+static DATABUILD_EXE: &str = env!("CARGO_BIN_EXE_data-build");
+
+#[test]
+fn data_build() {
+    let work_dir = tempfile::tempdir().unwrap();
+
+    let cas = work_dir.path().join("asset_store");
+    let project_dir = work_dir.path();
+    let buildindex_path = work_dir.path().join("build.index");
+    let manifest_path = work_dir.path().join("output.manifest");
+
+    // create output directory
+    fs::create_dir(&cas).expect("new directory");
+
+    // create project that contains test resource.
+    let resource_name = {
+        let resource_name = "test_source";
+        {
+            let mut project = Project::create_new(&project_dir).expect("new project");
+            let mut resources = ResourceRegistry::default();
+            resources.register_type(
+                test_resource::TYPE_ID,
+                Box::new(test_resource::TestResourceProc {}),
+            );
+            let resource = resources
+                .new_resource(test_resource::TYPE_ID)
+                .expect("new resource");
+
+            project
+                .add_resource(
+                    ResourcePath::from(resource_name),
+                    test_resource::TYPE_ID,
+                    &resource,
+                    &mut resources,
+                )
+                .expect("adding the resource");
+        }
+        let config = Config::new(&buildindex_path, CompiledAssetStoreAddr::from(cas.clone()));
+        let mut build = DataBuild::open(config).expect("new build index");
+        build.source_pull().expect("successful pull");
+
+        resource_name
+    };
+
+    let mut command = {
+        let target = "game";
+        let platform = "windows";
+        let locale = "en";
+        let mut command = std::process::Command::new(DATABUILD_EXE);
+        command.arg("compile");
+        command.arg(resource_name);
+        command.arg(format!("--cas={}", cas.to_str().unwrap()));
+        command.arg(format!("--target={}", target));
+        command.arg(format!("--platform={}", platform));
+        command.arg(format!("--locale={}", locale));
+        command.arg(format!(
+            "--manifest={}",
+            manifest_path.to_str().expect("valid path")
+        ));
+        command.arg(format!(
+            "--buildindex={}",
+            buildindex_path.to_str().unwrap()
+        ));
+        command
+    };
+
+    let output = command.output().expect("valid output");
+    if output.status.success() {
+        println!(
+            "{:?}",
+            std::str::from_utf8(&output.stdout).expect("valid utf8")
+        );
+    } else {
+        println!(
+            "{:?}",
+            std::str::from_utf8(&output.stderr).expect("valid utf8")
+        );
+    }
+
+    assert!(output.status.success());
+
+    let output = std::str::from_utf8(&output.stdout).expect("valid utf8");
+    assert!(output.contains("CompiledAsset"));
+}
