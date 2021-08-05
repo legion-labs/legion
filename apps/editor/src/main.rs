80fc0e98d14367c3950ed361829c71ddf5f41669
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-08-05 00:22:05 -0400

    Add main window stub

--- apps/editor/src/main.rs
+++ apps/editor/src/main.rs
@@ -1,3 +1,36 @@
+//! Minimalistic Sciter sample.
+
+use sciter_js::{sciter, window};
+
 fn main() {
-    println!("Hello, world!");
+    // Step 1: Include the 'minimal.html' file as a byte array.
+    // Hint: Take a look into 'minimal.html' which contains some tiscript code.
+    let html = include_bytes!("../resources/main_window.htm");
+
+    // Step 2: Enable the features we need in our tiscript code.
+    sciter::set_global_options(sciter::GlobalOption::ScriptRuntimeFeatures(
+        sciter::ScriptRuntimeFeatures::ALLOW_FILE_IO // Enables `Sciter.machineName()`.  Required for opening file dialog (`view.selectFile()`)
+         | sciter::ScriptRuntimeFeatures::ALLOW_SYSINFO, // Enables opening file dialog (`view.selectFile()`)
+    ))
+    .unwrap();
+
+    sciter::set_global_options(sciter::GlobalOption::UxTheming(true)).unwrap();
+
+    // Enable debug mode for all windows, so that we can inspect them via Inspector.
+    sciter::set_global_options(sciter::GlobalOption::DebugMode(true)).unwrap();
+
+    // Step 3: Create a new main sciter window of type `sciter::Window`.
+    // Hint: The sciter Window wrapper (src/window.rs) contains more
+    // interesting functions to open or attach to another existing window.
+    let mut window = window::WindowBuilder::main().build();
+
+    // Step 4: Load HTML byte array from memory to `sciter::Window`.
+    // Hint: second parameter is an optional uri, it can be `None` in simple cases,
+    // but it is useful for debugging purposes (check the Inspector tool from the Sciter SDK).
+    // Also you can use a `load_file` method, but it requires an absolute path
+    // of the main document to resolve HTML resources properly.
+    window.load_html(html, Some("example://details-summary.htm"));
+
+    // Step 5: Show window and run the main app message loop until window been closed.
+    window.event_loop();
 }
