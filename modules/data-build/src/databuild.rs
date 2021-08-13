234d3313a8867d6c6853d00b0e6a476207e45c3b
Kris Stasik <kris.stasik@legionlabs.com>
2021-08-13 15:17:42 -0400

    Move data build options to a separate file

--- modules/data-build/src/databuild.rs
+++ modules/data-build/src/databuild.rs
@@ -7,9 +7,7 @@ use std::path::{Path, PathBuf};
 use std::time::SystemTime;
 use std::{env, io};
 
-use legion_asset_store::compiled_asset_store::{
-    CompiledAssetStore, CompiledAssetStoreAddr, LocalCompiledAssetStore,
-};
+use legion_asset_store::compiled_asset_store::{CompiledAssetStore, LocalCompiledAssetStore};
 use legion_data_compiler::compiler_api::DATA_BUILD_VERSION;
 use legion_data_compiler::compiler_cmd::{
     list_compilers, CompilerCompileCmd, CompilerCompileCmdOutput, CompilerHashCmd, CompilerInfo,
@@ -22,7 +20,7 @@ use legion_resources::{Project, ResourceId, ResourcePathRef, ResourceType};
 
 use crate::asset_file_writer::write_assetfile;
 use crate::buildindex::{BuildIndex, CompiledAssetInfo, CompiledAssetReference};
-use crate::Error;
+use crate::{DataBuildOptions, Error};
 
 #[derive(Clone, Debug)]
 struct CompileStat {
@@ -53,74 +51,6 @@ fn compute_context_hash(
     compiler_hash.hash(&mut hasher);
     databuild_version.hash(&mut hasher);
     hasher.finish()
-}
-
-/// Options and flags used by [`DataBuild`].
-///
-/// To open or create `DataBuild` first call [`DataBuildOptions::new`], then chain calls to
-/// methods to set different options, then call [`DataBuildOptions::open_or_create`].
-/// This will give  you a [`Result`] with a [`DataBuild`] that you can further operate on.
-///
-/// # Example Usage
-///
-/// ```
-/// # use legion_data_build::DataBuildOptions;
-/// # use legion_asset_store::compiled_asset_store::CompiledAssetStoreAddr;
-/// let mut build = DataBuildOptions::new("./build.index")
-///         .asset_store(&CompiledAssetStoreAddr::from("./asset_store/"))
-///         .compiler_dir("./compilers/")
-///         .create(".");
-/// ```
-#[derive(Clone)]
-pub struct DataBuildOptions {
-    buildindex_path: PathBuf,
-    assetstore_path: CompiledAssetStoreAddr,
-    compiler_search_paths: Vec<PathBuf>,
-}
-
-impl DataBuildOptions {
-    /// Creates a new data build options.
-    pub fn new(buildindex_path: impl AsRef<Path>) -> Self {
-        Self {
-            buildindex_path: buildindex_path.as_ref().to_owned(),
-            assetstore_path: CompiledAssetStoreAddr::from(buildindex_path.as_ref()),
-            compiler_search_paths: vec![],
-        }
-    }
-
-    /// Set asset store location for compiled assets.
-    pub fn asset_store(&mut self, assetstore_path: &CompiledAssetStoreAddr) -> &mut Self {
-        self.assetstore_path = assetstore_path.clone();
-        self
-    }
-
-    /// Adds a directory to compiler search paths.
-    pub fn compiler_dir<T: AsRef<Path>>(&mut self, dir: T) -> &mut Self {
-        self.compiler_search_paths.push(dir.as_ref().to_owned());
-        self
-    }
-
-    /// Opens the existing build index.
-    ///
-    /// If the build index does not exist it creates one if a project is present in the same directory.
-    pub fn open_or_create(&self) -> Result<DataBuild, Error> {
-        DataBuild::open_or_create(self)
-    }
-
-    /// Opens existing build index.
-    ///
-    /// The following conditions need to be met to successfully open a build index:
-    /// * [`CompiledAssetStore`](`legion_asset_store::compiled_asset_store::CompiledAssetStore`) must exist under address set by [`DataBuildOptions::asset_store()`].
-    /// * Build index must exist and be of a supported version provided by [`DataBuildOptions::new()`].
-    /// * The build index must point to an existing [`Project`].
-    pub fn open(&self) -> Result<DataBuild, Error> {
-        DataBuild::open(self)
-    }
-
-    /// Create new build index for a specified project.
-    pub fn create(&self, project_dir: impl AsRef<Path>) -> Result<DataBuild, Error> {
-        DataBuild::new(self, project_dir.as_ref())
-    }
 }
 
 /// Data build interface.
@@ -161,7 +91,7 @@ pub struct DataBuild {
 }
 
 impl DataBuild {
-    fn new(config: &DataBuildOptions, project_dir: &Path) -> Result<Self, Error> {
+    pub(crate) fn new(config: &DataBuildOptions, project_dir: &Path) -> Result<Self, Error> {
         let project = Self::open_project(project_dir)?;
 
         let build_index = BuildIndex::create_new(
@@ -182,7 +112,7 @@ impl DataBuild {
         })
     }
 
-    fn open(config: &DataBuildOptions) -> Result<Self, Error> {
+    pub(crate) fn open(config: &DataBuildOptions) -> Result<Self, Error> {
         let asset_store = LocalCompiledAssetStore::open(config.assetstore_path.clone())
             .ok_or(Error::InvalidAssetStore)?;
 
@@ -199,7 +129,7 @@ impl DataBuild {
     /// Opens the existing build index.
     ///
     /// If the build index does not exist it creates one if a project is present in the directory.
-    fn open_or_create(config: &DataBuildOptions) -> Result<Self, Error> {
+    pub(crate) fn open_or_create(config: &DataBuildOptions) -> Result<Self, Error> {
         let asset_store = LocalCompiledAssetStore::open(config.assetstore_path.clone())
             .ok_or(Error::InvalidAssetStore)?;
         match BuildIndex::open(&config.buildindex_path, Self::version()) {
