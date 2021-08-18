79b43858443d3618c3dcf89701795611e4a2b548
mparenteau-legion <marc.parenteau@legionlabs.com>
2021-08-18 11:19:05 -0400

    clippy for ecs-macros and macro-utils

--- core/ecs-macros/src/lib.rs
+++ core/ecs-macros/src/lib.rs
@@ -1,4 +1,77 @@
-extern crate proc_macro;
+// BEGIN - Legion Labs lints v0.2
+// do not change or add/remove here, but one can add exceptions after this section
+#![deny(unsafe_code)]
+#![warn(
+    clippy::all,
+    clippy::await_holding_lock,
+    clippy::char_lit_as_u8,
+    clippy::checked_conversions,
+    clippy::dbg_macro,
+    clippy::debug_assert_with_mut_call,
+    clippy::doc_markdown,
+    clippy::empty_enum,
+    clippy::enum_glob_use,
+    clippy::exit,
+    clippy::expl_impl_clone_on_copy,
+    clippy::explicit_deref_methods,
+    clippy::explicit_into_iter_loop,
+    clippy::fallible_impl_from,
+    clippy::filter_map_next,
+    clippy::float_cmp_const,
+    clippy::fn_params_excessive_bools,
+    clippy::if_let_mutex,
+    clippy::implicit_clone,
+    clippy::imprecise_flops,
+    clippy::inefficient_to_string,
+    clippy::invalid_upcast_comparisons,
+    clippy::large_types_passed_by_value,
+    clippy::let_unit_value,
+    clippy::linkedlist,
+    clippy::lossy_float_literal,
+    clippy::macro_use_imports,
+    clippy::manual_ok_or,
+    clippy::map_err_ignore,
+    clippy::map_flatten,
+    clippy::map_unwrap_or,
+    clippy::match_on_vec_items,
+    clippy::match_same_arms,
+    clippy::match_wildcard_for_single_variants,
+    clippy::mem_forget,
+    clippy::mismatched_target_os,
+    clippy::mut_mut,
+    clippy::mutex_integer,
+    clippy::needless_borrow,
+    clippy::needless_continue,
+    clippy::needless_pass_by_value,
+    clippy::option_option,
+    clippy::path_buf_push_overwrite,
+    clippy::ptr_as_ptr,
+    clippy::ref_option_ref,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::same_functions_in_if_condition,
+    clippy::semicolon_if_nothing_returned,
+    clippy::string_add_assign,
+    clippy::string_lit_as_bytes,
+    clippy::string_to_string,
+    clippy::todo,
+    clippy::trait_duplication_in_bounds,
+    clippy::unimplemented,
+    clippy::unnested_or_patterns,
+    clippy::unused_self,
+    clippy::useless_transmute,
+    clippy::use_self,
+    clippy::verbose_file_reads,
+    clippy::zero_sized_map_values,
+    future_incompatible,
+    nonstandard_style,
+    broken_intra_doc_links,
+    private_intra_doc_links,
+    missing_crate_level_docs,
+    rust_2018_idioms
+)]
+// END - Legion Labs standard lints v0.2
+// crate-specific exceptions:
+#![allow()]
 
 use legion_macro_utils::LegionManifest;
 use proc_macro::TokenStream;
@@ -21,7 +94,7 @@ struct AllTuples {
 }
 
 impl Parse for AllTuples {
-    fn parse(input: ParseStream) -> Result<Self> {
+    fn parse(input: ParseStream<'_>) -> Result<Self> {
         let macro_ident = input.parse::<Ident>()?;
         input.parse::<Comma>()?;
         let start = input.parse::<LitInt>()?.base10_parse()?;
@@ -33,7 +106,7 @@ impl Parse for AllTuples {
             idents.push(input.parse::<Ident>()?);
         }
 
-        Ok(AllTuples {
+        Ok(Self {
             macro_ident,
             start,
             end,
@@ -331,7 +404,7 @@ pub fn derive_system_param(input: TokenStream) -> TokenStream {
                     .map_or_else(SystemParamFieldAttributes::default, |a| {
                         syn::custom_keyword!(ignore);
                         let mut attributes = SystemParamFieldAttributes::default();
-                        a.parse_args_with(|input: ParseStream| {
+                        a.parse_args_with(|input: ParseStream<'_>| {
                             if input.parse::<Option<ignore>>()?.is_some() {
                                 attributes.ignore = true;
                             }
@@ -457,6 +530,7 @@ pub fn derive_run_criteria_label(input: TokenStream) -> TokenStream {
     derive_label(input, Ident::new("RunCriteriaLabel", Span::call_site())).into()
 }
 
+#[allow(clippy::needless_pass_by_value)]
 fn derive_label(input: DeriveInput, label_type: Ident) -> TokenStream2 {
     let ident = input.ident;
     let ecs_path: Path = legion_ecs_path();
