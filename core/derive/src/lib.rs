3cb3e2edf9d75b4271325b5f531eb923f1234825
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-08-19 07:19:10 -0400

    Remove renderer marco derives

--- core/derive/src/lib.rs
+++ core/derive/src/lib.rs
@@ -78,10 +78,7 @@ mod bytes;
 mod enum_variant_meta;
 mod legion_main;
 mod modules;
-mod render_resource;
-mod render_resources;
 mod resource;
-mod shader_defs;
 
 use proc_macro::TokenStream;
 
@@ -96,26 +93,6 @@ pub fn derive_from_resources(input: TokenStream) -> TokenStream {
 #[proc_macro_derive(Bytes)]
 pub fn derive_bytes(input: TokenStream) -> TokenStream {
     bytes::derive_bytes(input)
-}
-
-/// Derives the `RenderResources` trait. Each field must implement `RenderResource` or this will fail.
-/// You can ignore fields using `#[render_resources(ignore)]`.
-#[proc_macro_derive(RenderResources, attributes(render_resources))]
-pub fn derive_render_resources(input: TokenStream) -> TokenStream {
-    render_resources::derive_render_resources(input)
-}
-
-/// Derives the `RenderResource` trait. The type must also implement `Bytes` or this will fail.
-#[proc_macro_derive(RenderResource)]
-pub fn derive_render_resource(input: TokenStream) -> TokenStream {
-    render_resource::derive_render_resource(input)
-}
-
-/// Derives the `ShaderDefs` trait. Each field must implement `ShaderDef` or this will fail.
-/// You can ignore fields using `#[shader_defs(ignore)]`.
-#[proc_macro_derive(ShaderDefs, attributes(shader_def))]
-pub fn derive_shader_defs(input: TokenStream) -> TokenStream {
-    shader_defs::derive_shader_defs(input)
 }
 
 /// Generates a dynamic plugin entry point function for the given `Plugin` type.  
--- core/derive/src/modules.rs
+++ core/derive/src/modules.rs
@@ -1,5 +1,3 @@
 pub const LEGION_APP: &str = "legion_app";
-pub const LEGION_ASSET: &str = "legion_asset";
 pub const LEGION_CORE: &str = "legion_core";
-pub const LEGION_RENDER: &str = "legion_render";
 pub const LEGION_UTILS: &str = "legion_utils";
