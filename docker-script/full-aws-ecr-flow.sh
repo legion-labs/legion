d5dad823ba591d8abe68223fe4dab4b0dbd8dec6
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-08-25 11:52:52 -0400

    Fix scripts

--- docker-script/full-aws-ecr-flow.sh
+++ docker-script/full-aws-ecr-flow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 AWS_REGION=$1
-sh docker-scripts/build-docker-images.sh
-sh docker-scripts/authenticate-aws-ecr.sh $AWS_REGION
-sh docker-scripts/create-aws-ecr-repository.sh $AWS_REGION
-sh docker-scripts/tag-push-docker-images.sh $AWS_REGION
+sh docker-script/build-docker-images.sh
+sh docker-script/authenticate-aws-ecr.sh $AWS_REGION
+sh docker-script/create-aws-ecr-repository.sh $AWS_REGION
+sh docker-script/tag-push-docker-images.sh $AWS_REGION
