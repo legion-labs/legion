033a69cb49c792d3fa68b3f1c005a720788033e6
mparenteau-legion <marc.parenteau@legionlabs.com>
2021-09-01 11:23:54 -0400

    add runtime-srv, empty hull for now

---
+++ server/runtime-srv/src/main.rs
@@ -0,0 +1,115 @@
+//! The runtime server is the portion of the Legion Engine that runs off runtime
+//! data to simulate a world. It is tied to the lifetime of a runtime client.
+//!
+//! * Tracking Issue: [legion/crate/#xx](https://github.com/legion-labs/legion/issues/xx)
+//! * Design Doc: [legion/book/project-resources](/book/todo.html)
+//!
+
+// BEGIN - Legion Labs lints v0.2
+// do not change or add/remove here, but one can add exceptions after this section
+#![deny(unsafe_code)]
+#![warn(
+    clippy::all,
+    clippy::await_holding_lock,
+    clippy::char_lit_as_u8,
+    clippy::checked_conversions,
+    clippy::dbg_macro,
+    clippy::debug_assert_with_mut_call,
+    clippy::doc_markdown,
+    clippy::empty_enum,
+    clippy::enum_glob_use,
+    clippy::exit,
+    clippy::expl_impl_clone_on_copy,
+    clippy::explicit_deref_methods,
+    clippy::explicit_into_iter_loop,
+    clippy::fallible_impl_from,
+    clippy::filter_map_next,
+    clippy::float_cmp_const,
+    clippy::fn_params_excessive_bools,
+    clippy::if_let_mutex,
+    clippy::implicit_clone,
+    clippy::imprecise_flops,
+    clippy::inefficient_to_string,
+    clippy::invalid_upcast_comparisons,
+    clippy::large_types_passed_by_value,
+    clippy::let_unit_value,
+    clippy::linkedlist,
+    clippy::lossy_float_literal,
+    clippy::macro_use_imports,
+    clippy::manual_ok_or,
+    clippy::map_err_ignore,
+    clippy::map_flatten,
+    clippy::map_unwrap_or,
+    clippy::match_on_vec_items,
+    clippy::match_same_arms,
+    clippy::match_wildcard_for_single_variants,
+    clippy::mem_forget,
+    clippy::mismatched_target_os,
+    clippy::mut_mut,
+    clippy::mutex_integer,
+    clippy::needless_borrow,
+    clippy::needless_continue,
+    clippy::needless_pass_by_value,
+    clippy::option_option,
+    clippy::path_buf_push_overwrite,
+    clippy::ptr_as_ptr,
+    clippy::ref_option_ref,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::same_functions_in_if_condition,
+    clippy::semicolon_if_nothing_returned,
+    clippy::string_add_assign,
+    clippy::string_lit_as_bytes,
+    clippy::string_to_string,
+    clippy::todo,
+    clippy::trait_duplication_in_bounds,
+    clippy::unimplemented,
+    clippy::unnested_or_patterns,
+    clippy::unused_self,
+    clippy::useless_transmute,
+    clippy::use_self,
+    clippy::verbose_file_reads,
+    clippy::zero_sized_map_values,
+    future_incompatible,
+    nonstandard_style,
+    broken_intra_doc_links,
+    private_intra_doc_links,
+    missing_crate_level_docs,
+    rust_2018_idioms
+)]
+// END - Legion Labs standard lints v0.2
+// crate-specific exceptions:
+#![allow()]
+
+use legion::app::{ScheduleRunnerPlugin, ScheduleRunnerSettings};
+use legion::core::CorePlugin;
+use legion::prelude::*;
+use legion::utils::Duration;
+
+fn main() {
+    println!("Legion Labs runtime engine");
+
+    // Need a root entity (world) as a context
+
+    // Start app with 60 fps
+    App::new()
+        .insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(
+            1.0 / 60.0,
+        )))
+        .add_plugin(CorePlugin::default())
+        .add_plugin(ScheduleRunnerPlugin::default())
+        .add_plugin(TransformPlugin::default())
+        .add_system(frame_counter)
+        .run();
+}
+
+fn frame_counter(mut state: Local<'_, CounterState>) {
+    if state.count % 60 == 0 {
+        println!("{}", state.count / 60);
+    }
+    state.count += 1;
+}
+
+#[derive(Default)]
+struct CounterState {
+    count: u32,
+}
