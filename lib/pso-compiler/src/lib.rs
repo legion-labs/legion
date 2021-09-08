461ced9dd4196a928925696cb3be8b33ec91f9a6
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-09-08 00:48:16 -0400

    Change to supported dependency

--- lib/pso-compiler/src/lib.rs
+++ lib/pso-compiler/src/lib.rs
@@ -3,7 +3,7 @@ use hassle_rs::compile_hlsl;
 pub fn test_build() {
     let code = "
     Texture2D<float4> g_input    : register(t0, space0);
-    RWTexture2D<float4> g_output : register(u0, space0);
+    RWTexture2D<float4> g_output : register(u0, space1);
 
     [numthreads(8, 8, 1)]
     void copyCs(uint3 dispatchThreadId : SV_DispatchThreadID)
@@ -20,11 +20,8 @@ pub fn test_build() {
         &[("MY_DEFINE", Some("Value")), ("OTHER_DEFINE", None)],
     );
 
-    let module = spirv_reflect::ShaderModule::load_u8_data(&ir.unwrap()).expect("should work");
-    for i in module
-        .enumerate_descriptor_bindings(Some("copyCs"))
-        .expect("works")
-    {
+    let module = rspirv_reflect::Reflection::new_from_spirv(&ir.unwrap()).expect("should work");
+    for i in module.get_descriptor_sets().expect("works") {
         println!("{:?}", i);
     }
 }
