7d945fbec6884fbc67214dd0754b6e167aaa1a3e
mad-legion <mad@legionlabs.com>
2021-09-13 11:53:46 -0400

    get_tsc_frequency fails in wsl
    will need to calibrate the tsc frequency by reading the clock and tsc values and logging the results.

--- lib/telemetry/tests/test_log.rs
+++ lib/telemetry/tests/test_log.rs
@@ -26,12 +26,19 @@ fn type_name_of<T>(_: &T) -> &'static str {
 }
 
 fn get_tsc_frequency() -> Result<u64, String> {
+    // does not work in WSL
+    // more about the tsc frequency
+    //  https://stackoverflow.com/questions/35123379/getting-tsc-rate-from-x86-kernel
+    //  https://blog.trailofbits.com/2019/10/03/tsc-frequency-for-all-better-profiling-and-benchmarking/
+    //  https://stackoverflow.com/questions/51919219/determine-tsc-frequency-on-linux
     use raw_cpuid::CpuId;
     let cpuid = CpuId::new();
     let cpu_brand = cpuid
         .get_processor_brand_string()
         .map(|b| b.as_str().to_owned())
         .unwrap_or_else(|| "unknown".to_owned());
+
+    dbg!(cpuid);
 
     match cpuid.get_tsc_info() {
         Some(tsc_info) => match tsc_info.tsc_frequency() {
@@ -46,7 +53,7 @@ fn get_tsc_frequency() -> Result<u64, String> {
 }
 
 fn test_log_thread() {
-    println!("event frequency: {}", get_tsc_frequency().unwrap());
+    println!("TSC frequency: {}", get_tsc_frequency().unwrap_or_default());
     trace_scope!();
     let mut threads = Vec::new();
     for _ in 1..5 {
