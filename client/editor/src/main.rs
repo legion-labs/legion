74d8bb5e038383597f9656a84aabe514a1dba409
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-09-14 16:58:47 -0400

    UI Hello World

--- client/editor/src/main.rs
+++ client/editor/src/main.rs
@@ -1,7 +1,6 @@
-//! Minimalistic Sciter sample.
+use rust_embed::RustEmbed;
 
-static HTML: &[u8] = include_bytes!("../resources/main_window.htm");
-
+/*
 fn main() {
     legion_app::App::new()
         .insert_resource(legion_sciter::ToolWindowDescription {
@@ -15,15 +14,16 @@ fn main() {
         .run();
 }
 
-/*
+*/
+
 use sciter_js::{sciter, window};
 
-fn main() {
-    // Step 1: Include the 'minimal.html' file as a byte array.
-    // Hint: Take a look into 'minimal.html' which contains some tiscript code.
-    let html = include_bytes!("../resources/main_window.htm");
+#[derive(RustEmbed)]
+#[folder = "static/"]
+#[prefix = "this://app/"]
+struct StaticFolder;
 
-    // Step 2: Enable the features we need in our tiscript code.
+fn main() {
     sciter::set_global_options(sciter::GlobalOption::ScriptRuntimeFeatures(
         sciter::ScriptRuntimeFeatures::ALLOW_FILE_IO // Enables `Sciter.machineName()`.  Required for opening file dialog (`view.selectFile()`)
          | sciter::ScriptRuntimeFeatures::ALLOW_SYSINFO, // Enables opening file dialog (`view.selectFile()`)
@@ -31,23 +31,13 @@ fn main() {
     .unwrap();
 
     sciter::set_global_options(sciter::GlobalOption::UxTheming(true)).unwrap();
-
-    // Enable debug mode for all windows, so that we can inspect them via Inspector.
     sciter::set_global_options(sciter::GlobalOption::DebugMode(true)).unwrap();
 
-    // Step 3: Create a new main sciter window of type `sciter::Window`.
-    // Hint: The sciter Window wrapper (src/window.rs) contains more
-    // interesting functions to open or attach to another existing window.
     let mut window = window::WindowBuilder::main().build();
 
-    // Step 4: Load HTML byte array from memory to `sciter::Window`.
-    // Hint: second parameter is an optional uri, it can be `None` in simple cases,
-    // but it is useful for debugging purposes (check the Inspector tool from the Sciter SDK).
-    // Also you can use a `load_file` method, but it requires an absolute path
-    // of the main document to resolve HTML resources properly.
-    window.load_html(html, Some("example://details-summary.htm"));
+    window.set_embedded_source::<StaticFolder>();
+    window.load_file("this://app/main.htm");
 
-    // Step 5: Show window and run the main app message loop until window been closed.
-    window.event_loop();
+    window.show();
+    window::run_event_loop(|| {});
 }
-*/
