76c9ca76ad7cc59f8b5227e34fb02965ecb5d33a
Julien Kauffmann <julien.kauffmann@legionlabs.com>
2021-09-15 14:34:52 -0400

    Removed test code from the editor server

--- server/editor-srv/src/main.rs
+++ server/editor-srv/src/main.rs
@@ -1,16 +1,7 @@
 use std::time::Duration;
 
 use legion_app::{prelude::*, ScheduleRunnerPlugin, ScheduleRunnerSettings};
-use legion_async::{AsyncOperation, AsyncPlugin, TokioAsyncRuntime};
 use legion_ecs::prelude::*;
-
-struct Dropper {}
-
-impl Drop for Dropper {
-    fn drop(&mut self) {
-        println!("I was dropped.")
-    }
-}
 
 fn main() {
     App::new()
@@ -18,26 +9,7 @@ fn main() {
             1.0 / 60.0,
         )))
         .add_plugin(ScheduleRunnerPlugin::default())
-        .add_plugin(AsyncPlugin {})
-        .add_startup_system(
-            |mut commands: Commands, mut rt: ResMut<TokioAsyncRuntime>| {
-                commands.spawn().insert(Salesman {
-                    get_price: rt.start(async {
-                        println!("Sleeping for one second...");
-                        tokio::time::sleep(Duration::from_secs(1)).await;
-                        42
-                    }),
-                    get_other_price: rt.start(async {
-                        let _ = &Dropper {};
-                        println!("Sleeping for three seconds...");
-                        tokio::time::sleep(Duration::from_secs(3)).await;
-                        1234
-                    }),
-                });
-            },
-        )
         .add_system(frame_counter)
-        .add_system(online_loop_example)
         .run();
 }
 
@@ -46,31 +18,6 @@ fn frame_counter(mut state: Local<'_, CounterState>) {
         println!("{}", state.count / 60);
     }
     state.count += 1;
-}
-
-struct Salesman {
-    get_price: AsyncOperation<u32>,
-    get_other_price: AsyncOperation<u32>,
-}
-
-fn online_loop_example(callers: Query<&Salesman>) {
-    for caller in callers.iter() {
-        if let Some(v) = caller.get_price.take_result() {
-            match v {
-                Ok(v) => {
-                    println!("The price is: {:?}", v);
-                    caller.get_other_price.cancel();
-                }
-                Err(e) => println!("Could not fetch the price: {}", e),
-            };
-        }
-        if let Some(v) = caller.get_other_price.take_result() {
-            match v {
-                Ok(v) => println!("The other price is: {:?}", v),
-                Err(e) => println!("Could not fetch the other price: {}", e),
-            };
-        }
-    }
 }
 
 #[derive(Default)]
