7ef50320fbd69b69cfdc452c3ae638e836ee9667
Julien Kauffmann <julien.kauffmann@legionlabs.com>
2021-09-15 11:41:50 -0400

    Implemented async cancellation semantics

--- plugin/async/src/operation.rs
+++ plugin/async/src/operation.rs
@@ -1,27 +1,58 @@
 use std::{
     error::Error,
+    fmt,
     sync::{Arc, Mutex},
 };
 
 // Represents an async operation result.
 //
 // A lack of value indicates that the associated AsyncOperation is not done yet.
-pub type AsyncOperationResult<T> = Option<Result<T, Box<dyn Error + Send + Sync>>>;
+pub type AsyncOperationResult<T> = Option<Result<T, AsyncOperationError>>;
+
+// An error that can happen to an AsyncOperation.
+#[derive(Debug)]
+pub enum AsyncOperationError {
+    Cancelled,
+    Dropped,
+}
+
+impl fmt::Display for AsyncOperationError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Self::Cancelled => write!(f, "the asynchronous operation was cancelled"),
+            Self::Dropped => write!(f, "the asynchronous operation was dropped"),
+        }
+    }
+}
+
+impl Error for AsyncOperationError {}
+
+pub(crate) trait AsyncOperationCanceller: Send + Sync {
+    fn cancel(&self);
+}
 
 // Represents an async operation running in a separate thread pool, that can be
 // polled for completion.
 pub struct AsyncOperation<T> {
     result: Arc<Mutex<AsyncOperationResult<T>>>,
+    canceller: Box<dyn AsyncOperationCanceller>,
 }
 
 impl<T> AsyncOperation<T> {
-    pub fn new(result: Arc<Mutex<AsyncOperationResult<T>>>) -> Self {
-        Self { result }
+    pub(crate) fn new(
+        result: Arc<Mutex<AsyncOperationResult<T>>>,
+        canceller: Box<dyn AsyncOperationCanceller>,
+    ) -> Self {
+        Self { result, canceller }
     }
 }
 
 impl<T: Send + 'static> AsyncOperation<T> {
-    pub fn take_result(&self) -> Option<Result<T, Box<dyn Error + Send + Sync>>> {
+    pub fn take_result(&self) -> AsyncOperationResult<T> {
         self.result.lock().unwrap().take()
+    }
+
+    pub fn cancel(&self) {
+        self.canceller.cancel();
     }
 }
