00e1caa1cbe46b6cd743b21ecd9e96b61185bf44
mad-legion <mad@legionlabs.com>
2021-09-15 16:56:41 -0400

    reflection prototype
    #148

---
+++ lib/derive-transit/tests/test_reflect.rs
@@ -0,0 +1,23 @@
+use derive_transit::*;
+use transit::*;
+
+#[derive(Reflect)]
+#[allow(dead_code)]
+struct MyTestEvent {
+    some_64: u64,
+    some_32: u32,
+}
+
+#[test]
+fn test_reflect_simple_struct() {
+    let res = MyTestEvent::reflect();
+    assert_eq!("MyTestEvent", res.name);
+    assert_eq!(16, res.size);
+    assert_eq!(2, res.members.len());
+    assert_eq!("some_64", res.members[0].name);
+    assert_eq!(8, res.members[0].size);
+    assert_eq!(0, res.members[0].offset);
+    assert_eq!("some_32", res.members[1].name);
+    assert_eq!(4, res.members[1].size);
+    assert_eq!(8, res.members[1].offset);
+}
