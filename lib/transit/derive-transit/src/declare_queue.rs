f88a1f57cc667b88c947d66f3e38ced0b2022f84
mad-legion <mad@legionlabs.com>
2021-09-17 15:55:01 -0400

    push events in queue

--- lib/transit/derive-transit/src/declare_queue.rs
+++ lib/transit/derive-transit/src/declare_queue.rs
@@ -7,12 +7,22 @@ pub fn declare_queue_impl(input: TokenStream) -> TokenStream {
     let ast = parse::<DeriveInput>(input).unwrap();
     let struct_identifier = ast.ident.clone();
 
+    let mut value_tupe_counter: u8 = 0;
     let push_methods = ast.generics.params.iter().map(|p| match p {
         GenericParam::Type(t) => {
-            let snake_type = t.ident.to_string().to_case(Case::Snake);
+            let index = value_tupe_counter;
+            value_tupe_counter += 1;
+            let value_type_id = &t.ident;
+            let snake_type = value_type_id.to_string().to_case(Case::Snake);
             let push_id = format_ident!("push_{}", snake_type);
             quote! {
-                pub fn #push_id( &self, value: #t ){
+                pub fn #push_id( &mut self, value: #t ){
+                    let ptr = std::ptr::addr_of!(value) as *const u8;
+                    let slice = std::ptr::slice_from_raw_parts(ptr, std::mem::size_of::<#value_type_id>());
+                    self.buffer.push(#index);
+                    unsafe{
+                        self.buffer.extend_from_slice( &*slice );
+                    }
                 }
             }
         }
@@ -30,6 +40,10 @@ pub fn declare_queue_impl(input: TokenStream) -> TokenStream {
                 let mut buffer: Vec<u8> = Vec::new();
                 buffer.reserve(buffer_size);
                 Self { buffer }
+            }
+
+            pub fn len_bytes(&self) -> usize{
+                self.buffer.len()
             }
 
             #(#push_methods)*
--- lib/transit/tests/test_queue.rs
+++ lib/transit/tests/test_queue.rs
@@ -1,12 +1,12 @@
 use transit::*;
 
-#[derive(TransitReflect)]
+#[derive(TransitReflect, Debug)]
 struct MyTestEvent {
     some_64: u64,
     some_32: u32,
 }
 
-#[derive(TransitReflect)]
+#[derive(TransitReflect, Debug)]
 struct OtherEvent {
     some_64: u64,
 }
@@ -17,10 +17,12 @@ declare_queue_struct!(
 
 #[test]
 fn test_queue() {
-    let q = MyQueue::new(1024);
+    let mut q = MyQueue::new(1024);
     q.push_my_test_event(MyTestEvent {
-        some_64: 0,
+        some_64: 2,
         some_32: 1,
     });
-    q.push_other_event(OtherEvent { some_64: 0 });
+    assert_eq!(17, q.len_bytes());
+    q.push_other_event(OtherEvent { some_64: 3 });
+    assert_eq!(26, q.len_bytes());
 }
