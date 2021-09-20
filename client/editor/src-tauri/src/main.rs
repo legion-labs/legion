a85fce5f6a0cba8cab7d6882d11932e66693fb49
Julien Kauffmann <julien.kauffmann@legionlabs.com>
2021-09-20 16:36:43 -0400

    Added basic Tauri binding code

--- client/editor/src-tauri/src/main.rs
+++ client/editor/src-tauri/src/main.rs
@@ -3,15 +3,51 @@
   windows_subsystem = "windows"
 )]
 
-fn main() {
+use std::error::Error;
+use tokio::sync::Mutex;
+
+use legion_editor_proto::{editor_client::*, InitializeStreamRequest};
+use tonic::transport::Channel;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+  let client = Mutex::new(EditorClient::connect("http://[::1]:50051").await?);
+
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![my_custom_command])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .manage(client)
+    .invoke_handler(tauri::generate_handler![initialize_stream])
+    .run(tauri::generate_context!())?;
+
+  Ok(())
 }
 
 #[tauri::command]
-fn my_custom_command() -> String {
-  println!("I was called from JS!");
-  "foo".into()
+async fn initialize_stream(
+  client: tauri::State<'_, Mutex<EditorClient<Channel>>>,
+  rtc_session_description: String,
+) -> Result<String, String> {
+  let mut client = client.lock().await;
+
+  match initialize_stream_impl(&mut client, rtc_session_description).await {
+    Ok(rtc_session_description) => Ok(rtc_session_description),
+    Err(e) => Err(format!("{}", e)),
+  }
+}
+
+async fn initialize_stream_impl(
+  client: &mut EditorClient<Channel>,
+  rtc_session_description: String,
+) -> Result<String, Box<dyn Error>> {
+  let rtc_session_description = base64::decode(rtc_session_description)?;
+  let request = tonic::Request::new(InitializeStreamRequest {
+    rtc_session_description,
+  });
+
+  let response = client.initialize_stream(request).await?.into_inner();
+
+  if response.error.is_empty() {
+    Ok(base64::encode(response.rtc_session_description))
+  } else {
+    Err(response.error.into())
+  }
 }
