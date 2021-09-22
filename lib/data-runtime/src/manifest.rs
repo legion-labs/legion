aeba07dee6d4a4364c721d0ad1b01a095400130b
mparenteau-legion <marc.parenteau@legionlabs.com>
2021-09-22 15:56:44 -0400

    sort manifest entries when serializing

--- lib/data-runtime/src/manifest.rs
+++ lib/data-runtime/src/manifest.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 /// Description of a compiled asset.
+#[derive(Serialize, Deserialize)]
 pub struct CompiledAsset {
     /// The id of the asset.
     pub guid: AssetId,
@@ -15,7 +16,7 @@ pub struct CompiledAsset {
 }
 
 /// `Manifest` contains storage information about assets - their checksums and sizes.
-#[derive(Debug, Serialize, Deserialize, Default)]
+#[derive(Debug, Default)]
 pub struct Manifest(HashMap<AssetId, (i128, usize)>);
 
 impl Manifest {
@@ -27,5 +28,37 @@ impl Manifest {
     /// Add new information about an `Asset`.
     pub fn insert(&mut self, id: AssetId, checksum: i128, size: usize) {
         self.0.insert(id, (checksum, size));
+    }
+}
+
+impl Serialize for Manifest {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut entries: Vec<CompiledAsset> = Vec::new();
+        for (guid, (checksum, size)) in self.0.iter() {
+            entries.push(CompiledAsset {
+                guid: *guid,
+                checksum: *checksum,
+                size: *size,
+            });
+        }
+        entries.sort_by(|a, b| a.guid.partial_cmp(&b.guid).unwrap());
+        entries.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for Manifest {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let entries = Vec::<CompiledAsset>::deserialize(deserializer)?;
+        let mut manifest = Self::default();
+        for asset in entries {
+            manifest.0.insert(asset.guid, (asset.checksum, asset.size));
+        }
+        Ok(manifest)
     }
 }
