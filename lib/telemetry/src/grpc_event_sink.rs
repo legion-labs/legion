67bdece561b646fb6d9749f015605d7d8a1a8299
mad-legion <mad@legionlabs.com>
2021-09-24 16:05:22 -0400

    move grpc event sink in telemetry lib
    clippy warning about needless call by value

---
+++ lib/telemetry/src/grpc_event_sink.rs
@@ -0,0 +1,78 @@
+use crate::telemetry_ingestion_proto::telemetry_ingestion_client::TelemetryIngestionClient;
+use crate::{EventBlockSink, TelemetrySinkEvent};
+
+pub struct GRPCEventSink {
+    thread: Option<std::thread::JoinHandle<()>>,
+    sender: std::sync::mpsc::Sender<TelemetrySinkEvent>,
+}
+
+impl Drop for GRPCEventSink {
+    fn drop(&mut self) {
+        if let Some(handle) = self.thread.take() {
+            handle.join().expect("Error joining telemetry thread");
+        }
+    }
+}
+
+impl GRPCEventSink {
+    pub fn new(addr_server: &str) -> Self {
+        let addr = addr_server.to_owned();
+        let (sender, receiver) = std::sync::mpsc::channel::<TelemetrySinkEvent>();
+        Self {
+            thread: Some(std::thread::spawn(move || {
+                Self::thread_proc(addr, receiver);
+            })),
+            sender,
+        }
+    }
+
+    #[allow(clippy::needless_pass_by_value)] // we don't want to leave the receiver in the calling thread
+    fn thread_proc(addr: String, receiver: std::sync::mpsc::Receiver<TelemetrySinkEvent>) {
+        let tokio_runtime = tokio::runtime::Runtime::new().unwrap();
+        let mut client = match tokio_runtime.block_on(TelemetryIngestionClient::connect(addr)) {
+            Ok(c) => c,
+            Err(e) => {
+                println!("Error connecting to telemetry server: {}", e);
+                return;
+            }
+        };
+
+        loop {
+            match receiver.recv() {
+                Ok(message) => match message {
+                    TelemetrySinkEvent::OnInitProcess(process_info) => {
+                        match tokio_runtime.block_on(client.insert_process(process_info)) {
+                            Ok(response) => {
+                                dbg!(response);
+                            }
+                            Err(e) => {
+                                println!("insert_process failed: {}", e);
+                            }
+                        }
+                    }
+                    TelemetrySinkEvent::OnShutdown => {
+                        return;
+                    }
+                    TelemetrySinkEvent::OnLogBufferFull(log_buffer) => {
+                        dbg!(log_buffer);
+                    }
+                    TelemetrySinkEvent::OnThreadBufferFull(thread_buffer) => {
+                        dbg!(thread_buffer);
+                    }
+                },
+                Err(e) => {
+                    println!("Error in telemetry thread: {}", e);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+impl EventBlockSink for GRPCEventSink {
+    fn on_sink_event(&self, event: TelemetrySinkEvent) {
+        if let Err(e) = self.sender.send(event) {
+            dbg!(e);
+        }
+    }
+}
--- server/telemetry-ingestion-srv/test/generator.rs
+++ server/telemetry-ingestion-srv/test/generator.rs
@@ -1,84 +1,8 @@
 use std::sync::Arc;
 use telemetry::*;
-use telemetry_ingestion_proto::telemetry_ingestion_client::TelemetryIngestionClient;
-
-struct GRPCEventSink {
-    thread: Option<std::thread::JoinHandle<()>>,
-    sender: std::sync::mpsc::Sender<TelemetrySinkEvent>,
-}
-
-impl Drop for GRPCEventSink {
-    fn drop(&mut self) {
-        if let Some(handle) = self.thread.take() {
-            handle.join().expect("Error joining telemetry thread");
-        }
-    }
-}
-
-impl GRPCEventSink {
-    fn thread_proc(addr: String, receiver: std::sync::mpsc::Receiver<TelemetrySinkEvent>) {
-        let tokio_runtime = tokio::runtime::Runtime::new().unwrap();
-        let mut client = match tokio_runtime.block_on(TelemetryIngestionClient::connect(addr)) {
-            Ok(c) => c,
-            Err(e) => {
-                println!("Error connecting to telemetry server: {}", e);
-                return;
-            }
-        };
-
-        loop {
-            match receiver.recv() {
-                Ok(message) => match message {
-                    TelemetrySinkEvent::OnInitProcess(process_info) => {
-                        match tokio_runtime.block_on(client.insert_process(process_info)) {
-                            Ok(response) => {
-                                dbg!(response);
-                            }
-                            Err(e) => {
-                                println!("insert_process failed: {}", e);
-                            }
-                        }
-                    }
-                    TelemetrySinkEvent::OnShutdown => {
-                        return;
-                    }
-                    TelemetrySinkEvent::OnLogBufferFull(log_buffer) => {
-                        dbg!(log_buffer);
-                    }
-                    TelemetrySinkEvent::OnThreadBufferFull(thread_buffer) => {
-                        dbg!(thread_buffer);
-                    }
-                },
-                Err(e) => {
-                    println!("Error in telemetry thread: {}", e);
-                    return;
-                }
-            }
-        }
-    }
-}
-
-pub fn make_telemetry_connection(addr_server: &str) -> Arc<dyn EventBlockSink> {
-    let addr = addr_server.to_owned();
-    let (sender, receiver) = std::sync::mpsc::channel::<TelemetrySinkEvent>();
-    Arc::new(GRPCEventSink {
-        thread: Some(std::thread::spawn(move || {
-            GRPCEventSink::thread_proc(addr, receiver)
-        })),
-        sender,
-    })
-}
-
-impl EventBlockSink for GRPCEventSink {
-    fn on_sink_event(&self, event: TelemetrySinkEvent) {
-        if let Err(e) = self.sender.send(event) {
-            dbg!(e);
-        }
-    }
-}
 
 fn init_telemetry() {
-    let sink = make_telemetry_connection("http://127.0.0.1:8080");
+    let sink = Arc::new(GRPCEventSink::new("http://127.0.0.1:8080"));
     init_event_dispatch(1024, 1024 * 1024, sink).unwrap();
 }
 
