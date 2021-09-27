a029352ba4f28d76ed742f4877864ab78f75d8b8
Julien Kauffmann <julien.kauffmann@legionlabs.com>
2021-09-27 12:56:17 -0400

    Fixed build.rs incorrectly not reporting build process errors

--- client/editor/src/build.rs
+++ client/editor/src/build.rs
@@ -5,18 +5,13 @@ fn build_web_app() {
 
   let yarn_path = which("yarn").unwrap();
 
-  Command::new(yarn_path)
+  let mut process = Command::new(yarn_path)
     .arg("generate")
     .current_dir("frontend")
     .spawn()
     .unwrap();
-}
 
-#[cfg(not(feature = "custom-protocol"))]
-fn fake_build_web_app() {
-  use std::fs::create_dir_all;
-
-  create_dir_all("frontend/dist");
+  std::process::exit(process.wait().unwrap().code().unwrap());
 }
 
 fn main() {
@@ -24,8 +19,6 @@ fn main() {
 
   #[cfg(feature = "custom-protocol")]
   build_web_app();
-  #[cfg(not(feature = "custom-protocol"))]
-  fake_build_web_app();
 
   tauri_build::build()
 }
