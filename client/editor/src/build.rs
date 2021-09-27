800a882c2dc33eab863155aecabcc2aeb99cfaa4
Julien Kauffmann <julien.kauffmann@legionlabs.com>
2021-09-27 12:07:57 -0400

    Added code to fake create the dist directory

--- client/editor/src/build.rs
+++ client/editor/src/build.rs
@@ -12,11 +12,20 @@ fn build_web_app() {
     .unwrap();
 }
 
+#[cfg(not(feature = "custom-protocol"))]
+fn fake_build_web_app() {
+  use std::fs::create_dir_all;
+
+  create_dir_all("frontend/dist");
+}
+
 fn main() {
   println!("cargo:rerun-if-changed=frontend/dist");
 
   #[cfg(feature = "custom-protocol")]
   build_web_app();
+  #[cfg(not(feature = "custom-protocol"))]
+  fake_build_web_app();
 
   tauri_build::build()
 }
