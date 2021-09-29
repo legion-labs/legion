b68b376697defa9dc8c3f0e990ac816ecb45fe7d
mad-legion <mad@legionlabs.com>
2021-09-29 10:07:13 -0400

    create streams table, insert process date

--- server/telemetry-ingestion-srv/src/local_telemetry_db.rs
+++ server/telemetry-ingestion-srv/src/local_telemetry_db.rs
@@ -10,10 +10,10 @@ pub fn get_data_directory() -> Result<PathBuf> {
     Ok(PathBuf::from(folder))
 }
 
-async fn create_tables(connection: &mut sqlx::AnyConnection) -> Result<()> {
+async fn create_processes_tables(connection: &mut sqlx::AnyConnection) -> Result<()> {
     let sql = "
          CREATE TABLE processes(
-                  id VARCHAR(36), 
+                  process_id VARCHAR(36), 
                   exe VARCHAR(255), 
                   username VARCHAR(255), 
                   realname VARCHAR(255), 
@@ -21,13 +21,39 @@ async fn create_tables(connection: &mut sqlx::AnyConnection) -> Result<()> {
                   distro VARCHAR(255), 
                   cpu_brand VARCHAR(255), 
                   tsc_frequency BIGINT,
-                  start_time VARCHAR(255));
-         CREATE UNIQUE INDEX process_id on processes(id);";
+                  start_time VARCHAR(255),
+                  insert_date DATE);
+         CREATE UNIQUE INDEX process_id on processes(process_id);
+         CREATE INDEX process_insert_date on processes(insert_date);";
     sqlx::query(sql)
         .execute(connection)
         .await
         .with_context(|| String::from("Creating table processes and its indices"))?;
     Ok(())
+}
+
+async fn create_streams_tables(connection: &mut sqlx::AnyConnection) -> Result<()> {
+    // storing tags as text is simplistic - we should move to a tags table if we keep the telemetry metadata in a SQL db
+    let sql = "
+         CREATE TABLE streams(
+                  stream_id VARCHAR(36), 
+                  process_id VARCHAR(36), 
+                  dependencies_metadata BLOB,
+                  objects_metadata BLOB,
+                  tags TEXT
+                  );
+         CREATE UNIQUE INDEX stream_id on streams(stream_id);
+         CREATE INDEX stream_process_id on streams(process_id);";
+    sqlx::query(sql)
+        .execute(connection)
+        .await
+        .with_context(|| String::from("Creating table streams and its indices"))?;
+    Ok(())
+}
+
+async fn create_tables(connection: &mut sqlx::AnyConnection) -> Result<()> {
+    create_processes_tables(connection).await?;
+    create_streams_tables(connection).await
 }
 
 pub async fn alloc_sql_pool(data_folder: &Path) -> Result<sqlx::AnyPool> {
