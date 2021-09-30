c7ea25c19285fab2698201d908214bc6eb1bbe01
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-09-29 23:21:17 -0400

    Add Window plugin

---
+++ plugin/window/src/system.rs
@@ -0,0 +1,11 @@
+use crate::WindowCloseRequested;
+use legion_app::{AppExit, EventReader, EventWriter};
+
+pub fn exit_on_window_close_system(
+    mut app_exit_events: EventWriter<'_, AppExit>,
+    mut window_close_requested_events: EventReader<'_, WindowCloseRequested>,
+) {
+    if window_close_requested_events.iter().next().is_some() {
+        app_exit_events.send(AppExit);
+    }
+}
