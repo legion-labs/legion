e9296022e582deaa8c7c469693d53864bb537b6b
mad-legion <mad@legionlabs.com>
2021-09-30 14:33:16 -0400

    query process list

--- client/telemetry-dump/tests/test_local_telemetry.rs
+++ client/telemetry-dump/tests/test_local_telemetry.rs
@@ -1,3 +1,5 @@
+use anyhow::{Context, Result};
+use sqlx::Row;
 use std::path::{Path, PathBuf};
 use test_utils::*;
 
@@ -11,9 +13,36 @@ fn test_dir(test_name: &str) -> PathBuf {
     create_test_dir(&parent, test_name)
 }
 
+pub async fn alloc_sql_pool(data_folder: &Path) -> Result<sqlx::AnyPool> {
+    let db_uri = format!("sqlite://{}/telemetry.db3", data_folder.display());
+    let pool = sqlx::any::AnyPoolOptions::new()
+        .connect(&db_uri)
+        .await
+        .with_context(|| String::from("Connecting to telemetry database"))?;
+    Ok(pool)
+}
+
+#[tokio::main]
 #[test]
-fn test_local_dump() {
+async fn test_list_processes() {
     let src_dir = std::env::current_dir().unwrap().join("tests/data");
     let test_output = test_dir("list-processes");
     fs_extra::dir::copy(&src_dir, &test_output, &fs_extra::dir::CopyOptions::new()).unwrap();
+
+    let data_path = test_output.join("data");
+    let pool = alloc_sql_pool(&data_path).await.unwrap();
+    let mut connection = pool.acquire().await.unwrap();
+    let rows = sqlx::query(
+        "SELECT process_id, exe, start_time
+         FROM processes",
+    )
+    .fetch_all(&mut connection)
+    .await
+    .unwrap();
+    for r in rows {
+        let process_id: String = r.get("process_id");
+        let exe: String = r.get("exe");
+        let start_time: String = r.get("start_time");
+        println!("{} {} {}", process_id, exe, start_time);
+    }
 }
