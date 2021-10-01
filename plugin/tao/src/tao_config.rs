cce38f608776de45b7cc12fca6196a6a8c5653c9
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-10-01 01:58:39 -0400

    Fix documentation on linux (feature not available on linux...)

--- plugin/tao/src/tao_config.rs
+++ plugin/tao/src/tao_config.rs
@@ -4,8 +4,17 @@ pub struct WinitConfig {
     /// Configures the tao library to return control to the main thread after
     /// the [run](legion_app::App::run) loop is exited. Winit strongly recommends
     /// avoiding this when possible. Before using this please read and understand
-    /// the [caveats](tao::platform::run_return::EventLoopExtRunReturn::run_return)
-    /// in the tao documentation.
+    /// the following:
+    ///
+    /// # Caveats
+    /// return from run, despite its appearance at first glance, this is *not* a perfect replacement for
+    /// `poll_events`. For example, this function will not return on Windows or macOS while a
+    /// window is getting resized, resulting in all application logic outside of the
+    /// `event_handler` closure not running until the resize operation ends. Other OS operations
+    /// may also result in such freezes. This behavior is caused by fundamental limitations in the
+    /// underlying OS APIs, which cannot be hidden by `tao` without severe stability repercussions.
+    ///
+    /// You are strongly encouraged to use `run`, unless the use of this is absolutely necessary.
     ///
     /// This feature is only available on desktop `target_os` configurations.
     /// Namely `windows`, `macos`, `linux`, `dragonfly`, `freebsd`, `netbsd`, and
