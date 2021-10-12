8019eeb79a25a18ab16a27bf732f67c11ad5457b
Kris Stasik <kris.stasik@legionlabs.com>
2021-10-12 11:52:15 -0400

    Add ResourceId<->ResourcePathId utility to data-scrape

--- client/data-build-cli/src/scrape.rs
+++ client/data-build-cli/src/scrape.rs
@@ -2,9 +2,15 @@
 //!
 //! Provides functionalities that help diagnose project's data.
 //!
-//! # Resource Type Tool - `rty`
+//! The diagnostic tools in this binary include:
+//! * **rty** - `ResourceType` <-> String lookup utility.
+//! * **source** - `ResourceId` <-> `ResourcePathName` lookup utility.
+//!
+//! # `rty` - Resource Type Tool
 //!
 //! Various commands to help identify resource types known under a specified sourcecode directory.
+//!
+//! It helps with mapping between `Resource::TYPENAME` and `Resource::TYPE`.
 //!
 //! ### List resource types
 //!
@@ -34,6 +40,40 @@
 //! ```text
 //! $ data-scrape rty lib\ decode runtime_texture
 //! runtime_texture = f9c9670d
+//! ```
+//!
+//! # `source` - Resource Name Tool
+//!
+//! Various commands to help translate between source resource's pathname and its id.
+//!
+//! It helps with mapping between `ResourceId` and `ResourcePathName` of a **source resource**.
+//!
+//! ### List resources
+//!
+//! List all **source resources** under a specified project:
+//!
+//! ```text
+//! $ data-scrape source .\test\sample_data\ list
+//! /image/ground.psd = 13b5a84e00000000d9e5871a48bd55c5
+//! /world/sample_1/ground.mat = 2b368fed000000004e9d6dcb039451e3
+//! /world/sample_1/cube_3.ent.ins = 417862e50000000054537dedac8437c4
+//! ...
+//! ```
+//!
+//! ### Find id of a resource
+//!
+//! Show the id of the resource under a specified pathname.
+//!
+//! ```text
+//! $ data-scrape source .\test\sample_data\ id /world/sample_1/cube_1.ent.ins
+//! /world/sample_1/cube_1.ent.ins = 417862e500000000e9c81a578a265cda
+//! ```
+//!
+//! ### Find pathname of specified resource id.
+//!
+//! ```text
+//! $ data-scrape source .\test\sample_data\ name 417862e500000000e321168f3653db42
+//! /prefab/props/cube_group_cube_1.ent.ins = 417862e500000000e321168f3653db42
 //! ```
 
 // BEGIN - Legion Labs lints v0.5
@@ -91,12 +131,17 @@
 // crate-specific exceptions:
 #![allow()]
 
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 use clap::{AppSettings, Arg, SubCommand};
-use legion_data_runtime::ResourceType;
+use legion_data_offline::resource::{Project, ResourcePathName};
+use legion_data_runtime::{ResourceId, ResourceType};
 
-fn main() {
+#[allow(clippy::too_many_lines)]
+fn main() -> Result<(), String> {
     let matches = clap::App::new("Data Scraper")
         .setting(AppSettings::ArgRequiredElseHelp)
         .version(env!("CARGO_PKG_VERSION"))
@@ -123,6 +168,30 @@ fn main() {
                                 .required(true),
                         )
                         .about("Decodes hash value of resource type to human readable name"),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("source")
+                .about("Parse project index for source resource information")
+                .arg(Arg::with_name("path").help("Path to directory containing the project"))
+                .subcommand(SubCommand::with_name("list"))
+                .subcommand(
+                    SubCommand::with_name("name")
+                        .arg(
+                            Arg::with_name("id")
+                                .help("ResourceId to find a name of")
+                                .required(true),
+                        )
+                        .help("Finds a name of a given ResourceId"),
+                )
+                .subcommand(
+                    SubCommand::with_name("id")
+                        .arg(
+                            Arg::with_name("name")
+                                .help("ResourcePathName to find id of")
+                                .required(true),
+                        )
+                        .help("Find the id of a given ResourcePathName"),
                 ),
         )
         .get_matches();
@@ -161,7 +230,44 @@ fn main() {
                 println!("{}", cmd_args.usage());
             }
         }
+    } else if let ("source", Some(cmd_args)) = matches.subcommand() {
+        let proj_file = cmd_args
+            .value_of("path")
+            .map_or_else(|| std::env::current_dir().unwrap(), PathBuf::from);
+
+        let project = Project::open(proj_file).map_err(|e| e.to_string())?;
+
+        match cmd_args.subcommand() {
+            ("list", _) => {
+                for id in project.resource_list() {
+                    let name = project.resource_name(id).map_err(|e| e.to_string())?;
+                    println!("{} = {}", name, id);
+                }
+            }
+            ("name", Some(cmd_args)) => {
+                let id = cmd_args.value_of("id").unwrap();
+                let id = ResourceId::from_str(id).map_err(|e| e.to_string())?;
+                if let Ok(name) = project.resource_name(id) {
+                    println!("{} = {}", name, id);
+                } else {
+                    println!("None");
+                }
+            }
+            ("id", Some(cmd_args)) => {
+                let name = cmd_args.value_of("name").unwrap();
+                let name = ResourcePathName::from(name);
+                if let Ok(id) = project.find_resource(&name) {
+                    println!("{} = {}", name, id);
+                } else {
+                    println!("None");
+                }
+            }
+            _ => {
+                println!("{}", cmd_args.usage());
+            }
+        }
     }
+    Ok(())
 }
 
 struct ResourceTypeIterator {
