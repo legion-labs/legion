2b0b78965c66aaa7810f5dd4ea2529950fd2b472
mad-legion <mad@legionlabs.com>
2021-10-18 13:42:20 -0400

    send and record the frame_id of every chunk

--- client/editor/src/main.rs
+++ client/editor/src/main.rs
@@ -105,6 +105,17 @@ fn on_video_close() {
     flush_metrics_buffer();
 }
 
+fn record_json_metric(desc: &'static MetricDesc, value: &json::JsonValue) {
+    match value.as_i64() {
+        Some(int_value) => {
+            record_int_metric(desc, int_value as u64);
+        }
+        None => {
+            log::error!("Error converting {} to i64", value);
+        }
+    }
+}
+
 #[tauri::command]
 fn on_video_chunk_received(chunk_header: &str) {
     static CHUNK_INDEX_IN_FRAME_METRIC: MetricDesc = MetricDesc {
@@ -112,15 +123,19 @@ fn on_video_chunk_received(chunk_header: &str) {
         unit: "",
     };
 
+    static FRAME_ID_OF_CHUNK_RECEIVED: MetricDesc = MetricDesc {
+        name: "Frame ID of chunk received",
+        unit: "",
+    };
+
     match json::parse(chunk_header) {
-        Ok(header) => match header["chunk_index_in_frame"].as_i64() {
-            Some(chunk_index) => {
-                record_int_metric(&CHUNK_INDEX_IN_FRAME_METRIC, chunk_index as u64);
-            }
-            None => {
-                log::error!("chunk_index_in_frame not found in chunk header");
-            }
-        },
+        Ok(header) => {
+            record_json_metric(
+                &CHUNK_INDEX_IN_FRAME_METRIC,
+                &header["chunk_index_in_frame"],
+            );
+            record_json_metric(&FRAME_ID_OF_CHUNK_RECEIVED, &header["frame_id"]);
+        }
         Err(e) => {
             log::error!("Error parsing chunk header: {}", e);
         }
