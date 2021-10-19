e96b9e311a709794f9f0ad525a90ad9db151f7ed
Jalal El Mansouri <84198762+jelmansouri-legion@users.noreply.github.com>
2021-10-19 09:23:26 -0400

    Codegen suggestion (#404)
    * Small refactoring using a macro
    
    * Add all files
    
    * Minor changes
    
    * Fix loop

--- lib/data-codegen/src/lib.rs
+++ lib/data-codegen/src/lib.rs
@@ -132,3 +132,31 @@ pub fn generate_data_container_code(
 
     Ok(())
 }
+
+/// Helper function to be used in build.rs files to generate the proper
+/// binding to be included by crates
+///
+/// # Errors
+/// Returns `Err` if the data is format is not compliant
+#[macro_export]
+macro_rules! data_container_gen {
+    ( $( $x:expr ),* ) => {
+        let package_path = env!("CARGO_MANIFEST_DIR").to_lowercase();
+        $(
+            let mut data_path = package_path.replace("_runtime", "_offline");
+            data_path.push_str($x);
+            if package_path.ends_with("_offline") {
+                legion_data_codegen::generate_data_container_code(
+                    std::path::Path::new(&data_path),
+                    &legion_data_codegen::GenerationType::OfflineFormat,
+                ).expect("Offline data codegen failed");
+            } else if package_path.ends_with("_runtime") {
+                legion_data_codegen::generate_data_container_code(
+                    std::path::Path::new(&data_path),
+                    &legion_data_codegen::GenerationType::RuntimeFormat,
+                ).expect("Runtime data codegen failed");
+            }
+            println!("cargo:rerun-if-changed={}", $x);
+        )*
+    };
+}
