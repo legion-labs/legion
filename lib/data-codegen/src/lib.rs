37a26abed214c73b675e9dce2dc58a1f0d5d46e4
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-10-18 23:09:26 -0400

    Fix loop

--- lib/data-codegen/src/lib.rs
+++ lib/data-codegen/src/lib.rs
@@ -142,25 +142,20 @@ pub fn generate_data_container_code(
 macro_rules! data_container_gen {
     ( $( $x:expr ),* ) => {
         let package_path = env!("CARGO_MANIFEST_DIR").to_lowercase();
-
-        let mut data_path = package_path.replace("_runtime", "_offline");
         $(
+            let mut data_path = package_path.replace("_runtime", "_offline");
             data_path.push_str($x);
-        )*
-
-        if package_path.ends_with("_offline") {
-            legion_data_codegen::generate_data_container_code(
-                std::path::Path::new(&data_path),
-                &legion_data_codegen::GenerationType::OfflineFormat,
-            ).expect("Offline data codegen failed");
-        } else if package_path.ends_with("_runtime") {
-            legion_data_codegen::generate_data_container_code(
-                std::path::Path::new(&data_path),
-                &legion_data_codegen::GenerationType::RuntimeFormat,
-            ).expect("Runtime data codegen failed");
-        }
-
-        $(
+            if package_path.ends_with("_offline") {
+                legion_data_codegen::generate_data_container_code(
+                    std::path::Path::new(&data_path),
+                    &legion_data_codegen::GenerationType::OfflineFormat,
+                ).expect("Offline data codegen failed");
+            } else if package_path.ends_with("_runtime") {
+                legion_data_codegen::generate_data_container_code(
+                    std::path::Path::new(&data_path),
+                    &legion_data_codegen::GenerationType::RuntimeFormat,
+                ).expect("Runtime data codegen failed");
+            }
             println!("cargo:rerun-if-changed={}", $x);
         )*
     };
