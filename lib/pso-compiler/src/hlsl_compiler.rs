311f9bcf498e42906f67b88282e999b1e280755c
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-10-26 23:24:37 -0400

    Add shader tests (#440)

--- lib/pso-compiler/src/hlsl_compiler.rs
+++ lib/pso-compiler/src/hlsl_compiler.rs
@@ -15,7 +15,7 @@ pub struct CompileDefine {
 }
 
 pub enum ShaderSource<'a> {
-    Code(String),
+    Code(&'a str),
     Path(&'a Path),
 }
 
@@ -40,15 +40,15 @@ pub struct CompileResult {
     pub refl_info: Option<ShaderStageReflection>,
 }
 
-pub struct HLSLCompiler {
+pub struct HlslCompiler {
     _dxc: Dxc,
 }
 
-impl HLSLCompiler {
+impl HlslCompiler {
     pub fn new() -> Result<Self> {
         let dxc = Dxc::new(None)?;
 
-        Ok(HLSLCompiler { _dxc: dxc })
+        Ok(HlslCompiler { _dxc: dxc })
     }
 
     pub fn compile(&self, params: &CompileParams) -> Result<CompileResult> {
@@ -103,7 +103,7 @@ fn get_shader_source(params: &CompileParams) -> Result<String, anyhow::Error> {
 
     match &params.shader_source {
         ShaderSource::Code(code) => {
-            shader_code = code.clone();
+            shader_code = code.to_string();
         }
         ShaderSource::Path(path) => {
             let mut f = std::fs::File::open(path).unwrap();
@@ -254,5 +254,138 @@ fn to_push_constant(
     PushConstant {
         used_in_shader_stages: shader_stage_flags,
         size: push_constant.size,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use graphics_api::ShaderStageFlags;
+
+    use crate::{CompileParams, HlslCompiler, ShaderSource};
+
+    const SHADER: &str = "
+    struct VertexIn {
+        float3 pos : POSITION;
+    };
+    
+    struct VertexOut {
+        float4 hpos : SV_POSITION;
+        float4 color : COLOR;
+    };
+    
+    struct VertexColor {
+        float3 foo;    
+        float4 color;
+        float bar;
+    };
+    
+    [[vk::binding(0, 0)]]
+    RWByteAddressBuffer rw_test_byteaddressbuffer[1];
+    
+    [[vk::binding(1, 0)]]
+    ByteAddressBuffer test_byteaddressbuffer[2];
+    
+    [[vk::binding(2, 0)]]
+    StructuredBuffer<VertexColor> vertex_color;
+    
+    [[vk::binding(3, 0)]]
+    ConstantBuffer<VertexColor> cb_vertex_color[6];
+    
+    [[vk::binding(4, 0)]]
+    SamplerState sampl[12];
+    
+    [[vk::binding(5, 0)]]
+    Texture2D<float3> tex2d[2];
+    
+    [[vk::binding(6, 0)]]
+    RWTexture2D<float3> rw_tex2d[2];
+    
+    [[vk::binding(7, 0)]]
+    Texture2DArray<float3> tex2darray[2];
+    
+    [[vk::binding(8, 0)]]
+    RWTexture2DArray<float3> rw_tex2darray[2];
+    
+    [[vk::binding(9, 0)]]
+    Texture3D<float2> tex3d[2];
+    
+    [[vk::binding(10, 0)]]
+    RWTexture3D<float2> rw_tex3d[2];
+    
+    [[vk::binding(11, 0)]]
+    TextureCube<float> texcube[2];
+    
+    [[vk::binding(12, 0)]]
+    TextureCubeArray<float> rw_texcube[2];
+    
+    [[vk::binding(13, 0)]]
+    RWStructuredBuffer<VertexColor> rw_vertex_color;
+    
+    [[vk::push_constant]]
+    ConstantBuffer<VertexColor> push_cst;
+    
+    VertexOut main_vs(in VertexIn vIn) {
+    
+        VertexOut vOut;
+        vOut.hpos = float4(vIn.pos, 1.f);
+        vOut.color = vertex_color[0].color;   
+        vOut.color = push_cst.color;
+        return vOut;
+    }
+    
+    float4 main_ps(in VertexOut fIn) : SV_TARGET0  {
+        return fIn.color;
+    }";
+
+    #[test]
+    fn compile_vs_shader() {
+        let compiler = HlslCompiler::new().expect(
+            "dxcompiler dynamic library needs to be available in the default system search paths",
+        );
+
+        let compile_params = CompileParams {
+            shader_source: ShaderSource::Code(SHADER),
+            entry_point: "main_vs",
+            target_profile: "vs_6_1",
+            defines: Vec::new(),
+        };
+
+        let vs_out = compiler
+            .compile(&compile_params)
+            .expect("Shader compilation to succeed");
+        let refl_info = vs_out.refl_info.expect("Valid reflection info");
+        assert_eq!(refl_info.shader_stage, ShaderStageFlags::VERTEX);
+        assert_eq!(&refl_info.entry_point_name, "main_vs");
+        assert_eq!(refl_info.shader_resources.len(), 1);
+        assert_eq!(refl_info.shader_resources[0].name, "vertex_color");
+        assert_eq!(refl_info.shader_resources[0].element_count, 1);
+        assert_eq!(refl_info.shader_resources[0].binding, 2);
+        assert_eq!(refl_info.shader_resources[0].set_index, 0);
+        assert_eq!(
+            refl_info.shader_resources[0].used_in_shader_stages,
+            ShaderStageFlags::VERTEX
+        );
+    }
+
+    #[test]
+    fn compile_ps_shader() {
+        let compiler = HlslCompiler::new().expect(
+            "dxcompiler dynamic library needs to be available in the default system search paths",
+        );
+
+        let compile_params = CompileParams {
+            shader_source: ShaderSource::Code(SHADER),
+            entry_point: "main_ps",
+            target_profile: "ps_6_1",
+            defines: Vec::new(),
+        };
+
+        let ps_out = compiler
+            .compile(&compile_params)
+            .expect("Shader compilation to succeed");
+        let refl_info = ps_out.refl_info.expect("Valid reflection info");
+        assert_eq!(&refl_info.entry_point_name, "main_ps");
+        assert_eq!(refl_info.shader_stage, ShaderStageFlags::FRAGMENT);
+        assert_eq!(refl_info.shader_resources.len(), 0);
     }
 }
