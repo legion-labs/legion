b4479b774c8f9160c4f96f277cf813f740b3d672
Marc-Antoine Desroches <mad@legionlabs.com>
2021-10-29 15:58:04 -0400

    analytics-web: add vue-router

---
+++ client/analytics-web/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img alt="logo" src="../assets/logo.png">
+    <ProcessList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProcessList from '@/components/process-list.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ProcessList
+  }
+}
+</script>
