5a3f23373e33cfdd826329ba7dfe0e14cb4c8805
mparenteau-legion <marc.parenteau@legionlabs.com>
2021-11-02 08:49:39 -0400

    custom Reference serialization

--- lib/data-runtime/src/reference.rs
+++ lib/data-runtime/src/reference.rs
@@ -1,9 +1,8 @@
 use crate::{AssetRegistry, Handle, Resource, ResourceId};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::any::Any;
 
 /// A `ResourceReference` represents a reference to an external resource, that can be promoted to a handle
-#[derive(Serialize, Deserialize)]
 pub enum Reference<T>
 where
     T: Any + Resource,
@@ -15,7 +14,6 @@ where
     None,
 
     /// Reference is active, and be accessed through a typed handle
-    #[serde(skip)]
     Active(Handle<T>),
 }
 
@@ -28,6 +26,40 @@ where
         if let Self::Passive(resource_id) = self {
             let handle = registry.get_untyped(*resource_id).unwrap();
             *self = Self::Active(handle.into());
+        }
+    }
+}
+
+impl<T> Serialize for Reference<T>
+where
+    T: Any + Resource,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let resource_id = match self {
+            Self::Passive(resource_id) => Some(*resource_id),
+            Self::None => None,
+            Self::Active(handle) => Some(handle.id()),
+        };
+        resource_id.serialize(serializer)
+    }
+}
+
+impl<'de, T> Deserialize<'de> for Reference<T>
+where
+    T: Any + Resource,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        // Unless unset (None), a Reference will be deserialized as passive, and will require activation
+        let resource_id = Option::<ResourceId>::deserialize(deserializer)?;
+        match resource_id {
+            Some(resource_id) => Ok(Self::Passive(resource_id)),
+            None => Ok(Self::None),
         }
     }
 }
