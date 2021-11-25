343b18ec18c06c2731027d0c66b00c288c42699b
Kris Stasik <kris.stasik@legionlabs.com>
2021-11-25 09:45:27 -0500

    Cleanup build script

--- compiler/testentity/build.rs
+++ compiler/testentity/build.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let definition_path = "../../test/generic_data_offline/test_entity.rs";
-    let package_path = env!("CARGO_MANIFEST_DIR").to_lowercase();
-    let data_path = std::path::Path::new(&package_path);
-    let data_path = data_path.join(definition_path);
-    legion_data_codegen::generate_data_compiler_code(&data_path).expect("Compiler codegen failed");
-    println!("cargo:rerun-if-changed={:?}", data_path);
+    let source =
+        legion_data_codegen::definition_path("../../test/generic_data_offline/test_entity.rs");
+    legion_data_codegen::generate_data_compiler_code(&source).expect("Compiler codegen failed");
+    println!("cargo:rerun-if-changed={:?}", source);
 }
