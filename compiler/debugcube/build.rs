46be49f7cb89ee06883d5e2f8ec16c64447c4fa3
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-11-27 14:31:26 -0500

    Fix data container dirtying the build

--- compiler/debugcube/build.rs
+++ compiler/debugcube/build.rs
@@ -2,5 +2,5 @@ fn main() {
     let source =
         legion_data_codegen::definition_path("../../test/generic_data_offline/debug_cube.rs");
     legion_data_codegen::generate_data_compiler_code(&source).expect("Compiler codegen failed");
-    println!("cargo:rerun-if-changed={:?}", source);
+    println!("cargo:rerun-if-changed={}", source.to_str().unwrap());
 }
