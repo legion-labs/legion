bdb22c0ac764aa909d47fc0edaaa0ff012b04b62
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-11-30 16:51:53 -0500

    Minor fixes

--- lib/build-utils/src/lib.rs
+++ lib/build-utils/src/lib.rs
@@ -1,4 +1,30 @@
 //! Legion build utils
+//! This crate is meant to provide helpers for code generation in the monorepo
+//! We rely on code generation in multiple instances:
+//! * Proto files that generate rust and javascript files
+//! * Shader files definition that generate rust and hlsl
+//! * Data containers that generate rust files
+//!
+//! There is 2 ways of handling generated files in the rust ecosystem:
+//! * Relying on `OUT_DIR` environment variable to generate in place any necessary file. (tonic, windows api, ...)
+//! * Generating the files in the repo and committing them to the repo. (rust-analyser, rusoto, ...)
+//!
+//! We can't generate files in the crate directory and not have them committed, since we have to think about the case of an external dependency being downloaded in the local immutable register.
+//!
+//! Advantages:
+//! * Improves readability and UX of generated files (Go to definition works in VS Code, looking at code from github)
+//! * Allows inclusion of generated files from other systems (Javasctript, hlsl in a uniform manner) since `OUT_DIR` is only know during the cargo build of a given crate.
+//!
+//! Drawbacks:
+//! * Dummy conflict in generated code
+//! * We lose the ability to modify some src files from the github web interface since you
+//! * Confusion about non generated code and generated code (although mitigated by conventions)
+//!
+//! Restriction and rules:
+//! * We can't have binary files checked in
+//! * Modification of the generated files would not be allowed under any circumstances, the build machines fail if any change was detected
+//! * Files whose generation ca be driven by features, or that are platform dependent would still use OUT_DIR.
+//! * Other cases where the in repo generation doesn't bring much
 //!
 
 // BEGIN - Legion Labs lints v0.6
@@ -72,7 +98,7 @@ pub enum Error {
     #[error("Failed to build with the following error `{0}`")]
     Build(String),
     #[error("Missing necessary tool `{0}`")]
-    MissingTool(#[from] which::Error),
+    MissingTool(String),
     #[error("unknown error")]
     Unknown,
 }
@@ -172,7 +198,7 @@ pub fn build_protos(
     Ok(())
 }
 
-/// Build proto files
+/// Handle the copy/validation of the output files
 ///
 /// # Errors
 /// Returns a generation error or an IO error
@@ -197,7 +223,8 @@ pub fn handle_output(context: &Context) -> Result<()> {
 }
 
 fn run_cmd<S: AsRef<OsStr>>(command_path: S, args: &[&str], dir: &str) -> Result<()> {
-    let command_path = which::which(command_path)?;
+    let command_path = which::which(command_path.as_ref())
+        .map_err(|_err| Error::MissingTool(command_path.as_ref().to_str().unwrap().to_string()))?;
     let success = Command::new(&command_path)
         .args(args)
         .current_dir(dir)
