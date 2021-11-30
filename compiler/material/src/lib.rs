861f37075532796a55466a12f91345e26aac896e
Kris Stasik <kris.stasik@legionlabs.com>
2021-11-30 14:08:49 -0500

    Add missing lint sections

--- compiler/material/src/lib.rs
+++ compiler/material/src/lib.rs
@@ -1,7 +1,62 @@
+// BEGIN - Legion Labs lints v0.6
+// do not change or add/remove here, but one can add exceptions after this section
+#![deny(unsafe_code)]
+#![warn(future_incompatible, nonstandard_style, rust_2018_idioms)]
+// Rustdoc lints
+#![warn(
+    rustdoc::broken_intra_doc_links,
+    rustdoc::missing_crate_level_docs,
+    rustdoc::private_intra_doc_links
+)]
+// Clippy pedantic lints, treat all as warnings by default, add exceptions in allow list
+#![warn(clippy::pedantic)]
+#![allow(
+    clippy::cast_possible_truncation,
+    clippy::cast_sign_loss,
+    clippy::if_not_else,
+    clippy::items_after_statements,
+    clippy::missing_panics_doc,
+    clippy::module_name_repetitions,
+    clippy::must_use_candidate,
+    clippy::similar_names,
+    clippy::shadow_unrelated,
+    clippy::unreadable_literal,
+    clippy::unseparated_literal_suffix
+)]
+// Clippy nursery lints, still under development
+#![warn(
+    clippy::debug_assert_with_mut_call,
+    clippy::disallowed_method,
+    clippy::disallowed_type,
+    clippy::fallible_impl_from,
+    clippy::imprecise_flops,
+    clippy::mutex_integer,
+    clippy::path_buf_push_overwrite,
+    clippy::string_lit_as_bytes,
+    clippy::use_self,
+    clippy::useless_transmute
+)]
+// Clippy restriction lints, usually not considered bad, but useful in specific cases
+#![warn(
+    clippy::dbg_macro,
+    clippy::exit,
+    clippy::float_cmp_const,
+    clippy::map_err_ignore,
+    clippy::mem_forget,
+    clippy::missing_enforced_import_renames,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::string_to_string,
+    clippy::todo,
+    clippy::unimplemented,
+    clippy::verbose_file_reads
+)]
+// END - Legion Labs lints v0.6
+// crate-specific exceptions:
+#![allow()]
+
 use legion_data_compiler::{
     compiler_api::{
-        CompilationOutput, CompilerContext, CompilerDescriptor, CompilerError,
-        DATA_BUILD_VERSION,
+        CompilationOutput, CompilerContext, CompilerDescriptor, CompilerError, DATA_BUILD_VERSION,
     },
     compiler_utils::{hash_code_and_data, path_id_to_binary},
 };
@@ -21,7 +76,7 @@ pub static COMPILER_INFO: CompilerDescriptor = CompilerDescriptor {
     compile_func: compile,
 };
 
-fn compile(mut context: CompilerContext) -> Result<CompilationOutput, CompilerError> {
+fn compile(mut context: CompilerContext<'_>) -> Result<CompilationOutput, CompilerError> {
     let resources = context
         .take_registry()
         .add_loader::<legion_graphics_offline::Material>()
