6e1950efb99318845a990e464dad6a3f4503cb05
Marc-Antoine Desroches <mad@legionlabs.com>
2021-12-20 14:23:42 -0500

    fixed scope event serialization
    + updated analytics server data set

--- lib/telemetry/src/thread_events.rs
+++ lib/telemetry/src/thread_events.rs
@@ -11,7 +11,7 @@ pub trait ScopeEvent {
 #[derive(Debug, TransitReflect)]
 pub struct BeginScopeEvent {
     pub time: i64,
-    pub scope: GetScopeDesc,
+    pub scope: fn() -> ScopeDesc, //can't be GetScopeDesc because the reflection would not see it as a reference
 }
 
 impl InProcSerialize for BeginScopeEvent {}
@@ -24,7 +24,7 @@ impl ScopeEvent for BeginScopeEvent {
 #[derive(Debug, TransitReflect)]
 pub struct EndScopeEvent {
     pub time: i64,
-    pub scope: GetScopeDesc,
+    pub scope: fn() -> ScopeDesc, //can't be GetScopeDesc because the reflection would not see it as a reference
 }
 
 impl InProcSerialize for EndScopeEvent {}
