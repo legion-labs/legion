384ee3f684d49ad81bd213a2d5f0ce82543009c5
anthony-de-rochefort <anthony.derochefort@legionlabs.com>
2021-12-21 16:04:40 -0500

    WIP

---
+++ plugin/renderer/src/picking/manipulator.rs
@@ -0,0 +1,26 @@
+use std::sync::{Arc, Mutex};
+
+trait Manipulator {
+    fn new() -> Self;
+
+    
+}
+
+struct PositionManipulator {}
+
+impl Manipulator for PositionManipulator {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+struct RotationManipulator {}
+struct ScaleManipulator {}
+
+struct ManipulatorManagerInner {}
+
+struct ManipulatorManager {
+    inner: Arc<Mutex<ManipulatorManagerInner>>,
+}
+
+impl ManipulatorManager {}
