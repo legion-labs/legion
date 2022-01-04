3a9b0507d634ed606bb1941158a9b1ee5f7bd59e
Kevin COMBRIAT <kevin.combriat@legionlabs.com>
2022-01-04 14:52:20 -0500

    Legion frontend: Add a `run` function

--- client/editor/frontend/src/index.ts
+++ client/editor/frontend/src/index.ts
@@ -1,38 +1,14 @@
 import "./assets/index.css";
 
-import log, { Level } from "@lgn/frontend/src/lib/log";
-import { init as initLgnFrontend } from "@lgn/frontend";
+import { defaultAuthUserConfig, run } from "@lgn/frontend";
 import App from "@/App.svelte";
 import "@/workers/editorWorker";
 
-/**
- * Runs the application.
- */
-async function run() {
-  const target = document.querySelector("#root");
-
-  if (!target) {
-    log.error("#root element can't be found");
-
-    return;
-  }
-
-  const logLevel: Level = "warn";
-
-  if (logLevel) {
-    log.init();
-    log.set(logLevel);
-  }
-
-  await initLgnFrontend({ auth: true, forceAuth: false });
-
-  try {
-    new App({ target });
-  } catch (error) {
-    log.error(error);
-
-    return;
-  }
-}
-
-run();
+run({
+  appComponent: App,
+  auth: defaultAuthUserConfig(),
+  rootQuerySelector: "#root",
+  logLevel: "warn",
+})
+  // eslint-disable-next-line no-console
+  .catch((error) => console.error("Application couldn't start", error));
--- lib/browser/frontend/src/index.ts
+++ lib/browser/frontend/src/index.ts
@@ -1,37 +1,192 @@
 import { getUserInfo as tauriGetUserInfo } from "./lib/auth/tauri";
 import { userAuth as browserUserAuth } from "./lib/auth/browser";
-import log from "./lib/log";
+import log, { Level as LogLevel } from "./lib/log";
 import userInfo from "./stores/userInfo";
 import { UserInfo } from "./lib/auth";
 
+export type AuthUserConfig = {
+  /** Force authentication on application start */
+  forceAuth: boolean;
+  /** The path Cognito redirected the authed user to, typically `"/"` */
+  redirectedTo: string;
+  /** The path to redirect the user to after they got fully authenticated, typically `"/"` */
+  redirectTo: string;
+  /**
+   * Title used by the `history.replaceState` function,
+   * [ignored](window.history.replaceState) for now.
+   */
+  redirectionTitle: string;
+};
+
+export function defaultAuthUserConfig(): AuthUserConfig {
+  return {
+    forceAuth: false,
+    redirectTo: "/",
+    redirectedTo: "/",
+    redirectionTitle: "Home",
+  };
+}
+
 /**
- * Init some of the required dependencies.
+ * Find the root element
+ * @param rootQuerySelector A valid query selector that targets the root element
+ */
+export function getTarget(rootQuerySelector: string) {
+  const target = document.querySelector("#root");
+
+  if (!target) {
+    log.error(`${rootQuerySelector} element can't be found`);
+
+    return null;
+  }
+
+  return target;
+}
+
+type HookLogOnlyArgs<HasLog extends boolean> = {
+  log: HasLog extends true ? typeof log : null;
+};
+
+type HookLogOnlyFunction<HasLog extends boolean> = (
+  args: HookLogOnlyArgs<HasLog>
+) => void;
+
+type HookArgs<HasLog extends boolean, HasAuth extends boolean> = {
+  log: HasLog extends true ? typeof log : null;
+  userInfo: HasAuth extends true ? UserInfo : null;
+};
+
+type HookFunction<HasLog extends boolean, HasAuth extends boolean> = (
+  args: HookArgs<HasLog, HasAuth>
+) => void;
+
+export type Config<
+  SvelteComponent,
+  Log extends LogLevel | null,
+  Auth extends AuthUserConfig | null
+> = {
+  /** A Svelte component class */
+  appComponent: new (options: {
+    target: Element | ShadowRoot;
+  }) => SvelteComponent;
+  /**
+   * Enable authentication or not (using `null`).
+   *
+   * If authentication is not enabled some functionalities like `userInfo` will not be usable
+   */
+  auth: Auth;
+  /** A valid query selector to mount your app into  */
+  rootQuerySelector: string;
+  /** Log level, if set to `null` logs are entirely disabled  */
+  logLevel: Log;
+  /** Function(s) called before the user is authenticated, _can't be provided if `auth` is `null` */
+  onAuthStart?: Auth extends null
+    ? never
+    : HookLogOnlyFunction<Log extends null ? false : true>;
+  /** PostAuth function(s) called after the user is authenticated, _can't be provided if `auth` is `null` */
+  onAuthEnd?: Auth extends null
+    ? never
+    : HookFunction<
+        Log extends null ? false : true,
+        Auth extends null ? false : true
+      >;
+  /** Function(s) called before the Svelte application has started */
+  onInitStart?: HookFunction<
+    Log extends null ? false : true,
+    Auth extends null ? false : true
+  >;
+  /** Function(s) called after the Svelte application has started */
+  onInitEnd?: HookFunction<
+    Log extends null ? false : true,
+    Auth extends null ? false : true
+  >;
+};
+
+/**
+ * Run a Legion client.
  * _Must be called at the beginning of any application that uses this library._
  *
  * If the `forceAuth` option is `true` the unauthenticated users
  * will be redirected to Cognito.
  */
-export async function init({
-  auth,
-  forceAuth,
-}: {
-  auth: boolean;
-  forceAuth: boolean;
-}) {
+export async function run<SvelteComponent>(
+  config: Config<SvelteComponent, null, null>
+): Promise<void>;
+export async function run<SvelteComponent>(
+  config: Config<SvelteComponent, LogLevel, null>
+): Promise<void>;
+export async function run<SvelteComponent>(
+  config: Config<SvelteComponent, null, AuthUserConfig>
+): Promise<void>;
+export async function run<SvelteComponent>(
+  config: Config<SvelteComponent, LogLevel, AuthUserConfig>
+): Promise<void>;
+export async function run<SvelteComponent>({
+  appComponent: App,
+  auth: authConfig,
+  rootQuerySelector,
+  logLevel,
+  onAuthStart,
+  onAuthEnd,
+  onInitStart,
+  onInitEnd,
+}: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+Config<SvelteComponent, any, any>): Promise<void> {
+  const target = getTarget(rootQuerySelector);
+
+  if (logLevel) {
+    log.init();
+    log.set(logLevel);
+  }
+
+  if (!target) {
+    return;
+  }
+
   let userInfoSet: UserInfo | null = null;
 
-  if (auth) {
+  if (authConfig) {
+    if (onAuthStart) {
+      onAuthStart({ log });
+    }
+
     if (window.__TAURI__) {
-      userInfoSet = await tauriGetUserInfo(userInfo, { forceAuth });
+      userInfoSet = await tauriGetUserInfo(userInfo, {
+        forceAuth: authConfig.forceAuth,
+      });
     } else {
-      userInfoSet = await browserUserAuth(userInfo, { forceAuth });
+      userInfoSet = await browserUserAuth(userInfo, {
+        forceAuth: authConfig.forceAuth,
+      });
+    }
+
+    if (onAuthEnd) {
+      onAuthEnd({ log, userInfo: userInfoSet });
     }
   }
 
-  log.debug(
-    "user",
-    userInfoSet
-      ? log.json`User is authed: ${userInfoSet}`
-      : "User is not authed"
-  );
+  if (logLevel) {
+    log.debug(
+      "user",
+      userInfoSet
+        ? log.json`User is authed: ${userInfoSet}`
+        : "User is not authed"
+    );
+  }
+
+  if (onInitStart) {
+    onInitStart({ log, userInfo: userInfoSet });
+  }
+
+  try {
+    new App({ target });
+  } catch (error) {
+    log.error(error);
+
+    return;
+  }
+
+  if (onInitEnd) {
+    onInitEnd({ log, userInfo: userInfoSet });
+  }
 }
