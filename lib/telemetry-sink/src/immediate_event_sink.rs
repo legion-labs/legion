d62b37708d1d630ee5f68f54759764243a5f9b1b
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2022-01-06 14:59:56 -0500

    Feature parity with tracing chrom impl

--- lib/telemetry-sink/src/immediate_event_sink.rs
+++ lib/telemetry-sink/src/immediate_event_sink.rs
@@ -1,6 +1,9 @@
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex, RwLock},
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc, Mutex, RwLock,
+    },
 };
 
 use lgn_telemetry::{
@@ -14,14 +17,13 @@ pub struct ImmediateEventSink {
     simple_logger: SimpleLogger,
     chrome_trace_file: Option<String>,
     process_data: RwLock<Option<ProcessData>>,
-    thread_names: RwLock<HashMap<String, String>>,
+    thread_ids: RwLock<HashMap<String, (u64, String)>>,
     chrome_events: Mutex<json::Array>,
 }
 
 struct ProcessData {
     tsc_frequency: u64,
     start_ticks: i64,
-    process_id: String,
 }
 
 impl ImmediateEventSink {
@@ -30,7 +32,7 @@ impl ImmediateEventSink {
             simple_logger: SimpleLogger::new().with_utc_timestamps(),
             chrome_trace_file,
             process_data: RwLock::new(None),
-            thread_names: RwLock::new(HashMap::new()),
+            thread_ids: RwLock::new(HashMap::new()),
             chrome_events: Mutex::new(json::Array::new()),
         }
     }
@@ -38,16 +40,43 @@ impl ImmediateEventSink {
 
 impl EventSink for ImmediateEventSink {
     fn on_startup(&self, proc_info: ProcessInfo) {
+        if self.chrome_trace_file.is_none() {
+            return;
+        }
         let mut process_data = self.process_data.write().unwrap();
         *process_data = Some(ProcessData {
             tsc_frequency: proc_info.tsc_frequency,
             start_ticks: proc_info.start_ticks,
-            process_id: proc_info.process_id,
         });
+        let event = json::object! {
+            ph: "M",
+            name: "process_name",
+            pid: 1,
+            args: {
+                name: proc_info.exe,
+            },
+        };
+        let mut events = self.chrome_events.lock().unwrap();
+        events.push(event);
     }
     fn on_shutdown(&self) {
         if let Some(chrome_trace_file) = &self.chrome_trace_file {
-            let chrome_events = self.chrome_events.lock().unwrap();
+            let mut chrome_events = self.chrome_events.lock().unwrap();
+            let thread_ids = self.thread_ids.read().unwrap();
+            let mut id_names: Vec<_> = thread_ids.iter().map(|(_, id_name)| id_name).collect();
+            id_names.sort_by(|(_, a), (_, b)| a.cmp(b));
+            for (idx, (id, _)) in id_names.into_iter().enumerate() {
+                let event = json::object! {
+                    ph: "M",
+                    name: "thread_sort_index",
+                    pid: 1,
+                    tid: *id,
+                    args: {
+                        sort_index: idx,
+                    },
+                };
+                chrome_events.push(event);
+            }
             let trace_document = json::object! {
                 traceEvents: chrome_events.clone(),
             };
@@ -76,15 +105,34 @@ impl EventSink for ImmediateEventSink {
         if self.chrome_trace_file.is_none() {
             return;
         }
+        // Since perfetto doesn't support thread_sort_idx, we try at least to have the order
+        // of threads as they come in. At least main thread is always first.
+        static THREAD_ID_ASSIGNMENT: AtomicU64 = AtomicU64::new(0);
+        let thread_id = thread_stream
+            .properties()
+            .get("thread-id")
+            .expect("thread-id need to be set");
         let thread_name = thread_stream
             .properties()
             .get("thread-name")
-            .or_else(|| thread_stream.properties().get("thread-id"));
+            .or(Some(thread_id));
+        let thread_id = THREAD_ID_ASSIGNMENT.fetch_add(1, Ordering::Relaxed);
         if let Some(thread_name) = thread_name {
-            self.thread_names
-                .write()
-                .unwrap()
-                .insert(thread_stream.stream_id().to_owned(), thread_name.clone());
+            self.thread_ids.write().unwrap().insert(
+                thread_stream.stream_id().to_owned(),
+                (thread_id, thread_name.clone()),
+            );
+            let event = json::object! {
+                ph: "M",
+                name: "thread_name",
+                pid: 1,
+                tid: thread_id,
+                args: {
+                    name: thread_name.as_str(),
+                },
+            };
+            let mut events = self.chrome_events.lock().unwrap();
+            events.push(event);
         }
     }
 
@@ -101,32 +149,40 @@ impl EventSink for ImmediateEventSink {
         }
         let process_data = self.process_data.read().unwrap();
         if let Some(process_data) = &*process_data {
-            if let Some(thread_name) = self.thread_names.read().unwrap().get(&block.stream_id) {
+            if let Some((thread_id, _)) = self.thread_ids.read().unwrap().get(&block.stream_id) {
                 for x in block.events.iter() {
-                    let (phase, tick, name) = match x {
-                        ThreadEventQueueAny::BeginScopeEvent(evt) => {
-                            ("B", evt.time, evt.get_scope()().name)
-                        }
-                        ThreadEventQueueAny::EndScopeEvent(evt) => {
-                            //record_scope_event_dependencies(&evt, &mut recorded_deps, &mut deps);
-                            ("E", evt.time, evt.get_scope()().name)
-                        }
+                    let (phase, tick, name, file, line) = match x {
+                        ThreadEventQueueAny::BeginScopeEvent(evt) => (
+                            "B",
+                            evt.time,
+                            evt.get_scope()().name,
+                            evt.get_scope()().filename,
+                            evt.get_scope()().line,
+                        ),
+                        ThreadEventQueueAny::EndScopeEvent(evt) => (
+                            "E",
+                            evt.time,
+                            evt.get_scope()().name,
+                            evt.get_scope()().filename,
+                            evt.get_scope()().line,
+                        ),
                     };
-                    let time = 1000 * 1000 * (tick - process_data.start_ticks) as u64
-                        / process_data.tsc_frequency;
+                    let time = 1000.0 * 1000.0 * (tick - process_data.start_ticks) as f64
+                        / process_data.tsc_frequency as f64;
                     let event = json::object! {
                         name: name,
                         cat: "PERF",
                         ph: phase,
-                        pid: process_data.process_id.as_str(),
-                        tid: thread_name.as_str(),
+                        pid: 1,
+                        tid: *thread_id,
                         ts: time,
-
+                        args: {
+                            "[file]": file,
+                            "[line]": line,
+                        },
                     };
-                    {
-                        let mut events = self.chrome_events.lock().unwrap();
-                        events.push(event);
-                    }
+                    let mut events = self.chrome_events.lock().unwrap();
+                    events.push(event);
                 }
             }
         }
