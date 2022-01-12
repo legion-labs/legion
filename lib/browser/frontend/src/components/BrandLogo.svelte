b25468bf7bc09d2b191096fd5243b2cf82a305ce
Kevin COMBRIAT <kevin.combriat@legionlabs.com>
2022-01-12 18:35:51 -0500

    HQ snapshot and add logo to the top bar

---
+++ lib/browser/frontend/src/components/BrandLogo.svelte
@@ -0,0 +1,133 @@
+<script lang="ts">
+  interface $$Props {
+    class?: string;
+  }
+</script>
+
+<svg
+  class={$$props.class}
+  version="1.1"
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  x="0px"
+  y="0px"
+  viewBox="0 0 229.42 288.44"
+  style="enable-background:new 0 0 229.42 288.44;"
+  xml:space="preserve"
+>
+  <style type="text/css">
+    .st0 {
+      display: none;
+    }
+    .st1 {
+      display: inline;
+    }
+    .st2 {
+      fill: none;
+      stroke: #abe1fa;
+      stroke-width: 0.2195;
+      stroke-miterlimit: 10;
+    }
+    .st3 {
+      fill: none;
+      stroke: #abe1fa;
+      stroke-width: 0.2195;
+      stroke-miterlimit: 10;
+      stroke-dasharray: 2.2061, 2.2061;
+    }
+    .st4 {
+      fill: none;
+      stroke: #abe1fa;
+      stroke-width: 0.5;
+      stroke-miterlimit: 10;
+    }
+    .st5 {
+      fill: none;
+      stroke: #abe1fa;
+      stroke-width: 0.5;
+      stroke-miterlimit: 10;
+      stroke-dasharray: 4.9726, 4.9726;
+    }
+    .st6 {
+      display: inline;
+      fill: #abe1fa;
+    }
+    .st7 {
+      fill: #fc4d0f;
+    }
+    .st8 {
+      fill: none;
+    }
+  </style>
+  <g id="Safe_Zone" class="st0">
+    <g class="st1">
+      <g>
+        <line class="st2" x1="0" y1="57.35" x2="1.1" y2="57.35" />
+        <line class="st3" x1="3.3" y1="57.35" x2="227.22" y2="57.35" />
+        <line class="st2" x1="228.32" y1="57.35" x2="229.42" y2="57.35" />
+      </g>
+    </g>
+    <g class="st1">
+      <g>
+        <line class="st2" x1="0" y1="0" x2="1.1" y2="0" />
+        <line class="st3" x1="3.3" y1="0" x2="227.22" y2="0" />
+        <line class="st2" x1="228.32" y1="0" x2="229.42" y2="0" />
+      </g>
+    </g>
+    <g class="st1">
+      <g>
+        <line class="st2" x1="0" y1="231.06" x2="1.1" y2="231.06" />
+        <line class="st3" x1="3.3" y1="231.06" x2="227.22" y2="231.06" />
+        <line class="st2" x1="228.32" y1="231.06" x2="229.42" y2="231.06" />
+      </g>
+    </g>
+    <g class="st1">
+      <g>
+        <line class="st2" x1="0" y1="288.44" x2="1.1" y2="288.44" />
+        <line class="st3" x1="3.3" y1="288.44" x2="227.22" y2="288.44" />
+        <line class="st2" x1="228.32" y1="288.44" x2="229.42" y2="288.44" />
+      </g>
+    </g>
+    <g class="st1">
+      <g>
+        <line class="st4" x1="57.19" y1="0" x2="57.19" y2="2.5" />
+        <line class="st5" x1="57.19" y1="7.47" x2="57.19" y2="283.45" />
+        <line class="st4" x1="57.19" y1="285.94" x2="57.19" y2="288.44" />
+      </g>
+    </g>
+    <g class="st1">
+      <g>
+        <line class="st4" x1="0" y1="0" x2="0" y2="2.5" />
+        <line class="st5" x1="0" y1="7.47" x2="0" y2="283.45" />
+        <line class="st4" x1="0" y1="285.94" x2="0" y2="288.44" />
+      </g>
+    </g>
+    <rect x="0" y="231.06" class="st6" width="57.19" height="57.19" />
+    <g class="st1">
+      <g>
+        <line class="st4" x1="172.27" y1="0" x2="172.27" y2="2.5" />
+        <line class="st5" x1="172.27" y1="7.47" x2="172.27" y2="283.45" />
+        <line class="st4" x1="172.27" y1="285.94" x2="172.27" y2="288.44" />
+      </g>
+    </g>
+    <g class="st1">
+      <g>
+        <line class="st4" x1="229.3" y1="0" x2="229.3" y2="2.5" />
+        <line class="st5" x1="229.3" y1="7.47" x2="229.3" y2="283.45" />
+        <line class="st4" x1="229.3" y1="285.94" x2="229.3" y2="288.44" />
+      </g>
+    </g>
+    <rect x="172.22" class="st6" width="57.19" height="57.19" />
+    <rect x="172.22" y="231.06" class="st6" width="57.19" height="57.19" />
+    <rect x="0" class="st6" width="57.19" height="57.19" />
+  </g>
+  <g id="RGB">
+    <path
+      class="st7"
+      d="M114.93,173.86H57.51v-2.5c0-42.79,22.47-62.15,37.33-74.96c13.21-11.39,19.85-17.11,19.85-39.12h57.43
+		c0,48.33-23.94,68.96-39.78,82.62c-12.3,10.6-17.4,14.99-17.4,31.46V173.86z"
+    />
+    <rect x="114.95" y="173.86" class="st7" width="57.19" height="57.19" />
+    <rect class="st8" width="229.42" height="288.44" />
+  </g>
+</svg>
