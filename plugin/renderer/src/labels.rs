55fd9ec7d650958f42454b073f357cb170883553
anthony-de-rochefort <anthony.derochefort@legionlabs.com>
2022-01-13 14:49:26 -0500

    missed file

--- plugin/renderer/src/labels.rs
+++ plugin/renderer/src/labels.rs
@@ -1,7 +1,7 @@
 use lgn_ecs::schedule::SystemLabel;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, SystemLabel)]
-pub enum RendererSystemLabel {
-    Generation,
+pub enum CommandBufferLabel {
+    Generate,
     Submit,
 }
