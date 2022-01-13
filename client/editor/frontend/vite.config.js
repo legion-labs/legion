4e3322dcc63fa62ae2b31d79534f59bcfa036bd3
Kevin COMBRIAT <kevin.combriat@legionlabs.com>
2022-01-12 19:02:59 -0500

    Editor client deactivate wasm example for now as it seems to block the cargo run commands

--- client/editor/frontend/vite.config.js
+++ client/editor/frontend/vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import viteTsProto from "vite-plugin-ts-proto";
-import viteWasmPack from "vite-plugin-wasm";
+// import viteWasmPack from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,15 +19,15 @@ export default defineConfig({
         { name: "@lgn/proto-streaming", glob: "*.proto" },
       ],
     }),
-    viteWasmPack({
-      crates: [
-        {
-          path: "../../../lib/simple-wasm-logger",
-          packageName: "@lgn/simple-wasm-logger",
-        },
-      ],
-      outDir: "frontend",
-      quiet: true,
-    }),
+    // viteWasmPack({
+    //   crates: [
+    //     {
+    //       path: "../../../lib/simple-wasm-logger",
+    //       packageName: "@lgn/simple-wasm-logger",
+    //     },
+    //   ],
+    //   outDir: "frontend",
+    //   quiet: true,
+    // }),
   ],
 });
