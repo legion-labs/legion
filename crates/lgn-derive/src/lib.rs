ea899b7a822dfcd2a0aa9ad459ab881b3e223b7d
Marc Parenteau <marc.parenteau@legionlabs.com>
2022-01-18 11:01:54 -0500

    update derive and core crates

--- crates/lgn-derive/src/lib.rs
+++ crates/lgn-derive/src/lib.rs
@@ -59,23 +59,13 @@
 //#![allow()]
 
 mod app_plugin;
-mod bytes;
-mod enum_variant_meta;
 mod legion_main;
-mod modules;
 
 use lgn_macro_utils::{derive_label, LegionManifest};
 use proc_macro::TokenStream;
 use quote::format_ident;
 
-/// Derives the Bytes trait. Each field must also implements Bytes or this will
-/// fail.
-#[proc_macro_derive(Bytes)]
-pub fn derive_bytes(input: TokenStream) -> TokenStream {
-    bytes::derive_bytes(input)
-}
-
-/// Generates a dynamic plugin entry point function for the given `Plugin` type.
+/// Generates a dynamic plugin entry point function for the given `Plugin` type.  
 #[proc_macro_derive(DynamicPlugin)]
 pub fn derive_dynamic_plugin(input: TokenStream) -> TokenStream {
     app_plugin::derive_dynamic_plugin(input)
@@ -86,15 +76,10 @@ pub fn legion_main(attr: TokenStream, item: TokenStream) -> TokenStream {
     legion_main::legion_main(attr, item)
 }
 
-#[proc_macro_derive(EnumVariantMeta)]
-pub fn derive_enum_variant_meta(input: TokenStream) -> TokenStream {
-    enum_variant_meta::derive_enum_variant_meta(input)
-}
-
 #[proc_macro_derive(AppLabel)]
 pub fn derive_app_label(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
-    let mut trait_path = LegionManifest::default().get_path(crate::modules::LEGION_APP);
+    let mut trait_path = LegionManifest::default().get_path("lgn_app");
     trait_path.segments.push(format_ident!("AppLabel").into());
     derive_label(input, trait_path)
 }
