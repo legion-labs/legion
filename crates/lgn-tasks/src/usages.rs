5bd7e20704640488453a5a749835999a202477a8
Marc Parenteau <marc.parenteau@legionlabs.com>
2022-01-20 14:15:26 -0500

    update task-pool doc

--- crates/lgn-tasks/src/usages.rs
+++ crates/lgn-tasks/src/usages.rs
@@ -2,13 +2,7 @@
 //! kind of work should go in each pool is latency requirements.
 //!
 //! For CPU-intensive work (tasks that generally spin until completion) we have a standard
-//! [`ComputeTaskPool`]  and an [`AsyncComputeTaskPool`]. Work that does not need to be completed to
-//! present the next frame should go to the [`AsyncComputeTaskPool`]
-//!
-//! For IO-intensive work (tasks that spend very little time in a "woken" state) we have an IO
-//! task pool. The tasks here are expected to complete very quickly. Generally they should just
-//! await receiving data from somewhere (i.e. disk) and signal other systems when the data is ready
-//! for consumption. (likely via channels)
+//! [`ComputeTaskPool`].
 
 use std::ops::Deref;
 
