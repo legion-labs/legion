3eae36e82e603a4cc2edbb2875506c895da87f8d
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2022-01-20 00:34:53 -0500

    Add support for changed since arg for CI

--- build/lgn-monorepo/src/ci.rs
+++ build/lgn-monorepo/src/ci.rs
@@ -11,6 +11,9 @@ use crate::{context::Context, Result};
 pub struct Args {
     #[clap(subcommand)]
     command: Option<Commands>,
+    #[clap(long)]
+    /// Run on packages changed since the merge base of this commit
+    pub(crate) changed_since: Option<String>,
 }
 
 #[derive(Debug, Subcommand)]
@@ -21,15 +24,20 @@ enum Commands {
         /// Run formatting check
         #[clap(long)]
         fmt: bool,
+        /// Runs cargo check on the graphics-api crate
         #[clap(long)]
         gfx_no_api: bool,
+        /// Run clippy checks
         #[clap(long)]
         clippy: bool,
+        /// Run repo lints
         #[clap(long)]
         repo_lints: bool,
+        /// Run cargo-deny checks
         #[clap(long)]
         cargo_deny: bool,
     },
+    /// Run all tests
     #[clap(name = "test")]
     Test {
         /// Run build tests
@@ -39,6 +47,7 @@ enum Commands {
         #[clap(long)]
         run: bool,
     },
+    /// Run benches
     #[clap(name = "bench")]
     Bench {
         /// Run build tests
@@ -53,7 +62,7 @@ enum Commands {
 #[span_fn]
 pub fn run(args: &Args, ctx: &Context) -> Result<()> {
     if let Some(ref command) = args.command {
-        run_command(ctx, command)?;
+        run_command(ctx, &args.changed_since, command)?;
     } else {
         let command = Commands::Check {
             fmt: true,
@@ -62,12 +71,12 @@ pub fn run(args: &Args, ctx: &Context) -> Result<()> {
             repo_lints: true,
             cargo_deny: true,
         };
-        run_command(ctx, &command)?;
+        run_command(ctx, &args.changed_since, &command)?;
         let command = Commands::Test {
             build: true,
             run: true,
         };
-        run_command(ctx, &command)?;
+        run_command(ctx, &args.changed_since, &command)?;
         // do not run benches by default
         //let command = Commands::Bench {
         //    build: true,
@@ -78,7 +87,7 @@ pub fn run(args: &Args, ctx: &Context) -> Result<()> {
     Ok(())
 }
 
-fn run_command(ctx: &Context, command: &Commands) -> Result<()> {
+fn run_command(ctx: &Context, changed_since: &Option<String>, command: &Commands) -> Result<()> {
     match command {
         Commands::Check {
             fmt,
@@ -92,10 +101,10 @@ fn run_command(ctx: &Context, command: &Commands) -> Result<()> {
                 check_fmt(ctx)?;
             }
             if all || *gfx_no_api {
-                check_graphic_crate(ctx)?;
+                check_graphic_crate(ctx, changed_since)?;
             }
             if all || *clippy {
-                check_clippy(ctx)?;
+                check_clippy(ctx, changed_since)?;
             }
             if all || *repo_lints {
                 check_repo_lints(ctx)?;
@@ -108,44 +117,24 @@ fn run_command(ctx: &Context, command: &Commands) -> Result<()> {
         Commands::Test { build, run } => {
             let all = !build && !run;
             if all || *build {
-                test_build(ctx)?;
+                test_build(ctx, changed_since)?;
             }
             if all || *run {
-                test_run(ctx)?;
+                test_run(ctx, changed_since)?;
             }
             Ok(())
         }
         Commands::Bench { build, run } => {
             let all = !build && !run;
             if all || *build {
-                bench_build(ctx)?;
+                bench_build(ctx, changed_since)?;
             }
             if all || *run {
-                bench_run(ctx)?;
+                bench_run(ctx, changed_since)?;
             }
             Ok(())
         }
     }
-}
-
-#[span_fn]
-fn check_clippy(ctx: &Context) -> Result<()> {
-    action_step!("-- CI --", "Running clippy checks");
-    let args = clippy::Args {
-        package_args: SelectedPackageArgs {
-            workspace: true,
-            ..SelectedPackageArgs::default()
-        },
-        build_args: BuildArgs {
-            all_targets: true,
-            all_features: true,
-            locked: true,
-            ..BuildArgs::default()
-        },
-        args: vec!["-D".into(), "warnings".into()],
-        ..clippy::Args::default()
-    };
-    clippy::run(&args, ctx)
 }
 
 #[span_fn]
@@ -161,11 +150,12 @@ fn check_fmt(ctx: &Context) -> Result<()> {
 }
 
 #[span_fn]
-fn check_graphic_crate(ctx: &Context) -> Result<()> {
+fn check_graphic_crate(ctx: &Context, changed_since: &Option<String>) -> Result<()> {
     action_step!("-- CI --", "Running check on the graphics crate");
     let args = check::Args {
         package_args: SelectedPackageArgs {
             package: vec!["lgn-graphics-api".into()],
+            changed_since: changed_since.clone(),
             ..SelectedPackageArgs::default()
         },
         build_args: BuildArgs {
@@ -175,6 +165,27 @@ fn check_graphic_crate(ctx: &Context) -> Result<()> {
         },
     };
     check::run(&args, ctx)
+}
+
+#[span_fn]
+fn check_clippy(ctx: &Context, changed_since: &Option<String>) -> Result<()> {
+    action_step!("-- CI --", "Running clippy checks");
+    let args = clippy::Args {
+        package_args: SelectedPackageArgs {
+            workspace: true,
+            changed_since: changed_since.clone(),
+            ..SelectedPackageArgs::default()
+        },
+        build_args: BuildArgs {
+            all_targets: true,
+            all_features: true,
+            locked: true,
+            ..BuildArgs::default()
+        },
+        args: vec!["-D".into(), "warnings".into()],
+        ..clippy::Args::default()
+    };
+    clippy::run(&args, ctx)
 }
 
 #[span_fn]
@@ -210,7 +221,7 @@ fn check_cargo_deny(ctx: &Context) -> Result<()> {
 }
 
 #[span_fn]
-fn test_build(ctx: &Context) -> Result<()> {
+fn test_build(ctx: &Context, changed_since: &Option<String>) -> Result<()> {
     action_step!("-- CI --", "Running tests build");
     {
         let args = build::Args {
@@ -225,9 +236,13 @@ fn test_build(ctx: &Context) -> Result<()> {
     }
     let args = test::Args {
         package_args: SelectedPackageArgs {
+            changed_since: changed_since.clone(),
             ..SelectedPackageArgs::default()
         },
-        build_args: BuildArgs::default(),
+        build_args: BuildArgs {
+            //all_features: changed_since.is_some(),
+            ..BuildArgs::default()
+        },
         no_run: true,
         ..test::Args::default()
     };
@@ -235,13 +250,17 @@ fn test_build(ctx: &Context) -> Result<()> {
 }
 
 #[span_fn]
-fn test_run(ctx: &Context) -> Result<()> {
+fn test_run(ctx: &Context, changed_since: &Option<String>) -> Result<()> {
     action_step!("-- CI --", "Running tests");
     let args = test::Args {
         package_args: SelectedPackageArgs {
+            changed_since: changed_since.clone(),
             ..SelectedPackageArgs::default()
         },
-        build_args: BuildArgs::default(),
+        build_args: BuildArgs {
+            //all_features: changed_since.is_some(),
+            ..BuildArgs::default()
+        },
         args: if machine_has_discreet_gpu()? {
             vec![]
         } else {
@@ -254,10 +273,11 @@ fn test_run(ctx: &Context) -> Result<()> {
 }
 
 #[span_fn]
-fn bench_build(ctx: &Context) -> Result<()> {
+fn bench_build(ctx: &Context, changed_since: &Option<String>) -> Result<()> {
     action_step!("-- CI --", "Building benches");
     let args = bench::Args {
         package_args: SelectedPackageArgs {
+            changed_since: changed_since.clone(),
             ..SelectedPackageArgs::default()
         },
         ..bench::Args::default()
@@ -266,10 +286,11 @@ fn bench_build(ctx: &Context) -> Result<()> {
 }
 
 #[span_fn]
-fn bench_run(ctx: &Context) -> Result<()> {
+fn bench_run(ctx: &Context, changed_since: &Option<String>) -> Result<()> {
     action_step!("-- CI --", "Running benches");
     let args = bench::Args {
         package_args: SelectedPackageArgs {
+            changed_since: changed_since.clone(),
             ..SelectedPackageArgs::default()
         },
         ..bench::Args::default()
