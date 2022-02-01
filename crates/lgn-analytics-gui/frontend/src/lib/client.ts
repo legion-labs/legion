1f4f87fcc22819a68782f450c14fac2519c684b6
Tim <timothee.nitsas@legionlabs.com>
2022-02-01 14:19:14 -0500

    pnpm prettier:fix

--- crates/lgn-analytics-gui/frontend/src/lib/client.ts
+++ crates/lgn-analytics-gui/frontend/src/lib/client.ts
@@ -1,5 +1,8 @@
-import { GrpcWebImpl, PerformanceAnalyticsClientImpl } from "@lgn/proto-telemetry/dist/analytics";
+import {
+  GrpcWebImpl,
+  PerformanceAnalyticsClientImpl,
+} from "@lgn/proto-telemetry/dist/analytics";
 
 export const client = new PerformanceAnalyticsClientImpl(
-    new GrpcWebImpl("http://" + location.hostname + ":9090", {})
+  new GrpcWebImpl("http://" + location.hostname + ":9090", {})
 );
