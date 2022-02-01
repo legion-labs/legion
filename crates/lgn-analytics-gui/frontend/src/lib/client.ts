4d295efe937b5a50d0eb17e9a84bd01e5ad0d9b3
Tim <timothee.nitsas@legionlabs.com>
2022-01-31 20:58:01 -0500

    Typescript client

---
+++ crates/lgn-analytics-gui/frontend/src/lib/client.ts
@@ -0,0 +1,5 @@
+import { GrpcWebImpl, PerformanceAnalyticsClientImpl } from "@lgn/proto-telemetry/dist/analytics";
+
+export const client = new PerformanceAnalyticsClientImpl(
+    new GrpcWebImpl("http://" + location.hostname + ":9090", {})
+);
