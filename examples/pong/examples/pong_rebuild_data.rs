2a0b9f6b7df171a60730d76c0a5b937c8e4c0e12
Marc Parenteau <marc.parenteau@legionlabs.com>
2022-02-02 14:20:38 -0500

    build derived resources

--- examples/pong/examples/pong_rebuild_data.rs
+++ examples/pong/examples/pong_rebuild_data.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 use lgn_content_store::{ContentStoreAddr, HddContentStore};
-//use lgn_data_build::DataBuildOptions;
+use lgn_data_build::DataBuildOptions;
 use lgn_data_compiler::compiler_node::CompilerRegistryOptions;
 use lgn_data_offline::{
     resource::{Project, ResourceRegistry, ResourceRegistryOptions},
@@ -13,7 +13,7 @@ use lgn_data_offline::{
 };
 use lgn_data_runtime::{manifest::Manifest, AssetRegistryOptions};
 use lgn_data_runtime::{Resource, ResourceId, ResourceTypeAndId};
-//use lgn_data_transaction::BuildManager;
+use lgn_data_transaction::BuildManager;
 use tokio::sync::Mutex;
 
 #[tokio::main]
@@ -44,28 +44,32 @@ async fn main() {
     let content_store = HddContentStore::open(ContentStoreAddr::from(build_dir.clone())).unwrap();
     let resource_registry = resource_registry.create_async_registry();
 
+    let resource_ids = create_offline_data(&mut project, &resource_registry).await;
+
     let mut asset_registry = AssetRegistryOptions::new()
         .add_device_dir(project.resource_dir())
         .add_device_cas(Box::new(content_store), Manifest::default());
     sample_data::offline::add_loaders(&mut asset_registry);
     generic_data::offline::add_loaders(&mut asset_registry);
     lgn_scripting::offline::add_loaders(&mut asset_registry);
-    let _asset_registry = asset_registry.create();
+    let asset_registry = asset_registry.create();
 
-    let _compilers = CompilerRegistryOptions::default()
+    let compilers = CompilerRegistryOptions::default()
         .add_compiler(&lgn_compiler_runtime_entity::COMPILER_INFO)
         .add_compiler(&lgn_compiler_debugcube::COMPILER_INFO)
         .add_compiler(&lgn_compiler_script2asm::COMPILER_INFO);
 
-    // let options = DataBuildOptions::new(&build_dir, compilers)
-    //     .content_store(&ContentStoreAddr::from(build_dir.as_path()))
-    //     .asset_registry(asset_registry.clone());
+    let options = DataBuildOptions::new(&build_dir, compilers)
+        .content_store(&ContentStoreAddr::from(build_dir.as_path()))
+        .asset_registry(asset_registry.clone());
 
-    // let build_manager = BuildManager::new(options, &project, Manifest::default())
-    //     .await
-    //     .unwrap();
+    let mut build_manager = BuildManager::new(options, &project, Manifest::default())
+        .await
+        .unwrap();
 
-    create_offline_data(&mut project, &resource_registry).await;
+    for id in resource_ids {
+        build_manager.build_all_derived(id, &project).await.unwrap();
+    }
 }
 
 fn clean_folders(project_dir: impl AsRef<Path>) {
@@ -94,7 +98,7 @@ fn clean_folders(project_dir: impl AsRef<Path>) {
 async fn create_offline_data(
     project: &mut Project,
     resource_registry: &Arc<Mutex<ResourceRegistry>>,
-) {
+) -> Vec<ResourceTypeAndId> {
     // ground
     let ground_path_id = {
         let mut resources = resource_registry.lock().await;
@@ -217,7 +221,7 @@ async fn create_offline_data(
     };
 
     // scene
-    let _scene_id = {
+    let scene_id = {
         let mut resources = resource_registry.lock().await;
         let id = ResourceTypeAndId {
             kind: sample_data::offline::Entity::TYPE,
@@ -246,7 +250,7 @@ async fn create_offline_data(
     };
 
     // script
-    let _script_path_id = {
+    let script_id = {
         let mut resources = resource_registry.lock().await;
         let id = ResourceTypeAndId {
             kind: lgn_scripting::offline::Script::TYPE,
@@ -283,7 +287,8 @@ async fn create_offline_data(
             )
             .await
             .unwrap();
-        let path: ResourcePathId = id.into();
-        path.push(lgn_scripting::runtime::Script::TYPE)
+        id
     };
+
+    vec![scene_id, script_id]
 }
