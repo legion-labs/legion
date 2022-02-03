89f37c26b2c2b7154a854e5309efe382906d894a
Marc Parenteau <marc.parenteau@legionlabs.com>
2022-02-03 11:16:05 -0500

    add scene, step physics in update

--- crates/lgn-physics/src/lib.rs
+++ crates/lgn-physics/src/lib.rs
@@ -7,9 +7,30 @@ use lgn_app::prelude::*;
 use lgn_ecs::prelude::*;
 use physx::prelude::*;
 
+// type aliases
+
 type PxAllocator = physx::foundation::DefaultAllocator;
 type PxMaterial = physx::material::PxMaterial<()>;
 type PxShape = physx::shape::PxShape<(), PxMaterial>;
+type PxArticulationLink = physx::articulation_link::PxArticulationLink<(), PxShape>;
+type PxRigidStatic = physx::rigid_static::PxRigidStatic<(), PxShape>;
+type PxRigidDynamic = physx::rigid_dynamic::PxRigidDynamic<(), PxShape>;
+type PxArticulation = physx::articulation::PxArticulation<(), PxArticulationLink>;
+type PxArticulationReducedCoordinate =
+    physx::articulation_reduced_coordinate::PxArticulationReducedCoordinate<(), PxArticulationLink>;
+type PxScene = physx::scene::PxScene<
+    (),
+    PxArticulationLink,
+    PxRigidStatic,
+    PxRigidDynamic,
+    PxArticulation,
+    PxArticulationReducedCoordinate,
+    OnCollision,
+    OnTrigger,
+    OnConstraintBreak,
+    OnWakeSleep,
+    OnAdvance,
+>;
 
 #[derive(Default)]
 pub struct PhysicsPlugin {}
@@ -29,15 +50,78 @@ impl Plugin for PhysicsPlugin {
 
 impl PhysicsPlugin {
     fn setup(mut commands: Commands<'_, '_>) {
-        // Holds a PxFoundation and a PxPhysics.
-        // Also has an optional Pvd and transport, not enabled by default.
-        // The default allocator is the one provided by PhysX.
-        let physics = PhysicsFoundation::<PxAllocator, PxShape>::default();
+        let mut physics = PhysicsFoundation::<PxAllocator, PxShape>::default();
+
+        let scene: Owner<PxScene> = physics
+            .create(SceneDescriptor {
+                gravity: PxVec3::new(0.0, -9.81, 0.0),
+                on_advance: Some(OnAdvance),
+                ..SceneDescriptor::new(())
+            })
+            .unwrap();
 
         commands.insert_resource(physics);
+        commands.insert_resource(scene);
     }
 
-    fn update(physics: Res<'_, PhysicsFoundation<PxAllocator, PxShape>>) {
-        drop(physics);
+    fn update(
+        // physics: Res<'_, PhysicsFoundation<PxAllocator, PxShape>>,
+        mut scene: ResMut<'_, Owner<PxScene>>,
+    ) {
+        #[allow(unsafe_code)]
+        let mut scratch = unsafe { ScratchBuffer::new(4) };
+        scene
+            .step(
+                0.1,
+                None::<&mut physx_sys::PxBaseTask>,
+                Some(&mut scratch),
+                true,
+            )
+            .expect("error occurred during simulation");
+
+        let _actors = scene.get_dynamic_actors();
+
+        // drop(physics);
+        drop(scene);
+    }
+}
+
+// callbacks
+
+struct OnCollision;
+impl CollisionCallback for OnCollision {
+    fn on_collision(
+        &mut self,
+        _header: &physx_sys::PxContactPairHeader,
+        _pairs: &[physx_sys::PxContactPair],
+    ) {
+    }
+}
+struct OnTrigger;
+impl TriggerCallback for OnTrigger {
+    fn on_trigger(&mut self, _pairs: &[physx_sys::PxTriggerPair]) {}
+}
+
+struct OnConstraintBreak;
+impl ConstraintBreakCallback for OnConstraintBreak {
+    fn on_constraint_break(&mut self, _constraints: &[physx_sys::PxConstraintInfo]) {}
+}
+struct OnWakeSleep;
+impl WakeSleepCallback<PxArticulationLink, PxRigidStatic, PxRigidDynamic> for OnWakeSleep {
+    fn on_wake_sleep(
+        &mut self,
+        _actors: &[&physx::actor::ActorMap<PxArticulationLink, PxRigidStatic, PxRigidDynamic>],
+        _is_waking: bool,
+    ) {
+    }
+}
+
+struct OnAdvance;
+impl AdvanceCallback<PxArticulationLink, PxRigidDynamic> for OnAdvance {
+    fn on_advance(
+        &self,
+        _actors: &[&physx::rigid_body::RigidBodyMap<PxArticulationLink, PxRigidDynamic>],
+        _transforms: &[PxTransform],
+    ) {
     }
 }
