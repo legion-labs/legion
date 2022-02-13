e2d05ded23db90684a0c208a377605b5ff0b0e23
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2022-02-13 01:25:35 -0500

    Add unified clang-format path for proto files (#1014)
    * Add unified clang-format path for proto files
    
    * ignoring target
    
    * Add ignore clang-format to determinator

--- build/monorepo/src/vscode.rs
+++ build/monorepo/src/vscode.rs
@@ -163,8 +163,6 @@ pub fn run(args: &Args, ctx: &Context) -> Result<()> {
 
     let settings_file = ctx.workspace_root().join(".vscode").join("settings.json");
     let settings = json!({
-        // Read by the vscode extension zxh404.vscode-proto3
-        "clang-format.style": "{BasedOnStyle: Google}",
         "editor.formatOnSave": true,
         "files.eol": "\n",
         "protoc": {
