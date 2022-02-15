0d92277ad87a93f3c2f42fa2c6d6f7c3146a4bdb
mad <mad@legionlabs.com>
2022-02-15 14:09:32 -0500

    use analytics_web_client to auth user in analytics

--- crates/lgn-analytics-gui/frontend/src/index.ts
+++ crates/lgn-analytics-gui/frontend/src/index.ts
@@ -2,6 +2,8 @@ import "./assets/index.css";
 import { run } from "@lgn/web-client";
 import App from "./App.svelte";
 
+const redirectUri = document.location.origin + "/"
+
 run({
   appComponent: App,
   auth: {
@@ -9,20 +11,15 @@ run({
     redirectionTitle: "Home",
     issuerUrl:
       "https://cognito-idp.ca-central-1.amazonaws.com/ca-central-1_SkZKDimWz",
-    redirectUri: "http://localhost:3000/",
-    clientId: "5m58nrjfv6kr144prif9jk62di",
+    redirectUri: redirectUri,
+    clientId: "2kp01gr54dfc7qp1325hibcro3",
     login: {
       cookies: {
-        accessToken: "analytics_access_token",
-        refreshToken: "analytics_refresh_token",
-      },
-      extraParams: {
-        identity_provider: "Azure",
+        accessToken: "analytics_access_token_v2",
+        refreshToken: "analytics_refresh_token_v2",
       },
       scopes: [
-        "aws.cognito.signin.user.admin",
         "email",
-        "https://legionlabs.com/editor/allocate",
         "openid",
         "profile",
       ],
