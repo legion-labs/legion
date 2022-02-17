5614709f6bbc34f67f4a179e70157cc8edbc957c
Tim <98831755+timnitsas-legion@users.noreply.github.com>
2022-02-17 17:07:13 -0500

    Using legion orange

--- crates/lgn-analytics-gui/frontend/src/components/User.svelte
+++ crates/lgn-analytics-gui/frontend/src/components/User.svelte
@@ -1,20 +1,10 @@
 <script lang="ts">
   export let user: string;
-
-  function hashString(string: string): number {
-    let hash = 0;
-    for (let i = 0; i < string.length; i++) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-      hash = hash & hash;
-    }
-    return hash;
-  }
 </script>
 
 <div class="flex gap-2 items-center">
   <div
-    class="w-6 h-6 flex justify-center items-center rounded-full text-xs text-white"
-    style="background:hsl({hashString(user) % 360}, 65%, 50%)"
+    class="w-6 h-6 flex justify-center items-center rounded-full text-xs text-white bg-orange-700"
   >
     <span class="capitalize">{user.charAt(0)}</span>
   </div>
