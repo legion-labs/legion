fee66ef4d971bb836f39d6c4fd71331a267e1dfa
Tim <98831755+timnitsas-legion@users.noreply.github.com>
2022-02-17 13:42:44 -0500

    Quick colors

--- crates/lgn-analytics-gui/frontend/src/components/User.svelte
+++ crates/lgn-analytics-gui/frontend/src/components/User.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
   export let user: string;
+
+  function hashString(string: String): number {
+    let hash = 0;
+    for (let i = 0; i < string.length; i++) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+      hash = hash & hash;
+    }
+    return hash;
+  }
 </script>
 
 <div class="flex gap-2 items-center">
   <div
-    class="w-6 h-6 flex justify-center items-center rounded-full bg-gray-500 text-xs text-white"
+    class="w-6 h-6 flex justify-center items-center rounded-full text-xs text-white"
+    style="background:hsla({hashString(user) % 360}, 65%, 50%, 1)"
   >
     <span class="capitalize">{user.charAt(0)}</span>
   </div>
