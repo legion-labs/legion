c5b19e1cad17973ec7eb274e12d25de533659968
mad <mad@legionlabs.com>
2022-02-18 13:08:10 -0500

    analytics : use https on alb by default

--- crates/lgn-analytics-gui/frontend/src/lib/client.ts
+++ crates/lgn-analytics-gui/frontend/src/lib/client.ts
@@ -6,16 +6,16 @@ import { grpc } from "@improbable-eng/grpc-web";
 import { authClient } from "@lgn/web-client/src/lib/auth";
 
 export function getRemoteHost() {
-  const remoteHost =
-    "analytics-nlb-cddd70eafd32d85b.elb.ca-central-1.amazonaws.com";
-  // const remoteHost = "analytics-api.playground.legionlabs.com";
+  // const remoteHost =
+  //   "analytics-nlb-cddd70eafd32d85b.elb.ca-central-1.amazonaws.com";
+  const remoteHost = "analytics-api.playground.legionlabs.com";
   // const remoteHost = "localhost";
   return remoteHost;
 }
 
 export function getUrl() {
-  // return "https://" + getRemoteHost() + ":443";
-  return "http://" + getRemoteHost() + ":9090";
+  return "https://" + getRemoteHost() + ":9090";
+  // return "http://" + getRemoteHost() + ":9090";
 }
 
 export async function makeGrpcClient() {
