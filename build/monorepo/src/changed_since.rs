68e296ed4892831e9fbd43638c3989a638c4e328
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2022-02-28 01:46:57 -0500

    Workflow refactoring

--- build/monorepo/src/changed_since.rs
+++ build/monorepo/src/changed_since.rs
@@ -3,44 +3,81 @@
 
 use crate::git::GitCli;
 use crate::{context::Context, Error, Result};
+use clap::ArgEnum;
 use determinator::{Determinator, DeterminatorSet};
 use guppy::graph::{cargo::CargoResolverVersion, DependencyDirection};
 use lgn_tracing::dispatch::{flush_thread_buffer, init_thread_stream};
 use lgn_tracing::{span_fn, span_scope, trace};
+use serde_json::{json, to_string_pretty};
+use wildmatch::WildMatch;
+
+#[derive(ArgEnum, Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Format {
+    Text,
+    Json,
+    PrettyJson,
+}
+
+impl Default for Format {
+    fn default() -> Self {
+        Self::Text
+    }
+}
 
 #[derive(Debug, clap::Args)]
 pub struct Args {
     /// List packages changed since this commit
     pub(crate) base: String,
-    /// list of packages to match
-    #[clap(long, multiple_values = true)]
-    pub(crate) matches_any: Vec<String>,
+    /// format: text, json
+    #[clap(long, arg_enum, default_value = "text")]
+    pub(crate) format: Format,
 }
 
-#[span_fn]
+//#[span_fn]
 pub fn run(args: &Args, ctx: &Context) -> Result<()> {
     let git_cli = ctx.git_cli().map_err(|err| {
         err.with_explanation("changed-since` must be run within a project cloned from a git repo.")
     })?;
-    let determinator_set = changed_since_impl(git_cli, ctx, &args.base)?;
+    let changed_elements = changed_since_impl(git_cli, ctx, &args.base)?;
+    let package_names: Vec<_> = changed_elements
+        .determinator_set
+        .affected_set
+        .packages(DependencyDirection::Forward)
+        .map(|p| p.name())
+        .collect();
 
-    {
-        span_scope!("print_packages");
-        for package in determinator_set
-            .affected_set
-            .packages(DependencyDirection::Forward)
-        {
-            if args.matches_any.is_empty()
-                || args
-                    .matches_any
-                    .iter()
-                    .any(|candidate| package.name().contains(candidate))
-            {
-                println!("{}", package.name());
+    match args.format {
+        Format::Text => {
+            println!("docs: {}", changed_elements.docs);
+            println!("npm_pkgs: {}", changed_elements.docs);
+            println!("cargo_pkgs:");
+            for name in package_names {
+                println!(" - {}", name);
             }
         }
+        Format::Json | Format::PrettyJson => {
+            let json = json!({
+                "docs": changed_elements.docs,
+                "npm_pkgs": changed_elements.npm_pkgs,
+                "cargo_pkgs": package_names,
+            });
+            let s = if args.format == Format::PrettyJson {
+                to_string_pretty(&json)
+                    .map_err(|err| Error::new("failed to pretty print out json").with_source(err))?
+            } else {
+                json.to_string()
+            };
+            println!("{}", s);
+        }
     }
+
     Ok(())
+}
+
+pub(crate) struct ChangedElements<'g> {
+    pub(crate) docs: bool,
+    pub(crate) npm_pkgs: bool,
+    pub(crate) determinator_set: DeterminatorSet<'g>,
 }
 
 #[span_fn]
@@ -48,7 +85,7 @@ pub(crate) fn changed_since_impl<'g>(
     git_cli: &GitCli,
     ctx: &'g Context,
     base: &str,
-) -> Result<DeterminatorSet<'g>> {
+) -> Result<ChangedElements<'g>> {
     let thread_pool = rayon::ThreadPoolBuilder::new()
         .start_handler(|_tid| init_thread_stream())
         .exit_handler(|_tid| flush_thread_buffer())
@@ -91,15 +128,39 @@ pub(crate) fn changed_since_impl<'g>(
 
     let (old_graph, new_graph, files_changed) = (old_graph?, new_graph?, files_changed?);
 
+    let npm_pkgs_wild = [
+        WildMatch::new("crates/*/frontend"),
+        WildMatch::new("npm-pkgs/*"),
+        WildMatch::new("crates/lgn-*-proto"),
+    ];
+    let mut docs = false;
+    let mut npm_pkgs = false;
+    for file in files_changed.iter() {
+        if !docs && file.starts_with("docs/") {
+            docs = true;
+        }
+        if !npm_pkgs && npm_pkgs_wild.iter().any(|w| w.matches(file.as_str())) {
+            npm_pkgs = true;
+        }
+        if docs && npm_pkgs {
+            break;
+        }
+    }
+
     trace!("running determinator");
     span_scope!("running_determinator");
     let mut determinator = Determinator::new(&old_graph, new_graph);
     let mut cargo_options = Determinator::default_cargo_options();
-    determinator
+    let determinator_set = determinator
         .add_changed_paths(&files_changed)
         .set_cargo_options(cargo_options.set_resolver(CargoResolverVersion::V2))
         .set_rules(&ctx.config().determinator)
-        .map_err(|err| Error::new("failed setting the rules").with_source(err))?;
+        .map_err(|err| Error::new("failed setting the rules").with_source(err))?
+        .compute();
 
-    Ok(determinator.compute())
+    Ok(ChangedElements {
+        docs,
+        npm_pkgs,
+        determinator_set,
+    })
 }
