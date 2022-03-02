3bee0f3469b1e3a47db23548c5792591fc08a7a9
Marc Parenteau <45855830+R3DP1XL@users.noreply.github.com>
2022-03-02 16:58:37 -0500

    remove unneeded offline physics type registration

--- crates/lgn-editor-srv/src/main.rs
+++ crates/lgn-editor-srv/src/main.rs
@@ -146,13 +146,6 @@ fn main() {
         exit_on_close: false,
     });
 
-    // register missing types
-    let _ = lgn_physics::offline::PhysicsRigidBox::get_default_instance();
-    let _ = lgn_physics::offline::PhysicsRigidCapsule::get_default_instance();
-    let _ = lgn_physics::offline::PhysicsRigidHeightField::get_default_instance();
-    let _ = lgn_physics::offline::PhysicsRigidPlane::get_default_instance();
-    let _ = lgn_physics::offline::PhysicsRigidSphere::get_default_instance();
-
     if let Some(test_name) = args.test {
         match test_name.as_str() {
             "lifecycle" => {
