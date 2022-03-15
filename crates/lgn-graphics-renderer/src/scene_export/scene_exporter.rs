994224c14f238caa74c8e72567dccd24b0d94e9c
Kirill <kirill.daybov@legionlabs.com>
2022-03-15 17:20:09 -0400

    Export lights

--- crates/lgn-graphics-renderer/src/scene_export/scene_exporter.rs
+++ crates/lgn-graphics-renderer/src/scene_export/scene_exporter.rs
@@ -1,11 +1,16 @@
 use lgn_app::{App, EventReader, Events, Plugin};
-use lgn_ecs::prelude::{IntoExclusiveSystem, Query, Res, ResMut, Without};
+use lgn_ecs::prelude::{IntoExclusiveSystem, Local, Query, Res, ResMut, Without};
 use lgn_transform::components::GlobalTransform;
 
 use crate::{
-    components::{CameraComponent, LightComponent, ManipulatorComponent, VisualComponent},
+    components::{
+        CameraComponent, LightComponent, LightType, ManipulatorComponent, VisualComponent,
+    },
     egui::egui_plugin::Egui,
 };
+
+type KhrLight = gltf::json::extensions::scene::khr_lights_punctual::Light;
+type KhrLightType = gltf::json::extensions::scene::khr_lights_punctual::Type;
 
 pub(crate) struct SceneExporterPlugin {}
 
@@ -24,19 +29,175 @@ pub(crate) fn on_scene_export_requested(
     cameras: Query<'_, '_, (&CameraComponent, &GlobalTransform)>,
 ) {
     if !event_scene_export_requested.is_empty() {
-        unimplemented!();
+        let dir = format!(
+            "{}/scene_export",
+            event_scene_export_requested
+                .iter()
+                .next()
+                .unwrap()
+                .export_path
+        );
+        let _ = std::fs::create_dir_all(&dir).unwrap();
+
+        let mut nodes = Vec::new();
+        let mut scene_node_indices = Vec::new();
+        let writer = std::fs::File::create(format!("{}/scene.gltf", dir)).expect("I/O error");
+        let mut node_idx = 0;
+
+        let extensions = Some(export_extensions(
+            &mut nodes,
+            &lights,
+            &mut node_idx,
+            &mut scene_node_indices,
+        ));
+
+        let scene = gltf::json::Scene {
+            extensions: Default::default(),
+            extras: Default::default(),
+            name: Some("Legion scene".into()),
+            nodes: scene_node_indices,
+        };
+
+        let root = gltf::json::Root {
+            extensions,
+            //accessors: todo!(),
+            //animations: todo!(),
+            //asset: todo!(),
+            //buffers: todo!(),
+            //buffer_views: todo!(),
+            //scene: todo!(),
+            //extras: todo!(),
+            extensions_used: vec!["KHR_lights_punctual".into()],
+            extensions_required: vec!["KHR_lights_punctual".into()],
+            //cameras: todo!(),
+            //images: todo!(),
+            //materials: todo!(),
+            //meshes: todo!(),
+            nodes,
+            //samplers: todo!(),
+            scenes: vec![scene],
+            //skins: todo!(),
+            //textures: todo!(),
+            ..gltf::json::Root::default()
+        };
+        gltf::json::serialize::to_writer_pretty(writer, &root).expect("Serialization error");
+
+        //let bin = to_padded_byte_vector(triangle_vertices);
+        //let mut writer = fs::File::create("triangle/buffer0.bin").expect("I/O error");
+        //writer.write_all(&bin).expect("I/O error");
+    }
+}
+
+fn export_extensions(
+    nodes: &mut Vec<gltf::json::Node>,
+    lights: &Query<'_, '_, (&LightComponent, &GlobalTransform)>,
+    node_idx: &mut u32,
+    scene_node_indices: &mut Vec<gltf::json::Index<gltf::json::Node>>,
+) -> gltf::json::extensions::root::Root {
+    let mut light_idx = 0;
+    let mut khr_lights = Vec::new();
+    for (light, transform) in lights.iter() {
+        khr_lights.push(export_light(light));
+        let node = gltf::json::Node {
+            camera: None,
+            children: None,
+            extensions: Some(gltf::json::extensions::scene::Node {
+                khr_lights_punctual: Some(
+                    gltf::json::extensions::scene::khr_lights_punctual::KhrLightsPunctual {
+                        light: gltf::json::Index::new(light_idx),
+                    },
+                ),
+            }),
+            extras: Default::default(),
+            matrix: None,
+            mesh: None,
+            name: Some(format!("Light {} orientation", light_idx)),
+            rotation: Some(gltf::json::scene::UnitQuaternion(transform.rotation.into())),
+            scale: None,
+            translation: None,
+            skin: None,
+            weights: None,
+        };
+        nodes.push(node);
+        let node = gltf::json::Node {
+            camera: None,
+            children: Some(vec![gltf::json::Index::new(*node_idx)]),
+            extensions: None,
+            extras: Default::default(),
+            matrix: None,
+            mesh: None,
+            name: Some(format!("Light {}", light_idx)),
+            rotation: Some(gltf::json::scene::UnitQuaternion(transform.rotation.into())),
+            scale: None,
+            translation: Some(transform.translation.into()),
+            skin: None,
+            weights: None,
+        };
+        nodes.push(node);
+        scene_node_indices.push(gltf::json::Index::new(*node_idx + 1));
+        light_idx += 1;
+        *node_idx += 2;
+    }
+    let extensions = gltf::json::extensions::root::Root {
+        khr_lights_punctual: Some(gltf::json::extensions::root::KhrLightsPunctual {
+            lights: khr_lights,
+        }),
+    };
+    extensions
+}
+
+fn export_light(light: &LightComponent) -> KhrLight {
+    KhrLight {
+        color: light.color.to_array(),
+        extensions: None,
+        extras: gltf::json::Extras::default(),
+        intensity: light.radiance,
+        name: None,
+        range: None,
+        spot: match light.light_type {
+            LightType::Spotlight { cone_angle } => {
+                Some(gltf::json::extensions::scene::khr_lights_punctual::Spot {
+                    inner_cone_angle: cone_angle * 0.9,
+                    outer_cone_angle: cone_angle,
+                })
+            }
+            _ => None,
+        },
+        type_: gltf::json::validation::Checked::Valid(match light.light_type {
+            LightType::Directional => KhrLightType::Directional,
+            LightType::Omnidirectional => KhrLightType::Point,
+            LightType::Spotlight { cone_angle } => KhrLightType::Spot,
+        }),
+    }
+}
+
+pub(crate) struct UiData {
+    path: String,
+}
+
+impl Default for UiData {
+    fn default() -> Self {
+        UiData {
+            path: String::from("C:/scene_export"),
+        }
     }
 }
 
 pub(crate) fn ui_scene_exporter(
     egui: Res<'_, Egui>,
     mut event_scene_export_requested: ResMut<'_, Events<SceneExportRequested>>,
+    mut ui_data: Local<'_, UiData>,
 ) {
     egui::Window::new("Scene export").show(&egui.ctx, |ui| {
+        ui.text_edit_singleline(&mut ui_data.path);
         if ui.button("Export").clicked() {
-            event_scene_export_requested.send(SceneExportRequested {});
+            event_scene_export_requested.send(SceneExportRequested {
+                export_path: ui_data.path.clone(),
+            });
         }
     });
 }
 
-pub(crate) struct SceneExportRequested {}
+pub(crate) struct SceneExportRequested {
+    export_path: String,
+}
