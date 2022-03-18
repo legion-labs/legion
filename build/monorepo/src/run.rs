b667dd7e8451a72110730c30944062556230d948
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2022-03-18 00:21:00 -0400

    Cleanup chrome trace from monorepo tooling

--- build/monorepo/src/run.rs
+++ build/monorepo/src/run.rs
@@ -17,10 +17,6 @@ pub struct Args {
     pub(crate) package_args: SelectedPackageArgs,
     #[clap(flatten)]
     pub(crate) build_args: BuildArgs,
-    /// Dump a chrome trace file if possible
-    #[clap(long)]
-    #[allow(clippy::option_option)]
-    ctrace: Option<Option<String>>,
     #[clap(name = "ARGS", parse(from_os_str), last = true)]
     args: Vec<OsString>,
     /// Skip npm packages build,
@@ -43,50 +39,15 @@ pub fn run(args: &Args, ctx: &Context) -> Result<()> {
 
     let mut packages = args.package_args.to_selected_packages(ctx)?;
     let bin = args.build_args.bin.first();
-    let mut trace_name = bin.map_or_else(
-        || {
-            args.package_args
-                .package
-                .first()
-                .map_or_else(|| "app".to_owned(), ToOwned::to_owned)
-        },
-        ToOwned::to_owned,
-    );
     if let Some(bin) = bin {
         packages.select_package_from_bin(bin.as_str(), ctx)?;
     } else if let Some(example) = args.build_args.example.first() {
         packages.select_package_from_example(example.as_str(), ctx)?;
     }
-    let env = if let Some(trace_file) = &args.ctrace {
-        trace_name = format!(
-            "trace-{}-{}-{}.json",
-            trace_name,
-            std::time::SystemTime::UNIX_EPOCH
-                .elapsed()
-                .unwrap()
-                .as_secs(),
-            if args.build_args.release {
-                "release"
-            } else {
-                "debug"
-            }
-        )
-        .replace(":", "-"); // Windows doesn't like colons in the filename
-        vec![(
-            "LGN_TRACE_FILE",
-            Some(
-                trace_file
-                    .as_ref()
-                    .map_or_else(|| trace_name.as_str(), String::as_str),
-            ),
-        )]
-    } else {
-        vec![]
-    };
     let cmd = CargoCommand::Run {
         direct_args: &direct_args,
         args: &pass_through_args,
-        env: env.as_slice(),
+        env: &[],
     };
 
     // Npm packages related code
