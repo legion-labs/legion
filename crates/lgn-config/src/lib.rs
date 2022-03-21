f743c2ca05a48d099df61e1cd5ba7f4fa48d90b3
Julien Kauffmann <julien.kauffmann@legionlabs.com>
2022-03-21 10:54:17 -0400

    Added basic configuration reading

--- crates/lgn-config/src/lib.rs
+++ crates/lgn-config/src/lib.rs
@@ -1,270 +1,126 @@
-//! config create exposes the legion config file to applications
+mod errors;
 
-// crate-specific lint exceptions:
-#![allow(clippy::missing_errors_doc)]
+use std::path::Path;
 
-use std::env;
-use std::mem::discriminant;
-use std::path::{Path, PathBuf};
-use std::sync::RwLock;
+use config::Config;
+pub use errors::{Error, Result};
 
-use lgn_tracing::warn;
-use toml::value::{Table, Value};
+/// The default filename for configuration files.
+pub static DEFAULT_FILENAME: &str = "legion.toml";
 
-const DEFAULT_CONFIG_FILENAME: &str = "legion.toml";
-const LOCAL_CONFIG_FILENAME: &str = "legion_local.toml";
+/// Load the configuration from all its various sources.
+///
+/// If a configuration value is set in different sources, the value from the
+/// last read source will be used.
+///
+/// Namely, the configuration will be loaded from the following locations, in
+/// order:
+///
+/// - `/etc/legion-labs/config.toml` on UNIX.
+/// - `$XDG_CONFIG_HOME/legion-labs/config.toml` on UNIX.
+/// - `$HOME/.config/legion-labs/config.toml` on UNIX.
+/// - {FOLDERID_RoamingAppData}/legion-labs/legion.toml on Windows.
+/// - Any `legion.toml` file in the current working directory, or one of its
+/// parent directories, stopping as soon as a file is found.
+/// - Environment variables, starting with `LGN`.
+pub fn load_config() -> Result<Config> {
+    let root = std::env::current_dir()?;
 
-pub struct Config {
-    config_path: PathBuf,
-    entries: Table,
+    load_config_from(root)
 }
 
-impl Config {
-    #[allow(clippy::new_without_default)]
-    pub fn new() -> Self {
-        // Search for the CONFIG file from the current exec direction,
-        // walking up to the parents directory
-        let mut config_dir = env::current_exe().unwrap();
-        config_dir.pop();
+/// Load a configuration, using the specified root as a working-directory.
+///
+/// # Note
+///
+/// If root is a relative path, then the ancestors search will stop at the
+/// relative root. While useful for tests, it's probably safer to invoke that
+/// with an absolute path in any other case, always.
+fn load_config_from(root: impl AsRef<Path>) -> Result<Config> {
+    let mut builder = Config::builder();
 
-        let mut global_table: Option<Table> = None;
-
-        loop {
-            let default_config = config_dir.join(DEFAULT_CONFIG_FILENAME);
-            if default_config.is_file() {
-                if let Some(mut default_table) = Self::load_toml_table(default_config.as_path()) {
-                    let local_config = config_dir.join(LOCAL_CONFIG_FILENAME);
-                    if local_config.is_file() {
-                        // apply local override
-                        if let Some(overrides) = Self::load_toml_table(local_config.as_path()) {
-                            Self::merge_entries(overrides, &mut default_table);
-                        };
-                    }
-                    global_table = Some(default_table);
-                }
-            }
-
-            if global_table.is_some() || !config_dir.pop() {
-                break;
-            }
-        }
-
-        if global_table.is_none() {
-            warn!("Config file {:?} not found", DEFAULT_CONFIG_FILENAME);
-        }
-
-        Self {
-            config_path: config_dir,
-            entries: global_table.unwrap_or_default(),
-        }
+    // On Unix, always read the system-wide configuration file first if it
+    // exists.
+    if cfg!(unix) {
+        builder = builder
+            .add_source(config::File::with_name("/etc/legion-labs/legion.toml").required(false));
     }
 
-    fn merge_entries(apply: Table, dest: &mut Table) {
-        apply.into_iter().for_each(|(section_name, section_table)| {
-            if let Value::Table(section_table) = section_table {
-                if let Some(dest_section) = dest
-                    .entry(section_name)
-                    .or_insert(Value::Table(Table::default()))
-                    .as_table_mut()
-                {
-                    section_table.into_iter().for_each(|(key_name, value)| {
-                        dest_section.insert(key_name, value); // override value in global table
-                    });
-                }
-            }
-        });
+    // If we have usr configuration folder, try to read from it.
+    if let Some(config_dir) = dirs::config_dir() {
+        let config_file_path = config_dir.join("legion-labs").join(DEFAULT_FILENAME);
+        builder = builder.add_source(config::File::from(config_file_path).required(false));
     }
 
-    fn load_toml_table(filename: &Path) -> Option<Table> {
-        std::fs::read_to_string(&filename)
-            .map_err(|err| format!("Failed to read TOML file {:?}: {}", &filename, err))
-            .and_then(|config_toml| {
-                config_toml
-                    .parse::<Value>()
-                    .map_err(|err| format!("Failed to parse TOML file {:?}: {}", &filename, err))
-                    .and_then(|table| {
-                        table
-                            .try_into::<Table>()
-                            .map_err(|err| format!("Invalid TOML format {:?}: {}", &filename, err))
-                    })
-            })
-            .map_err(|err| {
-                warn!("{}", err);
-                err
-            })
-            .ok()
-    }
+    // Then, try to read the closest file we found.
+    for dir in root.as_ref().ancestors() {
+        let config_file_path = dir.join(DEFAULT_FILENAME);
 
-    fn find_table_entry<'a>(&'a self, property_name: &str) -> Option<&'a Value> {
-        let mut parts = property_name.split('.');
-
-        if let Some(first_part) = parts.next() {
-            let mut node: &toml::Value = self.entries.get(first_part)?;
-
-            for part in parts {
-                if let toml::Value::Table(table) = node {
-                    if let Some(value) = table.get(part) {
-                        node = value;
-                    } else {
-                        warn!("Configs entry not found: {}", property_name);
-                        return None;
-                    }
-                }
-            }
-
-            Some(node)
-        } else {
-            warn!("Configs entry not found: {}", property_name);
-            None
+        if std::fs::metadata(&config_file_path).is_ok() {
+            builder = builder.add_source(config::File::from(config_file_path).required(false));
+            break;
         }
     }
 
-    fn find_table_entry_mut<'a>(&'a mut self, property_name: &str) -> Option<&'a mut Value> {
-        let mut parts = property_name.split('.');
+    // Finally, read from environment variables, starting with `LGN`.
+    let config = builder
+        .add_source(config::Environment::with_prefix("LGN").try_parsing(true))
+        .build()
+        .map_err(Error::Config)?;
 
-        if let Some(first_part) = parts.next() {
-            let mut node: &mut toml::Value = self.entries.get_mut(first_part)?;
-
-            for part in parts {
-                if let toml::Value::Table(table) = node {
-                    if let Some(value) = table.get_mut(part) {
-                        node = value;
-                    } else {
-                        warn!("Configs entry not found: {}", property_name);
-                        return None;
-                    }
-                }
-            }
-
-            Some(node)
-        } else {
-            warn!("Configs entry not found: {}", property_name);
-            None
-        }
-    }
-
-    pub fn get<'de, T>(&self, key: &str) -> Option<T>
-    where
-        T: serde::Deserialize<'de>,
-    {
-        self.find_table_entry(key)
-            .and_then(|value_entry| value_entry.clone().try_into::<T>().ok())
-    }
-
-    pub fn get_or<'a, T>(&'a self, key: &str, default_value: T) -> T
-    where
-        T: serde::Deserialize<'a>,
-    {
-        self.get(key).unwrap_or(default_value)
-    }
-
-    pub fn set<T>(&mut self, key: &str, value: T) -> bool
-    where
-        T: serde::Serialize,
-    {
-        self.find_table_entry_mut(key)
-            .map(|entry_value| match Value::try_from(value) {
-                Ok(new_value) if discriminant(&new_value) == discriminant(entry_value) => {
-                    *entry_value = new_value;
-                    Some(entry_value)
-                }
-                _ => None,
-            })
-            .is_some()
-    }
-
-    pub fn get_absolute_path(&self, key: &str) -> Option<PathBuf> {
-        self.find_table_entry(key)
-            .and_then(Value::as_str)
-            .map(|str| self.config_path.join(str))
-    }
-
-    /// Returns the path containing the configuration.
-    pub fn config_path(&self) -> &Path {
-        &self.config_path
-    }
+    Ok(config)
 }
 
-lazy_static::lazy_static! {
-    pub static ref CONFIGS : RwLock<Config> = RwLock::new(Config::new());
-}
+#[cfg(test)]
+mod tests {
+    use serde::{Deserialize, Serialize};
 
-#[macro_export]
-macro_rules! config_set {
-    ($param:literal, $val:expr ) => {
-        $crate::CONFIGS.write().unwrap().set($param, $val)
-    };
-}
+    use super::*;
 
-#[macro_export]
-macro_rules! config_get {
-    ($param:literal) => {
-        $crate::CONFIGS.read().unwrap().get($param)
-    };
-}
+    #[derive(Serialize, Deserialize, Debug)]
+    struct MyConfig {
+        my_bool: bool,
+        my_int: i64,
+        my_float: f64,
+        my_list: Vec<String>,
+    }
 
-#[macro_export]
-macro_rules! config_get_or {
-    ($param:literal, $def:expr) => {
-        $crate::CONFIGS.read().unwrap().get_or($param, $def)
-    };
-}
+    #[test]
+    fn test_load_config_from() {
+        let config = load_config_from("src/fixtures/prod").unwrap();
 
-#[test]
-fn test_config() {
-    use std::collections::HashMap;
+        assert_eq!(
+            "prod",
+            config.get_string("lgn-config.tests.environment").unwrap()
+        );
+        assert!(config.get_string("lgn-config.tests.non-existing").is_err());
+    }
 
-    let configs = Config::new();
+    #[test]
+    fn test_load_config_from_with_environment_variable_override() {
+        // Lets set en environment variable, as an override.
+        std::env::set_var("LGN_LGN-CONFIG.TESTS.ENVIRONMENT", "foo");
+        let config = load_config_from("src/fixtures/prod").unwrap();
+        std::env::remove_var("LGN_LGN-CONFIG.TESTS.ENVIRONMENT");
 
-    configs.get_absolute_path("editor_srv.project_dir").unwrap();
+        assert_eq!(
+            "foo",
+            config.get_string("lgn-config.tests.environment").unwrap()
+        );
+    }
 
-    let test_string: String = configs.get("test_config.test_string").unwrap();
-    assert_eq!(test_string, "TestString");
+    #[test]
+    fn test_load_config_from_with_struct() {
+        let config = load_config_from("src/fixtures").unwrap();
 
-    let test_bool: bool = configs.get("test_config.test_bool").unwrap();
-    assert!(!test_bool);
+        let my_config: MyConfig = config.get("lgn-config.tests.my_config").unwrap();
 
-    let test_int: i32 = configs.get("test_config.test_int").unwrap();
-    assert_eq!(test_int, 1337);
-
-    let test_float: f32 = configs.get("test_config.test_float").unwrap();
-    assert!((test_float - 1337.1337f32).abs() < f32::EPSILON);
-
-    let test_sub_config: i32 = configs.get("test_config.sub_config.test_nested").unwrap();
-    assert_eq!(test_sub_config, 42);
-
-    let test_config: HashMap<String, toml::Value> = configs.get("test_config").unwrap();
-    assert_eq!(
-        test_config,
-        [
-            (
-                "test_string".to_string(),
-                toml::Value::String("TestString".to_string())
-            ),
-            ("test_bool".to_string(), toml::Value::Boolean(false)),
-            ("test_int".to_string(), toml::Value::Integer(1337)),
-            ("test_float".to_string(), toml::Value::Float(1337.1337f64)),
-            (
-                "sub_config".to_string(),
-                toml::Value::Table(
-                    [("test_nested".to_string(), toml::Value::Integer(42),)]
-                        .into_iter()
-                        .collect()
-                )
-            ),
-        ]
-        .into_iter()
-        .collect(),
-    );
-}
-
-#[test]
-fn test_singleton_configs() {
-    let mut test_int: i32 = config_get!("test_config.test_int").unwrap();
-    assert_eq!(test_int, 1337);
-
-    assert!(config_set!("test_config.test_int", 1));
-
-    test_int = config_get!("test_config.test_int").unwrap();
-    assert_eq!(test_int, 1);
+        assert_eq!(true, my_config.my_bool);
+        assert_eq!(42, my_config.my_int);
+        assert_eq!(3.14, my_config.my_float);
+        assert_eq!(
+            vec!["a".to_string(), "b".to_string(), "c".to_string()],
+            my_config.my_list
+        );
+    }
 }
---
+++ crates/lgn-config/src/old.rs
@@ -0,0 +1,270 @@
+//! config create exposes the legion config file to applications
+
+// crate-specific lint exceptions:
+#![allow(clippy::missing_errors_doc)]
+
+use std::env;
+use std::mem::discriminant;
+use std::path::{Path, PathBuf};
+use std::sync::RwLock;
+
+use lgn_tracing::warn;
+use toml::value::{Table, Value};
+
+const DEFAULT_CONFIG_FILENAME: &str = "legion.toml";
+const LOCAL_CONFIG_FILENAME: &str = "legion_local.toml";
+
+pub struct Config {
+    config_path: PathBuf,
+    entries: Table,
+}
+
+impl Config {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        // Search for the CONFIG file from the current exec direction,
+        // walking up to the parents directory
+        let mut config_dir = env::current_exe().unwrap();
+        config_dir.pop();
+
+        let mut global_table: Option<Table> = None;
+
+        loop {
+            let default_config = config_dir.join(DEFAULT_CONFIG_FILENAME);
+            if default_config.is_file() {
+                if let Some(mut default_table) = Self::load_toml_table(default_config.as_path()) {
+                    let local_config = config_dir.join(LOCAL_CONFIG_FILENAME);
+                    if local_config.is_file() {
+                        // apply local override
+                        if let Some(overrides) = Self::load_toml_table(local_config.as_path()) {
+                            Self::merge_entries(overrides, &mut default_table);
+                        };
+                    }
+                    global_table = Some(default_table);
+                }
+            }
+
+            if global_table.is_some() || !config_dir.pop() {
+                break;
+            }
+        }
+
+        if global_table.is_none() {
+            warn!("Config file {:?} not found", DEFAULT_CONFIG_FILENAME);
+        }
+
+        Self {
+            config_path: config_dir,
+            entries: global_table.unwrap_or_default(),
+        }
+    }
+
+    fn merge_entries(apply: Table, dest: &mut Table) {
+        apply.into_iter().for_each(|(section_name, section_table)| {
+            if let Value::Table(section_table) = section_table {
+                if let Some(dest_section) = dest
+                    .entry(section_name)
+                    .or_insert(Value::Table(Table::default()))
+                    .as_table_mut()
+                {
+                    section_table.into_iter().for_each(|(key_name, value)| {
+                        dest_section.insert(key_name, value); // override value in global table
+                    });
+                }
+            }
+        });
+    }
+
+    fn load_toml_table(filename: &Path) -> Option<Table> {
+        std::fs::read_to_string(&filename)
+            .map_err(|err| format!("Failed to read TOML file {:?}: {}", &filename, err))
+            .and_then(|config_toml| {
+                config_toml
+                    .parse::<Value>()
+                    .map_err(|err| format!("Failed to parse TOML file {:?}: {}", &filename, err))
+                    .and_then(|table| {
+                        table
+                            .try_into::<Table>()
+                            .map_err(|err| format!("Invalid TOML format {:?}: {}", &filename, err))
+                    })
+            })
+            .map_err(|err| {
+                warn!("{}", err);
+                err
+            })
+            .ok()
+    }
+
+    fn find_table_entry<'a>(&'a self, property_name: &str) -> Option<&'a Value> {
+        let mut parts = property_name.split('.');
+
+        if let Some(first_part) = parts.next() {
+            let mut node: &toml::Value = self.entries.get(first_part)?;
+
+            for part in parts {
+                if let toml::Value::Table(table) = node {
+                    if let Some(value) = table.get(part) {
+                        node = value;
+                    } else {
+                        warn!("Configs entry not found: {}", property_name);
+                        return None;
+                    }
+                }
+            }
+
+            Some(node)
+        } else {
+            warn!("Configs entry not found: {}", property_name);
+            None
+        }
+    }
+
+    fn find_table_entry_mut<'a>(&'a mut self, property_name: &str) -> Option<&'a mut Value> {
+        let mut parts = property_name.split('.');
+
+        if let Some(first_part) = parts.next() {
+            let mut node: &mut toml::Value = self.entries.get_mut(first_part)?;
+
+            for part in parts {
+                if let toml::Value::Table(table) = node {
+                    if let Some(value) = table.get_mut(part) {
+                        node = value;
+                    } else {
+                        warn!("Configs entry not found: {}", property_name);
+                        return None;
+                    }
+                }
+            }
+
+            Some(node)
+        } else {
+            warn!("Configs entry not found: {}", property_name);
+            None
+        }
+    }
+
+    pub fn get<'de, T>(&self, key: &str) -> Option<T>
+    where
+        T: serde::Deserialize<'de>,
+    {
+        self.find_table_entry(key)
+            .and_then(|value_entry| value_entry.clone().try_into::<T>().ok())
+    }
+
+    pub fn get_or<'a, T>(&'a self, key: &str, default_value: T) -> T
+    where
+        T: serde::Deserialize<'a>,
+    {
+        self.get(key).unwrap_or(default_value)
+    }
+
+    pub fn set<T>(&mut self, key: &str, value: T) -> bool
+    where
+        T: serde::Serialize,
+    {
+        self.find_table_entry_mut(key)
+            .map(|entry_value| match Value::try_from(value) {
+                Ok(new_value) if discriminant(&new_value) == discriminant(entry_value) => {
+                    *entry_value = new_value;
+                    Some(entry_value)
+                }
+                _ => None,
+            })
+            .is_some()
+    }
+
+    pub fn get_absolute_path(&self, key: &str) -> Option<PathBuf> {
+        self.find_table_entry(key)
+            .and_then(Value::as_str)
+            .map(|str| self.config_path.join(str))
+    }
+
+    /// Returns the path containing the configuration.
+    pub fn config_path(&self) -> &Path {
+        &self.config_path
+    }
+}
+
+lazy_static::lazy_static! {
+    pub static ref CONFIGS : RwLock<Config> = RwLock::new(Config::new());
+}
+
+#[macro_export]
+macro_rules! config_set {
+    ($param:literal, $val:expr ) => {
+        $crate::CONFIGS.write().unwrap().set($param, $val)
+    };
+}
+
+#[macro_export]
+macro_rules! config_get {
+    ($param:literal) => {
+        $crate::CONFIGS.read().unwrap().get($param)
+    };
+}
+
+#[macro_export]
+macro_rules! config_get_or {
+    ($param:literal, $def:expr) => {
+        $crate::CONFIGS.read().unwrap().get_or($param, $def)
+    };
+}
+
+#[test]
+fn test_config() {
+    use std::collections::HashMap;
+
+    let configs = Config::new();
+
+    configs.get_absolute_path("editor_srv.project_dir").unwrap();
+
+    let test_string: String = configs.get("test_config.test_string").unwrap();
+    assert_eq!(test_string, "TestString");
+
+    let test_bool: bool = configs.get("test_config.test_bool").unwrap();
+    assert!(!test_bool);
+
+    let test_int: i32 = configs.get("test_config.test_int").unwrap();
+    assert_eq!(test_int, 1337);
+
+    let test_float: f32 = configs.get("test_config.test_float").unwrap();
+    assert!((test_float - 1337.1337f32).abs() < f32::EPSILON);
+
+    let test_sub_config: i32 = configs.get("test_config.sub_config.test_nested").unwrap();
+    assert_eq!(test_sub_config, 42);
+
+    let test_config: HashMap<String, toml::Value> = configs.get("test_config").unwrap();
+    assert_eq!(
+        test_config,
+        [
+            (
+                "test_string".to_string(),
+                toml::Value::String("TestString".to_string())
+            ),
+            ("test_bool".to_string(), toml::Value::Boolean(false)),
+            ("test_int".to_string(), toml::Value::Integer(1337)),
+            ("test_float".to_string(), toml::Value::Float(1337.1337f64)),
+            (
+                "sub_config".to_string(),
+                toml::Value::Table(
+                    [("test_nested".to_string(), toml::Value::Integer(42),)]
+                        .into_iter()
+                        .collect()
+                )
+            ),
+        ]
+        .into_iter()
+        .collect(),
+    );
+}
+
+#[test]
+fn test_singleton_configs() {
+    let mut test_int: i32 = config_get!("test_config.test_int").unwrap();
+    assert_eq!(test_int, 1337);
+
+    assert!(config_set!("test_config.test_int", 1));
+
+    test_int = config_get!("test_config.test_int").unwrap();
+    assert_eq!(test_int, 1);
+}
