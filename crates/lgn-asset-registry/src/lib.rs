00d32e6c27064ea3bbc8f3d57730023520197c51
Marc Parenteau <45855830+R3DP1XL@users.noreply.github.com>
2022-03-22 18:01:55 -0400

    fixed a few world.get_x...unwrap

--- crates/lgn-asset-registry/src/lib.rs
+++ crates/lgn-asset-registry/src/lib.rs
@@ -54,9 +54,7 @@ impl Plugin for AssetRegistryPlugin {
 
 impl AssetRegistryPlugin {
     fn pre_setup(world: &mut World) {
-        let mut config = world
-            .get_resource_mut::<AssetRegistrySettings>()
-            .expect("Missing AssetRegistrySettings resource, must add to app");
+        let mut config = world.resource_mut::<AssetRegistrySettings>();
 
         let content_store_addr = config.content_store_addr.clone();
         let content_store = HddContentStore::open(content_store_addr).unwrap_or_else(|| {
