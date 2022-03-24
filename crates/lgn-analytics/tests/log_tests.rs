219b85d766c315bb1bcac23b3572dca91892bc8e
mad <mad@legionlabs.com>
2022-03-24 15:29:30 -0400

    parse test log event

--- crates/lgn-analytics/tests/log_tests.rs
+++ crates/lgn-analytics/tests/log_tests.rs
@@ -1,9 +1,12 @@
+use lgn_analytics::parse_block;
 use lgn_telemetry_sink::stream_block::StreamBlock;
 use lgn_telemetry_sink::stream_info::get_stream_info;
+use lgn_telemetry_sink::TelemetryGuard;
 use lgn_tracing::event::TracingBlock;
 use lgn_tracing::logs::LogBlock;
 use lgn_tracing::logs::LogStaticStrInteropEvent;
 use lgn_tracing::logs::LogStream;
+use lgn_tracing_transit::Value;
 use std::collections::HashMap;
 use std::sync::Arc;
 
@@ -31,11 +34,12 @@ fn test_log_interop_metadata() {
 
 #[test]
 fn test_log_encode() {
+    let _telemetry_guard = TelemetryGuard::default();
     let mut stream = LogStream::new(1024, String::from("bogus_process_id"), &[], HashMap::new());
     let stream_id = stream.stream_id().to_string();
     stream.get_events_mut().push(LogStaticStrInteropEvent {
-        time: 0,
-        level: 0,
+        time: 1,
+        level: 2,
         target: "target".into(),
         msg: "msg".into(),
     });
@@ -43,4 +47,17 @@ fn test_log_encode() {
     Arc::get_mut(&mut block).unwrap().close();
     let encoded = block.encode().unwrap();
     assert_eq!(encoded.nb_objects, 1);
+    let stream_info = get_stream_info(&stream);
+    parse_block(&stream_info, &encoded.payload.unwrap(), |val| {
+        if let Value::Object(obj) = val {
+            assert_eq!(obj.type_name.as_str(), "LogStaticStrInteropEvent");
+            assert_eq!(obj.get::<i64>("time").unwrap(), 1);
+            assert_eq!(obj.get::<u32>("level").unwrap(), 2);
+            dbg!(obj);
+        } else {
+            panic!("log entry not an object");
+        }
+        Ok(true)
+    })
+    .unwrap();
 }
