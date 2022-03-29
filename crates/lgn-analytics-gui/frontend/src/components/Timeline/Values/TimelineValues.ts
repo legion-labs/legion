5a75a75141ee3cda7e97886b55429efeaae137e6
Tim <98831755+timnitsas-legion@users.noreply.github.com>
2022-03-29 17:42:36 -0400

    Timeline: quick fix until getComputedStyle() investigation

--- crates/lgn-analytics-gui/frontend/src/components/Timeline/Values/TimelineValues.ts
+++ crates/lgn-analytics-gui/frontend/src/components/Timeline/Values/TimelineValues.ts
@@ -7,6 +7,7 @@ export const pixelMargin = 4;
 export const asyncTaskName = "async tasks";
 
 function getThreadItemLength() {
+  return 170;
   const style = getComputedStyle(document.body);
   const item = style.getPropertyValue("--thread-item-length");
   return Number.parseInt(item.replace("px", ""));
