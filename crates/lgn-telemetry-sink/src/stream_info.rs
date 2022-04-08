d737de368ccfa6451d49606e2582d0fc510c1ee0
mad <mad@legionlabs.com>
2022-04-08 10:55:19 -0400

    fixed typo

--- crates/lgn-telemetry-sink/src/stream_info.rs
+++ crates/lgn-telemetry-sink/src/stream_info.rs
@@ -15,8 +15,8 @@ where
         lgn_tracing_transit::HeterogeneousQueue,
 {
     let dependencies_meta =
-        make_queue_metedata::<<<Block as TracingBlock>::Queue as ExtractDeps>::DepsQueue>();
-    let obj_meta = make_queue_metedata::<Block::Queue>();
+        make_queue_metadata::<<<Block as TracingBlock>::Queue as ExtractDeps>::DepsQueue>();
+    let obj_meta = make_queue_metadata::<Block::Queue>();
     StreamProto {
         process_id: stream.process_id().to_owned(),
         stream_id: stream.stream_id().to_owned(),
@@ -53,7 +53,7 @@ fn proto_from_udt(
     }
 }
 
-fn make_queue_metedata<Queue: lgn_tracing_transit::HeterogeneousQueue>() -> ContainerMetadata {
+fn make_queue_metadata<Queue: lgn_tracing_transit::HeterogeneousQueue>() -> ContainerMetadata {
     let udts = Queue::reflect_contained();
     let mut secondary_types = HashMap::new();
     let mut types: Vec<UserDefinedTypeProto> = udts
