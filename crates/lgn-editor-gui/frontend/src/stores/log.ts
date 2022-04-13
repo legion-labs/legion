60c67b3e0f454cd067f4a59e4b3eb59bb231bc2a
Marc Parenteau <45855830+R3DP1XL@users.noreply.github.com>
2022-04-13 11:38:06 -0400

    merge log subscription lambdas

--- crates/lgn-editor-gui/frontend/src/stores/log.ts
+++ crates/lgn-editor-gui/frontend/src/stores/log.ts
@@ -1,8 +1,9 @@
 import { derived, get, writable } from "svelte/store";
 
 import { throttled } from "@lgn/web-client/src/lib/store";
-import type { LogEntry } from "@lgn/web-client/src/types/log";
+import type { LogEntry, Source } from "@lgn/web-client/src/types/log";
 import { severityFromLevel } from "@lgn/web-client/src/types/log";
+import type { TraceEvent } from "@lgn/proto-log-stream/dist/log_stream";
 
 import { initEditorLogStream, initRuntimeLogStream } from "@/api";
 
@@ -22,63 +23,46 @@ export const logEntries = throttled(
 );
 
 export function initLogStreams() {
+  const processLogStreamResponse = (
+    lagging: number | undefined,
+    traceEvent: TraceEvent | undefined,
+    source: Source
+  ) => {
+    if (get(streamedLogEntries).length > buffer - 1) {
+      get(streamedLogEntries).shift();
+    }
+
+    if (typeof lagging === "number") {
+      // TODO: Handle lagging messages
+
+      return;
+    }
+
+    if (traceEvent) {
+      // Defaulting to "trace" if the severity cannot be converted from the level
+      const severity = severityFromLevel(traceEvent.level) ?? "trace";
+
+      streamedLogEntries.update((streamedLogEntries) => [
+        ...streamedLogEntries,
+        {
+          severity,
+          message: traceEvent.message,
+          source,
+          target: traceEvent.target,
+          datetime: new Date(traceEvent.time),
+        },
+      ]);
+    }
+  };
+
   const editorSubscription = initEditorLogStream().subscribe(
     ({ lagging, traceEvent }) => {
-      if (get(streamedLogEntries).length > buffer - 1) {
-        get(streamedLogEntries).shift();
-      }
-
-      if (typeof lagging === "number") {
-        // TODO: Handle lagging messages
-
-        return;
-      }
-
-      if (traceEvent) {
-        // Defaulting to "trace" if the severity cannot be converted from the level
-        const severity = severityFromLevel(traceEvent.level) ?? "trace";
-
-        streamedLogEntries.update((streamedLogEntries) => [
-          ...streamedLogEntries,
-          {
-            severity,
-            message: traceEvent.message,
-            source: "editor",
-            target: traceEvent.target,
-            datetime: new Date(traceEvent.time),
-          },
-        ]);
-      }
+      processLogStreamResponse(lagging, traceEvent, "editor");
     }
   );
-
   const runtimeSubscription = initRuntimeLogStream().subscribe(
     ({ lagging, traceEvent }) => {
-      if (get(streamedLogEntries).length > buffer - 1) {
-        get(streamedLogEntries).shift();
-      }
-
-      if (typeof lagging === "number") {
-        // TODO: Handle lagging messages
-
-        return;
-      }
-
-      if (traceEvent) {
-        // Defaulting to "trace" if the severity cannot be converted from the level
-        const severity = severityFromLevel(traceEvent.level) ?? "trace";
-
-        streamedLogEntries.update((streamedLogEntries) => [
-          ...streamedLogEntries,
-          {
-            severity,
-            message: traceEvent.message,
-            source: "runtime",
-            target: traceEvent.target,
-            datetime: new Date(traceEvent.time),
-          },
-        ]);
-      }
+      processLogStreamResponse(lagging, traceEvent, "runtime");
     }
   );
 
