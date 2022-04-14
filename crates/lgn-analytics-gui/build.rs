a7b20e509ee096211fa48973f726e3136ad73a6b
Kévin <94377405+kevin-legion@users.noreply.github.com>
2022-04-14 10:05:57 -0400

    Analytics client: Move analytics to npm-pkgs from crates, drop Tauri

--- crates/lgn-analytics-gui/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(feature = "custom-protocol")]
-    lgn_build_utils_web::build_web_app()?;
-
-    tauri_build::build();
-
-    Ok(())
-}
