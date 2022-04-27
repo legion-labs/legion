c7e6c4e11d7ba72a1e49b9f9204f46685413ad95
Kévin <94377405+kevin-legion@users.noreply.github.com>
2022-04-27 18:27:40 -0400

    Analytics client: Improve theme handling further

--- npm-pkgs/lgn-analytics-gui/src/components/Timeline/Tools/TimelineSearch.svelte
+++ npm-pkgs/lgn-analytics-gui/src/components/Timeline/Tools/TimelineSearch.svelte
@@ -8,14 +8,16 @@
 <div>
   <input
     type="text"
-    class="metric-search border-0 text-sm text-content-60 bg-background"
+    class="metric-search border-0 text-sm text-opacity-60 bg-background"
     placeholder="Search..."
     on:focus={() => (searching = true)}
     on:blur={() => (searching = false)}
     bind:value={$searchStore}
   />
   <button
-    class="text-sm bg-background text-content-60 w-5"
-    on:click={() => searchStore.set("")}><i class="bi bi-x-circle" /></button
+    class="text-sm bg-background w-5 text-opacity-60"
+    on:click={() => searchStore.set("")}
   >
+    <i class="bi bi-x-circle" />
+  </button>
 </div>
