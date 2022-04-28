3465e82db71148d274d9017c11b9a84ffbf231e6
Tim <98831755+timnitsas-legion@users.noreply.github.com>
2022-04-28 15:31:32 -0400

    Buildfix

--- npm-pkgs/lgn-analytics-gui/src/App.svelte
+++ npm-pkgs/lgn-analytics-gui/src/App.svelte
@@ -9,13 +9,13 @@
   import { Route, Router } from "@/lib/navigator";
   import Health from "@/pages/Health.svelte";
 
-  import ProcessList from "./components/List/ProcessList.svelte";
   import Log from "./components/Log/Log.svelte";
   import MetricsCanvas from "./components/Metric/MetricCanvas.svelte";
   import Header from "./components/Misc/Header.svelte";
   import LoadingBar from "./components/Misc/LoadingBar.svelte";
   import TimelineRenderer from "./components/Timeline/Timeline.svelte";
   import { themeContextKey, themeStorageKey } from "./constants";
+  import ProcessPage from "./components/Process/ProcessPage.svelte";
 
   export let initAuthStatus: InitAuthStatus | null;
 
@@ -47,7 +47,7 @@
   <div class="pl-5 pr-5 pt-5 overflow-hidden">
     <Router>
       <Route path="/" primary={false}>
-        <ProcessList />
+        <ProcessPage />
       </Route>
       <Route path="/health">
         <Health />
