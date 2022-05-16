2dbce2bd85815993aee077ce52e5cccf802706cb
Florence <northeastflo@gmail.com>
2022-05-16 16:30:06 -0400

    Basic animation systsem skeleton

---
+++ crates/lgn-animation/src/animation_event.rs
@@ -0,0 +1,21 @@
+pub enum EventType {
+    Immediate,
+    Duration,
+    Both,
+}
+
+pub struct Event {
+    event_type: EventType,
+}
+
+impl Event {
+    #[inline]
+    fn is_immediate_event() {
+        /* */
+    }
+
+    #[inline]
+    fn is_duration_event() {
+        /* */
+    }
+}
