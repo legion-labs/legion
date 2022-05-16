6c2db5a65a5e00f1466646c0aa1631bc4e730d2d
Florence <northeastflo@gmail.com>
2022-05-16 16:30:06 -0400

    Basic animation systsem skeleton

---
+++ crates/lgn-animation/src/animation_frame_time.rs
@@ -0,0 +1,11 @@
+pub struct FrameTime {
+    frame_index: u32,
+    percentage_through: f32,
+}
+
+impl FrameTime {
+    #[inline]
+    fn is_exactly_at_key_frame() {
+        /* */
+    }
+}
---
+++ crates/lgn-animation/src/animation_graph_events.rs
@@ -0,0 +1,4 @@
+pub struct SampledEventRange {
+    start_idx: i16,
+    end_idx: i16,
+}
