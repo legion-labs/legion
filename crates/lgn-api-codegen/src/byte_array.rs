4e904c5d588d8515224651b43e488ad5c6e45534
ademange-legion <102982011+ademange-legion@users.noreply.github.com>
2022-05-17 09:10:37 -0400

    [codegen] Improve byte array support (#1798)
    * Improve support for byte arrays in codegen.

---
+++ crates/lgn-api-codegen/src/byte_array.rs
@@ -0,0 +1,66 @@
+use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+pub struct ByteArray(pub Vec<u8>);
+
+impl Serialize for ByteArray {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&base64::encode_config(&self.0, base64::URL_SAFE_NO_PAD))
+    }
+}
+
+impl<'de> Deserialize<'de> for ByteArray {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        base64::decode_config(&s, base64::URL_SAFE_NO_PAD)
+            .map(ByteArray)
+            .map_err(D::Error::custom)
+    }
+}
+
+impl From<Vec<u8>> for ByteArray {
+    fn from(v: Vec<u8>) -> Self {
+        Self(v)
+    }
+}
+
+impl From<ByteArray> for Vec<u8> {
+    fn from(v: ByteArray) -> Self {
+        v.0
+    }
+}
+
+impl AsRef<[u8]> for ByteArray {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl std::ops::Deref for ByteArray {
+    type Target = Vec<u8>;
+    fn deref(&self) -> &Vec<u8> {
+        &self.0
+    }
+}
+
+impl std::ops::DerefMut for ByteArray {
+    fn deref_mut(&mut self) -> &mut Vec<u8> {
+        &mut self.0
+    }
+}
+
+impl std::fmt::Display for ByteArray {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            base64::encode_config(&self.0, base64::URL_SAFE_NO_PAD)
+        )
+    }
+}
