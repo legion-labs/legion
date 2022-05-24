691384d48254aa81b78315105dd3a18d0c41aab5
Anthony Demange <anthony.demange@legionlabs.com>
2022-05-24 09:42:39 -0400

    Use common fixture file

--- crates/lgn-api-codegen/src/typescript/mod.rs
+++ crates/lgn-api-codegen/src/typescript/mod.rs
@@ -86,7 +86,7 @@ mod tests {
 
     #[test]
     fn test_ts_generation() {
-        let data = include_str!("./fixtures/openapi.yaml");
+        let data = include_str!("../fixtures/openapi.yaml");
         let api = Api::try_from(&serde_yaml::from_str(data).unwrap()).unwrap();
         let content = generate(&api).unwrap();
 
