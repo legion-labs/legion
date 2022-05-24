3e42380513c00275a808c9222f9bd39acf876b5b
mad <mad@legionlabs.com>
2022-05-24 10:56:54 -0400

    SpanTableLocalWriter

--- crates/lgn-analytics-srv/src/span_table.rs
+++ crates/lgn-analytics-srv/src/span_table.rs
@@ -15,8 +15,75 @@ use crate::call_tree::CallTreeBuilder;
 use crate::column::Column;
 use crate::thread_block_processor::parse_thread_block;
 
+pub struct SpanTableLocalWriter {
+    file_writer: SerializedFileWriter<std::fs::File>,
+}
+
+impl SpanTableLocalWriter {
+    pub fn create(file_path: &Path) -> Result<Self> {
+        let message_type = "
+  message schema {
+    REQUIRED INT32 hash;
+    REQUIRED INT32 depth;
+    REQUIRED DOUBLE begin_ms;
+    REQUIRED DOUBLE end_ms;
+    REQUIRED INT64 id;
+    REQUIRED INT64 parent;
+  }
+";
+        let schema =
+            Arc::new(parse_message_type(message_type).with_context(|| "parsing spans schema")?);
+        let props = Arc::new(WriterProperties::builder().build());
+        let file = std::fs::File::create(file_path)
+            .with_context(|| format!("creating file {}", file_path.display()))?;
+        let file_writer = SerializedFileWriter::new(file, schema, props)
+            .with_context(|| "creating parquet writer")?;
+        Ok(Self { file_writer })
+    }
+
+    pub fn close(mut self) -> Result<()> {
+        self.file_writer.close()?;
+        Ok(())
+    }
+
+    pub fn append(&mut self, spans: SpanRowGroup) -> Result<()> {
+        let mut row_group_writer = self
+            .file_writer
+            .next_row_group()
+            .with_context(|| "creating row group writer")?;
+        spans
+            .hashes
+            .write_batch(&mut *row_group_writer)
+            .with_context(|| "writing hash column")?;
+        spans
+            .depths
+            .write_batch(&mut *row_group_writer)
+            .with_context(|| "writing depth column")?;
+        spans
+            .begins
+            .write_batch(&mut *row_group_writer)
+            .with_context(|| "writing begins column")?;
+        spans
+            .ends
+            .write_batch(&mut *row_group_writer)
+            .with_context(|| "writing begins column")?;
+        spans
+            .ids
+            .write_batch(&mut *row_group_writer)
+            .with_context(|| "writing ids column")?;
+        spans
+            .parents
+            .write_batch(&mut *row_group_writer)
+            .with_context(|| "writing parents column")?;
+        self.file_writer
+            .close_row_group(row_group_writer)
+            .with_context(|| "closing row group")?;
+        Ok(())
+    }
+}
+
 #[derive(Debug)]
-pub struct SpanTable {
+pub struct SpanRowGroup {
     pub hashes: Column<i32>,
     pub depths: Column<i32>,
     pub begins: Column<f64>,
@@ -25,7 +92,7 @@ pub struct SpanTable {
     pub parents: Column<i64>,
 }
 
-impl SpanTable {
+impl SpanRowGroup {
     pub fn new() -> Self {
         Self {
             hashes: Column::new(),
@@ -84,7 +151,7 @@ fn make_rows_from_tree_impl<RowFun>(
     }
 }
 
-fn make_rows_from_tree(tree: &CallTreeNode, next_id: &mut u64, table: &mut SpanTable) {
+fn make_rows_from_tree(tree: &CallTreeNode, next_id: &mut u64, table: &mut SpanRowGroup) {
     if tree.hash == 0 {
         for child in &tree.children {
             make_rows_from_tree_impl(child, 0, 0, next_id, &mut |row| table.append(&row));
@@ -99,9 +166,9 @@ pub async fn make_span_table(
     blob_storage: Arc<dyn BlobStorage>,
     process_id: &str,
     convert_ticks: &ConvertTicks,
-) -> Result<SpanTable> {
+) -> Result<SpanRowGroup> {
     let mut next_id = 1;
-    let mut table = SpanTable::new(); //todo: don't keep the whole table in memory, only row groups
+    let mut table = SpanRowGroup::new(); //todo: don't keep the whole table in memory, only row groups
     let streams = find_process_thread_streams(connection, process_id).await?;
     for stream in streams {
         let blocks = find_stream_blocks(connection, &stream.stream_id).await?;
@@ -126,54 +193,9 @@ pub async fn make_span_table(
 }
 
 #[span_fn]
-pub fn write_parquet(file_path: &Path, spans: &SpanTable) -> Result<()> {
-    let message_type = "
-  message schema {
-    REQUIRED INT32 hash;
-    REQUIRED INT32 depth;
-    REQUIRED DOUBLE begin_ms;
-    REQUIRED DOUBLE end_ms;
-    REQUIRED INT64 id;
-    REQUIRED INT64 parent;
-  }
-";
-    let schema =
-        Arc::new(parse_message_type(message_type).with_context(|| "parsing spans schema")?);
-    let props = Arc::new(WriterProperties::builder().build());
-    let file = std::fs::File::create(file_path)
-        .with_context(|| format!("creating file {}", file_path.display()))?;
-    let mut writer = SerializedFileWriter::new(file, schema, props)
-        .with_context(|| "creating parquet writer")?;
-    let mut row_group_writer = writer
-        .next_row_group()
-        .with_context(|| "creating row group writer")?;
-    spans
-        .hashes
-        .write_batch(&mut *row_group_writer)
-        .with_context(|| "writing hash column")?;
-    spans
-        .depths
-        .write_batch(&mut *row_group_writer)
-        .with_context(|| "writing depth column")?;
-    spans
-        .begins
-        .write_batch(&mut *row_group_writer)
-        .with_context(|| "writing begins column")?;
-    spans
-        .ends
-        .write_batch(&mut *row_group_writer)
-        .with_context(|| "writing begins column")?;
-    spans
-        .ids
-        .write_batch(&mut *row_group_writer)
-        .with_context(|| "writing ids column")?;
-    spans
-        .parents
-        .write_batch(&mut *row_group_writer)
-        .with_context(|| "writing parents column")?;
-    writer
-        .close_row_group(row_group_writer)
-        .with_context(|| "closing row group")?;
+pub fn write_parquet(file_path: &Path, spans: SpanRowGroup) -> Result<()> {
+    let mut writer = SpanTableLocalWriter::create(file_path)?;
+    writer.append(spans)?;
     writer.close().with_context(|| "closing parquet writer")?;
     Ok(())
 }
