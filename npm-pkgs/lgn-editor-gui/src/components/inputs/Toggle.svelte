48b5a8495886f67875403c06b3bbc5c81bed9489
Tim <98831755+timnitsas-legion@users.noreply.github.com>
2022-05-26 16:47:46 -0400

    Update Toggle.svelte

--- npm-pkgs/lgn-editor-gui/src/components/inputs/Toggle.svelte
+++ npm-pkgs/lgn-editor-gui/src/components/inputs/Toggle.svelte
@@ -6,7 +6,6 @@
   }>();
 
   export let value: boolean;
-
   export let disabled = false;
 
   function toggle() {
@@ -38,18 +37,13 @@
     @apply bg-orange-700;
   }
 
-  .disabled {
-    @apply text-gray-400 cursor-not-allowed;
+  .root.disabled {
+    @apply bg-surface-600 cursor-not-allowed;
   }
 
   .handler {
-    /* group-hover:bg-orange-700 */
     @apply h-4 w-4 rounded-full transition-all;
   }
-
-  /* .handler.disabled {
-    @apply group-hover:bg-gray-400;
-  } */
 
   .handler-off {
     @apply ml-[2px] bg-item-max;
@@ -57,5 +51,9 @@
 
   .handler-on {
     @apply ml-[10px] bg-item-max;
+  }
+
+  .handler.disabled {
+    @apply bg-item-min;
   }
 </style>
