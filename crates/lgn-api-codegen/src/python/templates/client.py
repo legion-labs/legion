1e7a3ccfe6da417a9ff0135a3d9198c9b7571a1b
Kirill <kirill.daybov@legionlabs.com>
2022-05-27 10:37:21 -0400

    WIP

--- crates/lgn-api-codegen/src/python/templates/client.py
+++ crates/lgn-api-codegen/src/python/templates/client.py
@@ -1,4 +1,19 @@
 import requests
+
+class Client:
+    def __init__(self, uri):
+        self.uri = uri
+
+    def get_cars(
+        self,
+        space_id,
+        names,
+        q,
+        extra
+    ):
+        ""
+
+
 response = requests.get('https://api.github.com')
 print(response)
 print(response.headers)
