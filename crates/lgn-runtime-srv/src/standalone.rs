f476ec188379657d33ac45d954de3a062a730eef
jsguay-legion <jean-sebastien.guay@legionlabs.com>
2022-05-31 11:38:58 -0400

    Fixed lgn-runtime-srv compilation

--- crates/lgn-runtime-srv/src/standalone.rs
+++ crates/lgn-runtime-srv/src/standalone.rs
@@ -1,10 +1,8 @@
 use lgn_app::prelude::App;
-use lgn_ecs::prelude::{EventReader, IntoExclusiveSystem, NonSend, Query, Res};
+use lgn_ecs::prelude::{EventReader, IntoExclusiveSystem, NonSend, Res, ResMut};
 use lgn_gilrs::GilrsPlugin;
 use lgn_graphics_renderer::{
-    components::{
-        RenderSurface, RenderSurfaceCreatedForWindow, RenderSurfaceExtents, RenderSurfaces,
-    },
+    components::{RenderSurfaceCreatedForWindow, RenderSurfaceExtents, RenderSurfaces},
     Renderer,
 };
 use lgn_presenter_window::component::PresenterWindow;
@@ -39,16 +37,12 @@ fn on_render_surface_created_for_window(
     mut render_surfaces: ResMut<'_, RenderSurfaces>,
 ) {
     for event in event_render_surface_created.iter() {
-        let render_surface = render_surfaces
-            .iter_mut()
-            .find(|x| x.id() == event.render_surface_id);
-        if let Some(mut render_surface) = render_surface {
-            let wnd = wnd_list.get(event.window_id).unwrap();
-            let extents = RenderSurfaceExtents::new(wnd.physical_width(), wnd.physical_height());
+        let render_surface = render_surfaces.get_from_window_id_mut(event.window_id);
+        let wnd = wnd_list.get(event.window_id).unwrap();
+        let extents = RenderSurfaceExtents::new(wnd.physical_width(), wnd.physical_height());
 
-            let winit_wnd = winit_wnd_list.get_window(event.window_id).unwrap();
-            render_surface
-                .register_presenter(|| PresenterWindow::from_window(&renderer, winit_wnd, extents));
-        }
+        let winit_wnd = winit_wnd_list.get_window(event.window_id).unwrap();
+        render_surface
+            .register_presenter(|| PresenterWindow::from_window(&renderer, winit_wnd, extents));
     }
 }
