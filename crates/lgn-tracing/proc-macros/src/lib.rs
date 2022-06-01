412987dd3296b9d163207851939c5b333510b844
Kévin <94377405+kevin-legion@users.noreply.github.com>
2022-06-01 16:47:16 -0400

    Analytics: Deactive all support for async span_fn for now

--- crates/lgn-tracing/proc-macros/src/lib.rs
+++ crates/lgn-tracing/proc-macros/src/lib.rs
@@ -40,56 +40,27 @@ pub fn span_fn(
     args: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let args = parse_macro_input!(args as TraceArgs);
     let mut function = parse_macro_input!(input as ItemFn);
+
+    if function.sig.asyncness.is_some() {
+        // NOOP For now
+        return proc_macro::TokenStream::from(quote! {
+            #function
+        });
+    };
+
+    let args = parse_macro_input!(args as TraceArgs);
 
     let function_name = args
         .alternative_name
         .map_or(function.sig.ident.to_string(), |n| n.to_string());
 
-    if function.sig.asyncness.is_none() {
-        function.block.stmts.insert(0, parse_quote! {
+    function.block.stmts.insert(
+        0,
+        parse_quote! {
             lgn_tracing::span_scope!(_METADATA_FUNC, concat!(module_path!(), "::", #function_name));
-        });
-
-        return proc_macro::TokenStream::from(quote! {
-            #function
-        });
-    }
-
-    let stmts = function.block.stmts;
-
-    function.block.stmts = vec![
-        parse_quote! {
-           static _METADATA_FUNC: lgn_tracing::spans::SpanMetadata = lgn_tracing::spans::SpanMetadata {
-               name: concat!(module_path!(), "::", #function_name),
-               location: lgn_tracing::spans::SpanLocation {
-                   lod: lgn_tracing::Verbosity::Max,
-                   target: module_path!(),
-                   module_path: module_path!(),
-                   file: file!(),
-                   line: line!()
-               }
-           };
         },
-        parse_quote! {
-            lgn_tracing::dispatch::on_begin_scope(&_METADATA_FUNC);
-        },
-        parse_quote! {
-            let __lgn_tracing_future = async move {
-                #(#stmts)*
-            };
-        },
-        parse_quote! {
-            let __lgn_tracing_output = lgn_tracing::spans::Instrumentation::new(__lgn_tracing_future, &_METADATA_FUNC).await;
-        },
-        parse_quote! {
-            lgn_tracing::dispatch::on_end_scope(&_METADATA_FUNC);
-        },
-        parse_quote! {
-            return __lgn_tracing_output;
-        },
-    ];
+    );
 
     proc_macro::TokenStream::from(quote! {
         #function
--- crates/lgn-tracing/src/spans/mod.rs
+++ crates/lgn-tracing/src/spans/mod.rs
@@ -3,7 +3,5 @@ pub use block::*;
 
 mod events;
 pub use events::*;
-mod instrument;
-pub use instrument::*;
 
 // todo: implement non thread based perf spans for other systems to be used
