b208eff493e0725114384e764b8b252a5cd1989a
Kévin <94377405+kevin-legion@users.noreply.github.com>
2022-06-02 16:00:46 -0400

    Analytics: Metrics - Use proper colors

--- npm-pkgs/lgn-analytics-gui/src/components/Metric/Lib/MetricColor.ts
+++ npm-pkgs/lgn-analytics-gui/src/components/Metric/Lib/MetricColor.ts
@@ -1,5 +1,17 @@
-import { schemeCategory10 } from "d3";
+const colors = [
+  "#79CDAF",
+  "#3984B6",
+  "#F0A04B",
+  "#EC7C92",
+  "#7E84FA",
+  "#62CB5A",
+  "#147AF3",
+  "#A73B8F",
+  "#FBDB68",
+  "#E5632F",
+  "#008F5D",
+];
 
 export function getMetricColor(index: number) {
-  return schemeCategory10[index % schemeCategory10.length];
+  return colors[index % colors.length];
 }
