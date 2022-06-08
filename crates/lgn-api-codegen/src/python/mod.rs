b643c567b4f780e1384ff8ea3e777718db9d46f4
Kirill Daibov <kirill.daybov@legionlabs.com>
2022-06-07 22:44:17 -0400

    Python generator for OpenAPI (#1999)
    * Empty Python generator
    
    * Revert "Clippy..."
    
    This reverts commit d30c5bb9ee83feb42a3858a97add6a07fe532ee8.
    
    * Basic generator
    
    * Models generator
    
    * WIP
    
    * WIP
    
    * Client and most of the generation is done
    
    * WIP
    
    * Requests work against Car server
    
    * Response JSON parsing
    
    * Binary response test, OneOf improvement, snap test added
    
    * Remove openapi.json
    
    * Updating to new API
    
    * Undo material_manager.rs change
    
    * Fix rebase

---
+++ crates/lgn-api-codegen/src/python/mod.rs
@@ -0,0 +1,66 @@
+use std::path::Path;
+
+use crate::{
+    api_types::{GenerationContext, MediaType, Type},
+    Generator, Result,
+};
+use askama::Template;
+use lazy_static::__Deref;
+
+mod filters;
+
+#[derive(askama::Template)]
+#[template(path = "__init__.py.jinja", escape = "none")]
+struct PythonTemplate<'a> {
+    pub ctx: &'a GenerationContext,
+}
+
+#[derive(Default)]
+pub(crate) struct PythonGenerator {}
+
+impl Generator for PythonGenerator {
+    fn generate(&self, ctx: &GenerationContext, output_dir: &Path) -> Result<()> {
+        std::fs::create_dir_all(output_dir)?;
+
+        let output_file = output_dir.join("api.py");
+        let content = generate_content(ctx)?;
+
+        std::fs::write(output_file, content)?;
+
+        Ok(())
+    }
+}
+
+fn generate_content(ctx: &GenerationContext) -> Result<String> {
+    let content = PythonTemplate { ctx }.render()?;
+
+    Ok(content)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use crate::{
+        openapi_loader::OpenApiRefLocation, visitor::Visitor, GenerationOptions, OpenApiLoader,
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_py_generation() {
+        let loader = OpenApiLoader::default();
+        let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("../../tests/api-codegen")
+            .canonicalize()
+            .unwrap();
+        let openapi = loader
+            .load_openapi(OpenApiRefLocation::new(&root, "cars.yaml".into()))
+            .unwrap();
+        let ctx = GenerationContext::new(root, GenerationOptions::default());
+        let ctx = Visitor::new(ctx).visit(&[openapi.clone()]).unwrap();
+        let content = generate_content(&ctx).unwrap();
+
+        insta::assert_snapshot!(content);
+    }
+}
