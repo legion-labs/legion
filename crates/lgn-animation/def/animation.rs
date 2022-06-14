ce411d60991d0845c0237d7bab940c4affbdab2d
dentalFloss <60110615+florenceCloutier@users.noreply.github.com>
2022-06-14 08:35:31 -0400

    Animation/plugin, animation clip and skeleton (#2010)
    * Basic animation system skeleton

---
+++ crates/lgn-animation/def/animation.rs
@@ -0,0 +1,29 @@
+use crate::animation_skeleton::Skeleton;
+use lgn_math::{Quat, Vec3};
+use lgn_transform::components::Transform;
+
+#[component]
+pub struct AnimationTransformBundle {
+    pub translation: Vec3,
+    pub rotation: Quat,
+    pub scale: Vec3,
+}
+
+// Necessary intermediate component because a Vec<Vec<>> is supported by the data code-generation
+#[component]
+pub struct AnimationTransformBundleVec {
+    pub anim_transform_vec: Vec<AnimationTransformBundle>,
+}
+
+#[component]
+struct AnimationTrack {
+    key_frames: Vec<AnimationTransformBundleVec>,
+    current_key_frame_index: u32,
+    duration_key_frames: Vec<f32>,
+    time_since_last_tick: f32,
+    looping: bool,
+
+    // Skeleton
+    bone_ids: Vec<i32>,
+    parent_indices: Vec<i32>,
+}
---
+++ crates/lgn-animation/src/labels.rs
@@ -0,0 +1,7 @@
+use lgn_ecs::schedule::StageLabel;
+
+/// The names of the animation stages
+#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
+pub enum AnimationStage {
+    Update,
+}
