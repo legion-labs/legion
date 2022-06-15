fd3cc83b7ba69960152c3f3fa7b6cf5a6f61d617
jsguay-legion <103773257+jsguay-legion@users.noreply.github.com>
2022-06-15 19:04:58 -0400

    Merge vdbdd/renderer_v2 into main (#2061)
    * PrepareRender port + Visibility stub
    * Added plot of renderer CPU frame times to egui
    * More PrepareRender port + Visibility stub
    * Improve render object allocator API (#2015)
    * Render object queues (#2020)
    * Add multiple command queues: one per PrimaryTable and a global one
    * PrimaryTableView (#2021)
    * RenderVisual (#2025)
    * Fix for GPU transform update (#2029)
    * Switching to RH Z-up, CCW (#2028)
    * RenderCamera (#2034)
    * Viewport loop (#2039)
    * Moved code from graphics-renderer/lib.rs to a new file, and introduced per-viewport loop
    * Added composite viewports step
    * Fleshed out RenderViewport, added RenderViewportPrivateData, moved viewport composite
    * Changed Viewport to contain an id to a RenderCamera instead of a copy
    * Moved viewport-related structs and functionality to a new file.
    * Moved assignment of camera to viewport
    * Remove allow unsafe_code on render_update
    * Added type name to RenderObjectKey in debug
    * Fix crashes on exit related to new RenderObject stuff (#2051)
    * Render Object rename pass (#2053)
    * Rename pass for RenderObject APIs
    * Rename RenderViewportPrivateData -> RenderViewportRendererData
    * optims (#2058)
    * SmallVec optims and spans added
    * Fix crash on exit for sandbox in snapshot mode.
    
    Co-authored-by: Vincent Duboisdendien <vincent.duboisdendien@legionlabs.com>
    Co-authored-by: vduboisdendien-legion <88214962+vduboisdendien-legion@users.noreply.github.com>
    Co-authored-by: Kirill Daibov <kirill.daybov@legionlabs.com>

--- crates/lgn-graphics-renderer/src/core/services/cameras.rs
+++ crates/lgn-graphics-renderer/src/core/services/cameras.rs
@@ -1,12 +1,165 @@
+use dolly::{
+    prelude::{Handedness, RightHanded},
+    transform::Transform,
+};
+use lgn_graphics_cgen_runtime::Float4;
+use lgn_math::{Angle, DMat4, Mat4, Vec2, Vec4};
 use lgn_transform::prelude::GlobalTransform;
 
-use crate::components::CameraComponent;
+use crate::{cgen, components::CameraComponent, UP_VECTOR};
 
-#[derive(Default)]
-pub struct RenderCamera {}
+pub fn view_transform(final_transform: &Transform<RightHanded>) -> GlobalTransform {
+    let eye = final_transform.position.as_dvec3();
+    let forward = final_transform.forward().as_dvec3();
+
+    let view_matrix = DMat4::look_at_rh(eye, eye + forward, UP_VECTOR.as_dvec3());
+    let (_scale, rotation, translation) = view_matrix.to_scale_rotation_translation();
+
+    let mut view_transform = GlobalTransform::identity();
+    view_transform.translation = translation.as_vec3();
+    view_transform.rotation = rotation.as_f32();
+
+    view_transform
+}
+
+pub fn build_projection(width: f32, height: f32, fov_y: Angle, z_near: f32) -> Mat4 {
+    let aspect_ratio = width / height;
+    Mat4::perspective_infinite_reverse_rh(fov_y.radians(), aspect_ratio, z_near)
+}
+
+pub fn build_culling_planes(
+    final_transform: &Transform<RightHanded>,
+    aspect_ratio: f32,
+    fov_y: Angle,
+    z_near: f32,
+    z_far: f32,
+) -> [Float4; 6] {
+    let eye = final_transform.position;
+    let forward = final_transform.forward();
+    let up = final_transform.up();
+    let right = final_transform.right();
+
+    let half_v_side = z_far * (fov_y.radians() * 0.5).tan();
+    let half_h_side = half_v_side * aspect_ratio;
+
+    let near_face_point = eye + forward * z_near;
+    let near_normal = -forward;
+    let near_plane: Float4 = Vec4::from((near_normal, -near_normal.dot(near_face_point))).into();
+
+    let far_face_point = eye + forward * z_far;
+    let far_normal = forward;
+    let far_plane: Float4 = Vec4::from((far_normal, -far_normal.dot(far_face_point))).into();
+
+    let front_mult_far = z_far * forward;
+
+    let right_side = front_mult_far - right * half_h_side;
+    let right_normal = up.cross(right_side).normalize();
+    let right_plane: Float4 = Vec4::from((right_normal, -right_normal.dot(eye))).into();
+
+    let left_side = front_mult_far + right * half_h_side;
+    let left_normal = left_side.cross(up).normalize();
+    let left_plane: Float4 = Vec4::from((left_normal, -left_normal.dot(eye))).into();
+
+    let top_side = front_mult_far - up * half_v_side;
+    let top_normal = top_side.cross(right).normalize();
+    let top_plane: Float4 = Vec4::from((top_normal, -top_normal.dot(eye))).into();
+
+    let bottom_side = front_mult_far + up * half_v_side;
+    let bottom_normal = right.cross(bottom_side).normalize();
+    let bottom_plane: Float4 = Vec4::from((bottom_normal, -bottom_normal.dot(eye))).into();
+
+    [
+        near_plane,
+        far_plane,
+        right_plane,
+        left_plane,
+        top_plane,
+        bottom_plane,
+    ]
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct RenderCamera<H: Handedness = RightHanded> {
+    pub final_transform: Transform<H>,
+    fov_y: Angle,
+    z_near: f32,
+    z_far: f32,
+}
 
 impl From<(&GlobalTransform, &CameraComponent)> for RenderCamera {
-    fn from(_: (&GlobalTransform, &CameraComponent)) -> Self {
-        RenderCamera {}
+    fn from((_global_transform, camera_component): (&GlobalTransform, &CameraComponent)) -> Self {
+        RenderCamera::new(camera_component) // TMP
+    }
+}
+
+impl RenderCamera {
+    pub fn new(camera_component: &CameraComponent) -> Self {
+        Self {
+            final_transform: camera_component.final_transform(),
+            fov_y: camera_component.fov_y(),
+            z_near: camera_component.z_near(),
+            z_far: camera_component.z_far(),
+        }
+    }
+
+    pub fn view_transform(&self) -> GlobalTransform {
+        view_transform(&self.final_transform)
+    }
+
+    pub fn build_projection(&self, width: f32, height: f32) -> Mat4 {
+        build_projection(width, height, self.fov_y, self.z_near)
+    }
+
+    pub fn build_culling_planes(&self, aspect_ratio: f32) -> [Float4; 6] {
+        build_culling_planes(
+            &self.final_transform,
+            aspect_ratio,
+            self.fov_y,
+            self.z_near,
+            self.z_far,
+        )
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    pub fn tmp_build_view_data(
+        &self,
+        pixel_width: f32,
+        pixel_height: f32,
+        logical_width: f32,
+        logical_height: f32,
+        cursor_x: f32,
+        cursor_y: f32,
+    ) -> cgen::cgen_type::ViewData {
+        let view_transform = self.view_transform();
+        let projection = self.build_projection(pixel_width, pixel_height);
+        let culling_planes = self.build_culling_planes(pixel_width / pixel_height);
+
+        let mut camera_props = cgen::cgen_type::ViewData::default();
+
+        camera_props.set_camera_translation(view_transform.translation.into());
+        camera_props.set_camera_rotation(Vec4::from(view_transform.rotation).into());
+        camera_props.set_projection(projection.into());
+        camera_props.set_culling_planes(culling_planes);
+        camera_props.set_pixel_size(
+            Vec4::new(
+                pixel_width,
+                pixel_height,
+                1.0 / pixel_width,
+                1.0 / pixel_height,
+            )
+            .into(),
+        );
+        camera_props.set_logical_size(
+            Vec4::new(
+                logical_width,
+                logical_height,
+                1.0 / logical_width,
+                1.0 / logical_height,
+            )
+            .into(),
+        );
+        camera_props.set_cursor_pos(Vec2::new(cursor_x, cursor_y).into());
+
+        camera_props
     }
 }
--- crates/lgn-graphics-renderer/src/picking/manipulator.rs
+++ crates/lgn-graphics-renderer/src/picking/manipulator.rs
@@ -1,11 +1,12 @@
 use lgn_ecs::prelude::{Commands, Entity, Res};
 use lgn_graphics_data::Color;
 use lgn_input::keyboard::KeyCode;
-use lgn_math::{Quat, Vec2, Vec3, Vec4, Vec4Swizzles};
+use lgn_math::{Mat4, Quat, Vec2, Vec3, Vec4, Vec4Swizzles};
 use lgn_transform::prelude::{GlobalTransform, Transform};
 
 use crate::{
-    components::{CameraComponent, ManipulatorComponent, RenderSurfaceExtents},
+    components::{CameraComponent, ManipulatorComponent},
+    core::{build_projection, view_transform},
     resources::DefaultMeshType,
 };
 
@@ -122,11 +123,16 @@ pub(super) fn new_world_point_for_cursor(
     let screen_offset = 2.0 * (cursor_pos / screen_size) - 1.0;
     let screen_pos = Vec4::new(screen_offset.x, screen_offset.y, 0.5, 1.0);
 
-    let projection_matrix = camera.build_projection(screen_size.x as f32, screen_size.y as f32);
+    let projection_matrix = build_projection(
+        screen_size.x as f32,
+        screen_size.y as f32,
+        camera.fov_y(),
+        camera.z_near(),
+    );
     let mut view_pos = projection_matrix.inverse().mul_vec4(screen_pos);
     view_pos /= view_pos.w;
 
-    let view_matrix = camera.view_transform().compute_matrix();
+    let view_matrix = view_transform(&camera.final_transform()).compute_matrix();
     let world_pos = view_matrix.inverse().mul_vec4(view_pos);
 
     let ray_dir = (world_pos.xyz() - camera_pos).normalize();
@@ -332,17 +338,10 @@ impl ManipulatorManager {
     pub fn scale_manipulator_for_viewport(
         entity_transform: &GlobalTransform,
         manipulator_transform: &Transform,
-        render_surface_extents: RenderSurfaceExtents,
-        camera: &CameraComponent,
+        projection_matrix: Mat4,
+        camera_transform: &GlobalTransform,
     ) -> GlobalTransform {
-        let projection_matrix = camera.build_projection(
-            render_surface_extents.width() as f32,
-            render_surface_extents.height() as f32,
-        );
-
-        let view_pos = camera
-            .view_transform()
-            .mul_vec3(entity_transform.translation);
+        let view_pos = camera_transform.mul_vec3(entity_transform.translation);
         let x_offset = view_pos + Vec3::new(0.5, 0.0, 0.0);
         let y_offset = view_pos + Vec3::new(0.0, 0.5, 0.0);
 
