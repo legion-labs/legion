f73005f9b05464cb262843a9c48bfc2b4716d518
Kirill Daibov <kirill.daybov@legionlabs.com>
2022-06-20 16:31:32 -0400

    Camera refactoring (#2081)
    Removed dependency on dolly. Transform is now taken from a GlobalTransform of the entity that has CameraComponent. CameraOptions resource is introduced that holds default camera setup and speed.
    
    The camera movement is no longer smoothed.

--- crates/lgn-graphics-renderer/src/components/camera_component.rs
+++ crates/lgn-graphics-renderer/src/components/camera_component.rs
@@ -1,9 +1,3 @@
-use std::marker::PhantomData;
-
-use dolly::driver::RigDriver;
-use dolly::prelude::{Handedness, Position, RightHanded, Smooth};
-use dolly::rig::{CameraRig, RigUpdateParams};
-use dolly::transform::Transform;
 use lgn_core::Time;
 use lgn_ecs::prelude::*;
 use lgn_graphics_data::runtime::CameraSetup;
@@ -20,79 +14,50 @@ use lgn_transform::components::GlobalTransform;
 use lgn_utils::HashMap;
 
 use crate::core::{PrimaryTableView, RenderCamera, RenderObjectId};
-use crate::UP_VECTOR;
 
-#[derive(Debug)]
-pub struct EulerRotator {
-    alpha: Angle,
-    beta: Angle,
-    gamma: Angle,
-
-    euler: EulerRot,
-}
-
-impl Default for EulerRotator {
-    fn default() -> Self {
-        Self::new(EulerRot::YZX)
-    }
-}
-
-impl EulerRotator {
-    pub fn new(euler: EulerRot) -> Self {
-        Self {
-            alpha: Angle::from_degrees(0.0),
-            beta: Angle::from_degrees(0.0),
-            gamma: Angle::from_degrees(0.0),
-            euler,
-        }
-    }
-
-    #[must_use]
-    pub fn rotation_quat(mut self, rotation: Quat) -> Self {
-        self.set_rotation_quat(rotation);
-        self
-    }
-
-    pub fn rotate(&mut self, alpha: Angle, beta: Angle, gamma: Angle) {
-        self.set_rotation_angles(self.alpha + alpha, self.beta + beta, self.gamma + gamma);
-    }
-
-    pub fn set_rotation_quat(&mut self, rotation: Quat) {
-        let (alpha, beta, gamma) = rotation.to_euler(self.euler);
-        self.set_rotation_angles(
-            Angle::from_radians(alpha),
-            Angle::from_radians(beta),
-            Angle::from_radians(gamma),
-        );
-    }
-
-    fn set_rotation_angles(&mut self, alpha: Angle, beta: Angle, gamma: Angle) {
-        self.alpha = Angle::from_radians(alpha.radians() % (std::f32::consts::TAU));
-        self.beta = Angle::from_radians(beta.radians() % (std::f32::consts::TAU));
-        self.gamma = Angle::from_radians(gamma.radians().clamp(-std::f32::consts::PI, 0.0));
-    }
-}
-
-impl<H: Handedness> RigDriver<H> for EulerRotator {
-    fn update(&mut self, params: RigUpdateParams<'_, H>) -> Transform<H> {
-        Transform {
-            position: params.parent.position,
-            rotation: Quat::from_euler(
-                self.euler,
-                self.alpha.radians(),
-                self.beta.radians(),
-                self.gamma.radians(),
-            ),
-            phantom: PhantomData,
-        }
-    }
-}
-#[derive(Component)]
-pub struct CameraComponent {
-    camera_rig: CameraRig,
+pub struct CameraOptions {
+    setup: CameraSetup,
     speed: f32,
     rotation_speed: Angle,
-    setup: CameraSetup,
+}
+
+impl CameraOptions {
+    fn reset(&self, camera_transform: &mut GlobalTransform) {
+        self.setup_camera_transform(camera_transform);
+    }
+
+    fn setup_camera_transform(&self, camera_transform: &mut GlobalTransform) {
+        let forward = self.setup.look_at - self.setup.eye;
+        let (yaw, pitch) = dir_to_yaw_pitch(forward);
+        camera_transform.rotation = Quat::from_euler(EulerRot::YZX, 0.0, yaw, pitch);
+        camera_transform.translation = self.setup.eye;
+    }
+}
+
+impl Default for CameraOptions {
+    fn default() -> Self {
+        let setup = CameraSetup {
+            eye: Vec3::new(0.0, -2.0, 1.0),
+            look_at: Vec3::ZERO,
+        };
+
+        Self {
+            speed: 2.5,
+            rotation_speed: Angle::from_degrees(40.0),
+            setup,
+        }
+    }
+}
+
+fn dir_to_yaw_pitch(dir: Vec3) -> (f32, f32) {
+    let yaw = (-dir.x).atan2(dir.y);
+    let dir_no_yaw = Mat3::from_rotation_z(-yaw) * dir;
+    let pitch = (dir_no_yaw.y).atan2(-dir_no_yaw.z);
+    (yaw, pitch)
+}
+
+#[derive(Component)]
+pub struct CameraComponent {
     fov_y: Angle,
     z_near: f32,
     z_far: f32,
@@ -100,18 +65,6 @@ pub struct CameraComponent {
 }
 
 impl CameraComponent {
-    pub fn position(&self) -> Vec3 {
-        self.camera_rig.final_transform.position
-    }
-
-    pub fn rotation(&self) -> Quat {
-        self.camera_rig.final_transform.rotation
-    }
-
-    pub fn final_transform(&self) -> Transform<RightHanded> {
-        self.camera_rig.final_transform
-    }
-
     pub fn fov_y(&self) -> Angle {
         self.fov_y
     }
@@ -127,44 +80,11 @@ impl CameraComponent {
     pub fn render_object_id(&self) -> Option<RenderObjectId> {
         self.render_object_id
     }
-
-    fn build_rig(setup: &CameraSetup) -> CameraRig {
-        let forward = (setup.look_at - setup.eye).normalize();
-        let forward_dot = forward.dot(UP_VECTOR);
-        let right = if (forward_dot - 1.0).abs() < std::f32::EPSILON {
-            Vec3::new(-1.0, 0.0, 0.0)
-        } else if (forward_dot + 1.0).abs() < std::f32::EPSILON {
-            Vec3::new(1.0, 0.0, 0.0)
-        } else {
-            forward.cross(UP_VECTOR).normalize()
-        };
-        let up = right.cross(forward);
-        let rotation = Quat::from_mat3(&Mat3::from_cols(right, up, -forward));
-
-        CameraRig::builder()
-            .with(Position::new(setup.eye))
-            .with(EulerRotator::new(EulerRot::YZX).rotation_quat(rotation))
-            .with(Smooth::new_position_rotation(0.2, 0.2))
-            .build()
-    }
-
-    fn reset(&mut self) {
-        self.camera_rig = Self::build_rig(&self.setup);
-    }
 }
 
 impl Default for CameraComponent {
     fn default() -> Self {
-        let setup = CameraSetup {
-            eye: Vec3::new(0.0, 2.0, 1.0),
-            look_at: Vec3::ZERO,
-        };
-
         Self {
-            camera_rig: Self::build_rig(&setup),
-            speed: 2.5,
-            rotation_speed: Angle::from_degrees(40.0),
-            setup,
             fov_y: Angle::from_radians(std::f32::consts::FRAC_PI_4),
             z_near: 0.01,
             z_far: 100.0,
@@ -173,10 +93,16 @@ impl Default for CameraComponent {
     }
 }
 
-pub(crate) fn tmp_create_camera(mut commands: Commands<'_, '_>) {
+#[allow(clippy::needless_pass_by_value)]
+pub(crate) fn tmp_create_camera(
+    mut commands: Commands<'_, '_>,
+    camera_options: Res<'_, CameraOptions>,
+) {
+    let mut camera_transform = GlobalTransform::default();
+    camera_options.reset(&mut camera_transform);
     commands
         .spawn()
-        .insert_bundle((GlobalTransform::default(), CameraComponent::default()));
+        .insert_bundle((camera_transform, CameraComponent::default()));
 }
 
 #[derive(Component, Default)]
@@ -184,14 +110,14 @@ pub(crate) struct CameraSetupApplied(); // marker component
 
 pub(crate) fn apply_camera_setups(
     camera_setups: Query<'_, '_, (Entity, &CameraSetup), Without<CameraSetupApplied>>,
-    mut cameras: Query<'_, '_, &mut CameraComponent>,
+    mut cameras: Query<'_, '_, &mut GlobalTransform, With<CameraComponent>>,
     mut commands: Commands<'_, '_>,
+    mut camera_options: ResMut<'_, CameraOptions>,
 ) {
     for (entity, setup) in camera_setups.iter() {
-        if let Some(mut camera) = cameras.iter_mut().next() {
-            let camera = camera.as_mut();
-            camera.setup = setup.clone();
-            camera.reset();
+        if let Some(mut transform) = cameras.iter_mut().next() {
+            camera_options.setup = setup.clone();
+            camera_options.reset(transform.as_mut());
         }
         commands
             .entity(entity)
@@ -203,7 +129,7 @@ pub(crate) fn apply_camera_setups(
 
 #[allow(clippy::needless_pass_by_value, clippy::too_many_arguments)]
 pub(crate) fn camera_control(
-    mut cameras_query: Query<'_, '_, &mut CameraComponent>,
+    mut cameras_query: Query<'_, '_, &mut GlobalTransform, With<CameraComponent>>,
     mut mouse_motion_events: EventReader<'_, '_, MouseMotion>,
     mut mouse_wheel_events: EventReader<'_, '_, MouseWheel>,
     mouse_buttons: Res<'_, Input<MouseButton>>,
@@ -211,6 +137,7 @@ pub(crate) fn camera_control(
     gamepads: Res<'_, Gamepads>,
     gamepad_axes: Res<'_, Axis<GamepadAxis>>,
     gamepad_buttons: Res<'_, Input<GamepadButton>>,
+    mut camera_options: ResMut<'_, CameraOptions>,
 
     time: Res<'_, Time>,
 ) {
@@ -218,8 +145,8 @@ pub(crate) fn camera_control(
         return;
     }
     // Need to associate inputs with window/camera... we don''t have that for now
-    for mut camera in cameras_query.iter_mut() {
-        let camera = camera.as_mut();
+    for mut transform in cameras_query.iter_mut() {
+        let transform = transform.as_mut();
 
         if keys.pressed(KeyCode::Z)
             && !keys.any_pressed([
@@ -229,7 +156,7 @@ pub(crate) fn camera_control(
                 KeyCode::RControl,
             ])
         {
-            camera.reset();
+            camera_options.reset(transform);
             continue;
         }
 
@@ -255,34 +182,33 @@ pub(crate) fn camera_control(
             let mut camera_translation_change = Vec3::ZERO;
 
             if keys.pressed(KeyCode::W) {
-                camera_translation_change += camera.camera_rig.final_transform.forward();
+                camera_translation_change += transform.forward();
             }
             if keys.pressed(KeyCode::S) {
-                camera_translation_change -= camera.camera_rig.final_transform.forward();
+                camera_translation_change -= transform.forward();
             }
             if keys.pressed(KeyCode::A) {
-                camera_translation_change -= camera.camera_rig.final_transform.right();
+                camera_translation_change -= transform.right();
             }
             if keys.pressed(KeyCode::D) {
-                camera_translation_change += camera.camera_rig.final_transform.right();
+                camera_translation_change += transform.right();
             }
 
             if let Some(gamepad) = gamepad {
                 if let Some(left_x) =
                     gamepad_axes.get(GamepadAxis(gamepad, GamepadAxisType::LeftStickX))
                 {
-                    camera_translation_change += left_x * camera.camera_rig.final_transform.right();
+                    camera_translation_change += left_x * transform.right();
                 }
 
                 if let Some(left_y) =
                     gamepad_axes.get(GamepadAxis(gamepad, GamepadAxisType::LeftStickY))
                 {
-                    camera_translation_change +=
-                        left_y * camera.camera_rig.final_transform.forward();
+                    camera_translation_change += left_y * transform.forward();
                 }
             }
 
-            let mut speed = camera.speed.exp();
+            let mut speed = camera_options.speed.exp();
             if let Some(gamepad) = gamepad {
                 if gamepad_buttons.pressed(GamepadButton(gamepad, GamepadButtonType::RightTrigger2))
                 {
@@ -294,20 +220,20 @@ pub(crate) fn camera_control(
             }
             camera_translation_change *= speed * time.delta_seconds();
 
-            camera
-                .camera_rig
-                .driver_mut::<Position>()
-                .translate(camera_translation_change);
+            transform.translation += camera_translation_change;
 
-            let rotation_speed = camera.rotation_speed;
-            let camera_driver = camera.camera_rig.driver_mut::<EulerRotator>();
+            let rotation_speed = camera_options.rotation_speed;
             for mouse_motion_event in mouse_motion_events.iter() {
-                let rotation = (rotation_speed.degrees() * time.delta_seconds()).min(10.0); // clamping rotation speed for when it's laggy
-                camera_driver.rotate(
-                    Angle::from_degrees(0.0),
-                    Angle::from_degrees(mouse_motion_event.delta.x * rotation),
-                    Angle::from_degrees(-mouse_motion_event.delta.y * rotation),
-                );
+                let rotation = (rotation_speed.degrees() * time.delta_seconds())
+                    .min(10.0)
+                    .to_radians(); // clamping rotation speed for when it's laggy
+                let (yaw, pitch) = dir_to_yaw_pitch(transform.forward());
+                let delta_yaw = mouse_motion_event.delta.x * rotation;
+                let delta_pitch = mouse_motion_event.delta.y * rotation;
+                let yaw = (yaw - delta_yaw) % std::f32::consts::TAU;
+                let pitch = (pitch - delta_pitch)
+                    .clamp(std::f32::EPSILON, std::f32::consts::PI - std::f32::EPSILON);
+                transform.rotation = Quat::from_euler(EulerRot::YZX, 0.0, yaw, pitch);
             }
             for mouse_wheel_event in mouse_wheel_events.iter() {
                 // Different signs on Line and Pixel is correct. Line returns positive values when scrolling up
@@ -317,10 +243,9 @@ pub(crate) fn camera_control(
                     // Last time I tested one segment of a wheel would yield 250 pixels,
                     MouseScrollUnit::Pixel => -mouse_wheel_event.y / 1000.0,
                 };
-                camera.speed = (camera.speed + speed_change).clamp(0.0, 10.0);
+                camera_options.speed = (camera_options.speed + speed_change).clamp(0.0, 10.0);
             }
         }
-        camera.camera_rig.update(time.delta_seconds());
     }
 }
 
@@ -387,5 +312,38 @@ pub(crate) fn reflect_camera_components(
         for (e, visual) in queries.p1().iter() {
             map.insert(e, visual.render_object_id.unwrap());
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use lgn_math::Vec3;
+
+    use crate::components::camera_component::dir_to_yaw_pitch;
+
+    use std::f32::{
+        consts::{FRAC_PI_2, FRAC_PI_4, PI},
+        EPSILON,
+    };
+
+    #[test]
+    fn test_dir_to_yaw_pitch() {
+        let (yaw, pitch) = dir_to_yaw_pitch(Vec3::new(0.0, -1.0, -1.0));
+        println!(
+            "yaw: {:.2}, pitch: {:.2}",
+            yaw.to_degrees(),
+            pitch.to_degrees()
+        );
+        assert!((yaw - (-PI)).abs() < EPSILON);
+        assert!((pitch - FRAC_PI_4).abs() < EPSILON);
+
+        let (yaw, pitch) = dir_to_yaw_pitch(Vec3::new(-1.0, -1.0, 0.0));
+        println!(
+            "yaw: {:.2}, pitch: {:.2}",
+            yaw.to_degrees(),
+            pitch.to_degrees()
+        );
+        assert!((yaw - 3.0 * FRAC_PI_4).abs() < EPSILON);
+        assert!((pitch - FRAC_PI_2).abs() < EPSILON);
     }
 }
