aec24eda981d11a75a6757fce7aa46fc6c1d0dd4
Florence <northeastflo@gmail.com>
2022-06-28 03:17:04 -0400

    Fixed an import

--- crates/lgn-animation/src/animation_system.rs
+++ crates/lgn-animation/src/animation_system.rs
@@ -1,6 +1,6 @@
 use crate::components::GraphDefinition;
-use lgn_core::Time;
 use lgn_ecs::prelude::{Query, Res};
+use lgn_time::prelude::Time;
 
 pub(crate) fn graph_update(mut graphs: Query<'_, '_, &mut GraphDefinition>, time: Res<'_, Time>) {
     let delta_time = time.delta_seconds();
