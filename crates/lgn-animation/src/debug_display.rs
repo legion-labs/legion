f04ef5b8be07347170834ea3e5e87aa2d0e1019a
jsguay-legion <jean-sebastien.guay@legionlabs.com>
2022-06-28 15:45:32 -0400

    Remove BumpAllocatorPool which was only used for DebugDisplay

--- crates/lgn-animation/src/debug_display.rs
+++ crates/lgn-animation/src/debug_display.rs
@@ -2,14 +2,12 @@ use crate::{
     animation_options::AnimationOptions, components::GraphDefinition,
     runtime_graph::node_state_machine::StateInfo,
 };
-use lgn_core::BumpAllocatorPool;
 use lgn_ecs::prelude::{Query, Res};
 use lgn_graphics_data::Color;
 use lgn_graphics_renderer::{debug_display::DebugDisplay, resources::DefaultMeshType};
 
 pub(crate) fn display_animation(
     debug_display: Res<'_, DebugDisplay>,
-    bump_allocator_pool: Res<'_, BumpAllocatorPool>,
     animation_options: Res<'_, AnimationOptions>,
     graphs: Query<'_, '_, &GraphDefinition>,
 ) {
@@ -17,44 +15,40 @@ pub(crate) fn display_animation(
         return;
     }
 
-    bump_allocator_pool.scoped_bump(|bump| {
-        debug_display.create_display_list(bump, |builder| {
-            for graph in graphs.iter() {
-                let active_state: &StateInfo = graph.get_current_node().get_active_state().unwrap();
-                let clip = active_state.state_node.child_node.get_clip().unwrap();
+    debug_display.create_display_list(|builder| {
+        for graph in graphs.iter() {
+            let active_state: &StateInfo = graph.get_current_node().get_active_state().unwrap();
+            let clip = active_state.state_node.child_node.get_clip().unwrap();
 
-                for n_bone in 0..clip.poses[clip.current_key_frame_index]
+            for n_bone in 0..clip.poses[clip.current_key_frame_index]
+                .skeleton
+                .bone_ids
+                .len()
+            {
+                let bone_depth: u8 = clip.poses[clip.current_key_frame_index]
                     .skeleton
-                    .bone_ids
-                    .len()
-                {
-                    let bone_depth: u8 = clip.poses[clip.current_key_frame_index]
-                        .skeleton
-                        .get_bone_depth(
-                            clip.poses[clip.current_key_frame_index].skeleton.bone_ids[n_bone]
-                                .unwrap(),
-                        )
-                        .try_into()
-                        .unwrap();
-                    let color_interval: u8 = (255
-                        / clip.poses[clip.current_key_frame_index]
-                            .skeleton
-                            .get_max_bone_depth()
-                            .unwrap())
+                    .get_bone_depth(
+                        clip.poses[clip.current_key_frame_index].skeleton.bone_ids[n_bone].unwrap(),
+                    )
                     .try_into()
                     .unwrap();
-                    let debug_color = Color::new(bone_depth * color_interval, 255, 52, 255);
-                    builder.add_default_mesh(
-                        &clip.poses[clip.current_key_frame_index].transforms[n_bone].global,
-                        DefaultMeshType::Sphere,
-                        debug_color,
-                    );
-                }
+                let color_interval: u8 = (255
+                    / clip.poses[clip.current_key_frame_index]
+                        .skeleton
+                        .get_max_bone_depth()
+                        .unwrap())
+                .try_into()
+                .unwrap();
+                let debug_color = Color::new(bone_depth * color_interval, 255, 52, 255);
+                builder.add_default_mesh(
+                    &clip.poses[clip.current_key_frame_index].transforms[n_bone].global,
+                    DefaultMeshType::Sphere,
+                    debug_color,
+                );
             }
-        });
+        }
     });
     drop(debug_display);
-    drop(bump_allocator_pool);
     drop(animation_options);
     drop(graphs);
 }
--- crates/lgn-core/src/memory/mod.rs
+++ crates/lgn-core/src/memory/mod.rs
@@ -2,5 +2,3 @@ mod handle;
 pub use handle::*;
 mod object_pool;
 pub use object_pool::*;
-mod bump_allocator;
-pub use bump_allocator::*;
